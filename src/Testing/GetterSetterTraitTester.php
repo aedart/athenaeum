d124bea818973cb0daa5ea297ccf421ba58f9241
Alin Eugen Deac <aedart@gmail.com>
2018-09-04 20:07:34 +0200

    Migrate trait tester (incomplete)

---
+++ src/Testing/GetterSetterTraitTester.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Aedart\Testing;
+
+/**
+ * Getter Setter Trait Tester
+ *
+ * <br />
+ *
+ * Offers assertion of "getter-setter-traits".
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Testing
+ */
+trait GetterSetterTraitTester
+{
+    /*****************************************************************
+     * Assertions
+     ****************************************************************/
+
+    // TODO: ...
+
+    /*****************************************************************
+     * Utilities
+     ****************************************************************/
+
+    // TODO: ...
+
+}
