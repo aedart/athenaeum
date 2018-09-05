0382f8e6b5b57727986df86848339e1433592baf
Alin Eugen Deac <aedart@gmail.com>
2018-09-05 19:59:09 +0200

    Migrate exception

---
+++ src/Testing/Exceptions/IncorrectPropertiesAmount.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Testing\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Incorrect Properties Amount
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Testing\Exceptions
+ */
+class IncorrectPropertiesAmount extends RuntimeException
+{
+
+}
