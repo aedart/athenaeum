a79710642ba67647975d1dd66d6da40fde951e06
Alin Eugen Deac <aedart@gmail.com>
2018-09-08 19:53:25 +0200

    Change tester trait, remove compatibility assert
    Idea might had been good, yet stops all tests with a fatal error that you
    cannot recover from. This is was a bad way of ensuring everything was in
    order. Perhaps PHP 8.x will allow traits to implement interfaces and thus
    resolve this automatically.
    
    Also, now using the console debugger

--- src/Testing/GetterSetterTraitTester.php
+++ src/Testing/GetterSetterTraitTester.php
@@ -2,6 +2,7 @@
 
 namespace Aedart\Testing;
 
+use Aedart\Testing\Helpers\ConsoleDebugger;
 use Aedart\Testing\Helpers\TraitTester;
 use PHPUnit\Framework\TestCase;
 use ReflectionException;
@@ -18,6 +19,8 @@ use ReflectionException;
  */
 trait GetterSetterTraitTester
 {
+    use ConsoleDebugger;
+
     /*****************************************************************
      * Assertions
      ****************************************************************/
@@ -42,24 +45,6 @@ trait GetterSetterTraitTester
     public function assertGetterSetterTraitMethods(string $trait, $setValue, $defaultValue)
     {
         $this->makeTraitTester($trait)->assertWithValues($setValue, $defaultValue);
-    }
-
-    // TODO: Needs improvement
-    public function assertTraitCompatibility(string $trait, string $interface)
-    {
-        $id = 'GstDummy_' . str_replace('.', '_', microtime(true));
-
-        $template = "class {$id} implements {$interface} { use {$trait}; }";
-
-        // PHP will automatically fail if the trait contains
-        // less or incorrect interface defined methods.
-        // This may not be the best way of testing this - but it works.
-        // Future versions will improve on this, and allow for none-blocking failures.
-        // --> PHP 7's Anonymous classes could be a good an alternative.
-        eval($template);
-
-        /** @var TestCase $this */
-        $this->assertTrue(true);
     }
 
     /*****************************************************************
