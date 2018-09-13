de03be1c41e8ed0e148c6e96eb90df46dc0ee1da
Alin Eugen Deac <aedart@gmail.com>
2018-09-13 18:42:15 +0200

    Migrate UndefinedProperty exception

---
+++ src/Properties/Exceptions/UndefinedProperty.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aedart\Properties\Exceptions;
+
+use LogicException;
+
+/**
+ * Undefined Property Exception
+ *
+ * <br />
+ *
+ * Throw this exception when a non-existing property is attempted accessed
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Properties\Exceptions
+ */
+class UndefinedProperty extends LogicException
+{
+
+}
