40415768cb8a28c474e58425f33ae16015dd2243
Alin Eugen Deac <aedart@gmail.com>
2018-09-15 12:50:56 +0200

    Add IntegrationTestCase

---
+++ src/Testing/TestCases/IntegrationTestCase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Aedart\Testing\TestCases;
+
+use Aedart\Container\IoC;
+use Aedart\Contracts\Container\IoC as IoCInterface;
+
+/**
+ * Integration Test Case
+ *
+ * <br />
+ *
+ * Base test-case for integration tests.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Testing\TestCases
+ */
+abstract class IntegrationTestCase extends UnitTestCase
+{
+    /**
+     * Service Container instance
+     *
+     * @var IoCInterface|null
+     */
+    protected $ioc;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _before()
+    {
+        parent::_before();
+
+        $this->ioc = IoC::getInstance();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _after()
+    {
+        if(isset($this->ioc)){
+            $this->ioc->destroy();
+            $this->ioc = null;
+        }
+
+        parent::_after();
+    }
+
+    /*****************************************************************
+     * Helpers and Utilities
+     ****************************************************************/
+}
