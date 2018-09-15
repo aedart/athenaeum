990d73674d463d6b3ca3843949933041b841f79b
Alin Eugen Deac <aedart@gmail.com>
2018-09-15 12:45:17 +0200

    Add IoC doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,6 +54,13 @@ function genSidebarComponents () {
             ]
         },
         {
+            title: 'Container',
+            collapsable: true,
+            children: [
+                'container/',
+            ]
+        },
+        {
             title: 'Properties',
             collapsable: true,
             children: [
