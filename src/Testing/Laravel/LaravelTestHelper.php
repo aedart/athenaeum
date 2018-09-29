42ce0b1f2db084d1cdafc46c3c85f332ae06e70e
Alin Eugen Deac <aedart@gmail.com>
2018-09-29 08:06:52 +0200

    Migrate LaravelTestHelper
    Previous TestHelperTrait

---
+++ src/Testing/Laravel/LaravelTestHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Aedart\Testing\Laravel;
+
+use Illuminate\Foundation\Testing\Concerns\InteractsWithAuthentication;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithExceptionHandling;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithSession;
+use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
+use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
+
+/**
+ * Laravel Test Helper
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Testing\Laravel
+ */
+trait LaravelTestHelper
+{
+    use ApplicationInitiator;
+    use InteractsWithAuthentication;
+    use InteractsWithConsole;
+    use InteractsWithContainer;
+    use InteractsWithDatabase;
+    use InteractsWithExceptionHandling;
+    use InteractsWithSession;
+    use MakesHttpRequests;
+    use MocksApplicationServices;
+}
