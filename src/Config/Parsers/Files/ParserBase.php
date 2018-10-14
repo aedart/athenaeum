241ecc642c1acdf82869cf70838f7760b59ac275
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 18:26:40 +0200

    Migrate ParserBase
    Previous "AbstractParser"

---
+++ src/Config/Parsers/Files/ParserBase.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Aedart\Config\Parsers\Files;
+
+use Aedart\Config\Parsers\Exceptions\FileDoesNotExist;
+use Aedart\Config\Parsers\Exceptions\UnableToParseFile;
+use Aedart\Contracts\Config\Parsers\Exceptions\FileDoesNotExistException;
+use Aedart\Contracts\Config\Parsers\FileParser;
+use Aedart\Support\Helpers\Filesystem\FileTrait;
+use Throwable;
+
+/**
+ * File Parser Base
+ *
+ * <br />
+ *
+ * Abstraction for a File Parser.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Parsers\Files
+ */
+abstract class ParserBase implements FileParser
+{
+    use FileTrait;
+
+    /**
+     * File path that holds configuration
+     *
+     * @var null|string
+     */
+    protected $filePath = null;
+
+    /**
+     * ParserBase constructor.
+     *
+     * @param null|string $filePath [optional]
+     *
+     * @throws FileDoesNotExistException
+     */
+    public function __construct(?string $filePath = null)
+    {
+        if(isset($filePath)){
+            $this->setFilePath($filePath);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFilePath(string $filePath): FileParser
+    {
+        if( ! is_file($filePath)){
+            throw new FileDoesNotExist(sprintf('%s does not exist', $filePath));
+        }
+
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilePath(): ?string
+    {
+        return $this->filePath;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasFilePath(): bool
+    {
+        return isset($this->filePath);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadAndParse(): array
+    {
+        if( ! $this->hasFilePath()){
+            throw new UnableToParseFile('No file path has been specified');
+        }
+
+        try {
+            $content = $this->getFile()->get($this->getFilePath());
+        } catch (Throwable $e){
+            throw new UnableToParseFile($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return $this->parse($content);
+    }
+}
