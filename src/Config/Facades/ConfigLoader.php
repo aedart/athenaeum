c0efb36d342845d1619ccf35d4d166ddd65d3142
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 17:47:51 +0200

    Migrate ConfigLoader Facade

---
+++ src/Config/Facades/ConfigLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Aedart\Config\Facades;
+
+
+use Aedart\Contracts\Config\Loader;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Config Loader Facade
+ *
+ * @method Loader setDirectory(string $path)
+ * @method string|null getDirectory()
+ * @method bool hasDirectory()
+ * @method void load()
+ * @method Repository parse(string $filePath)
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Facades
+ */
+class ConfigLoader extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected static function getFacadeAccessor()
+    {
+        return Loader::class;
+    }
+}
