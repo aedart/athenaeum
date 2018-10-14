03ff783bbae826b6bc860d1dc5bbf495b33ff363
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 18:51:27 +0200

    Migrate loader exception

---
+++ src/Config/Loaders/Exceptions/InvalidPath.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Config\Loaders\Exceptions;
+
+use Aedart\Contracts\Config\Loaders\Exceptions\InvalidPathException;
+use RuntimeException;
+
+/**
+ * Invalid Path Exception
+ *
+ * @see \Aedart\Contracts\Config\Loaders\Exceptions\InvalidPathException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Loaders\Exceptions
+ */
+class InvalidPath extends RuntimeException implements InvalidPathException
+{
+
+}
