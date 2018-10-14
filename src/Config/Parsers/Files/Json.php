80c02bde9a413788a84e2fb5c450be0eb9d4ef18
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 18:37:51 +0200

    Migrate Json file parser

---
+++ src/Config/Parsers/Files/Json.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Aedart\Config\Parsers\Files;
+
+use Aedart\Config\Parsers\Exceptions\UnableToParseFile;
+use Aedart\Utils\Json as JsonUtil;
+use Throwable;
+
+/**
+ * Json Configuration File Parser
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Parsers\Files
+ */
+class Json extends ParserBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    static public function getFileType(): string
+    {
+        return 'json';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parse(string $content): array
+    {
+        try {
+
+            return JsonUtil::decode($content, true);
+
+        } catch (Throwable $e){
+            throw new UnableToParseFile($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
