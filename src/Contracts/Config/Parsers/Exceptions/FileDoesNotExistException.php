d7b61e1b7517c10c5febdac042a54c5419f8d673
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 17:20:14 +0200

    Migrate File parser interface
    Previous "Parser" from config-loader package

---
+++ src/Contracts/Config/Parsers/Exceptions/FileDoesNotExistException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Contracts\Config\Parsers\Exceptions;
+
+/**
+ * File Does Not Exist Exception
+ *
+ * <br />
+ *
+ * Throw this exception whenever a file does not exist
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Config\Parsers\Exceptions
+ */
+interface FileDoesNotExistException extends FileParserException
+{
+
+}
---
+++ src/Contracts/Config/Parsers/Exceptions/FileParserException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aedart\Contracts\Config\Parsers\Exceptions;
+
+use Throwable;
+
+/**
+ * File Parser Exception
+ *
+ * <br />
+ *
+ * General File parser exception.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Config\Parsers\Exceptions
+ */
+interface FileParserException extends Throwable
+{
+
+}
