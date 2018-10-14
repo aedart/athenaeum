6d18f871c07cc5103fa4c12f11bc39d7e9ff0ad2
Alin Eugen Deac <aedart@gmail.com>
2018-10-14 17:30:37 +0200

    Migrate FileParserFactory facade

---
+++ src/Config/Facades/FileParserFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Aedart\Config\Facades;
+
+use Aedart\Contracts\Config\Parsers\Factories\FileParserFactory as FileParserFactoryInterface;
+use Aedart\Contracts\Config\Parsers\FileParser;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * File Parser Factory
+ *
+ * @method FileParser make(string $type) Creates a new File Parser instance
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Facades
+ */
+class FileParserFactory extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected static function getFacadeAccessor()
+    {
+        return FileParserFactoryInterface::class;
+    }
+}
