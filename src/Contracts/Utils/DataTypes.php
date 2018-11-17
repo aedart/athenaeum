bf71f3bc38abdf0fc0a8bd73beafaa0d604c3535
Alin Eugen Deac <aedart@gmail.com>
2018-11-17 13:16:40 +0100

    Add DataTypes interface

---
+++ src/Contracts/Utils/DataTypes.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Aedart\Contracts\Utils;
+
+/**
+ * PHP Data Types
+ *
+ * @link http://php.net/manual/en/language.types.intro.php
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Utils
+ */
+interface DataTypes
+{
+    /**
+     * Boolean Type
+     */
+    const BOOL_TYPE = 'bool';
+
+    /**
+     * Integer Type
+     */
+    const INT_TYPE = 'int';
+
+    /**
+     * Float (double) Type
+     */
+    const FLOAT_TYPE = 'float';
+
+    /**
+     * String Type
+     */
+    const STRING_TYPE = 'string';
+
+    /**
+     * Scalar Types
+     */
+    const SCALAR_TYPES = [
+        self::BOOL_TYPE,
+        self::INT_TYPE,
+        self::FLOAT_TYPE,
+        self::STRING_TYPE
+    ];
+
+    // -------------------------------------------------------------- //
+
+    /**
+     * Array Type
+     */
+    const ARRAY_TYPE = 'array';
+
+    /**
+     * Object Type
+     */
+    const OBJECT_TYPE = 'object';
+
+    /**
+     * Callable Type
+     */
+    const CALLABLE_TYPE = 'callable';
+
+    /**
+     * Iterable Type
+     */
+    const ITERABLE_TYPE = 'iterable';
+
+    /**
+     * Compound Types
+     */
+    const COMPOUND_TYPES = [
+        self::ARRAY_TYPE,
+        self::OBJECT_TYPE,
+        self::CALLABLE_TYPE,
+        self::ITERABLE_TYPE
+    ];
+
+    // -------------------------------------------------------------- //
+
+    /**
+     * Resource Type
+     */
+    const RESOURCE_TYPE = 'resource';
+
+    /**
+     * Null Type
+     */
+    const NULL_TYPE = 'null';
+
+    /**
+     * Special Types
+     */
+    const SPECIAL_TYPES = [
+        self::RESOURCE_TYPE,
+        self::NULL_TYPE
+    ];
+}
