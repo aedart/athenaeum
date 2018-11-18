d4f51cf04325ea1bfafe8de752971cbe9384ee21
Alin Eugen Deac <aedart@gmail.com>
2018-11-18 17:33:46 +0100

    Add test for create aware-of command

---
+++ tests/Integration/Console/CreateAwareOfCommandTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Aedart\Tests\Integration\Console;
+
+use Aedart\Console\CreateAwareOfCommand;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Testing\TestCases\IntegrationTestCase;
+use Codeception\Configuration;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * CreateAwareOfCommandTest
+ *
+ * @group console
+ * @group aware-of
+ * @group aware-of-command
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Console
+ */
+class CreateAwareOfCommandTest extends IntegrationTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function _before()
+    {
+        parent::_before();
+
+        $fs = new Filesystem();
+        $fs->deleteDirectory( Configuration::outputDir() . 'aware-of/');
+    }
+
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Assert the given aware of component exists
+     *
+     * @param string $path
+     *
+     * @throws \Codeception\Exception\ConfigurationException
+     */
+    protected function assertAwareOfComponentExists(string $path)
+    {
+        $prefix = Configuration::outputDir() . 'aware-of/';
+
+        // Determine if exists
+        $this->assertFileExists($prefix . $path);
+
+        // Attempt to include component. PHP will fail if something
+        // is off.
+        require $prefix . $path;
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     */
+    public function canCreateAwareOfComponents()
+    {
+        $command = new CreateAwareOfCommand();
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'config' => Configuration::dataDir() . 'configs/aware-of-command/demo.php',
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+
+        ConsoleDebugger::output($output);
+
+        // --------------------------------------------------------------- //
+
+        $this->assertContains('[OK]', $output);
+
+        $this->assertAwareOfComponentExists('Contracts/Arrays/CategoriesAware.php');
+        $this->assertAwareOfComponentExists('Traits/Arrays/CategoriesTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Booleans/IsDestroyedAware.php');
+        $this->assertAwareOfComponentExists('Traits/Booleans/IsDestroyedTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Callables/CallbackAware.php');
+        $this->assertAwareOfComponentExists('Traits/Callables/CallbackTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Floats/PriceAware.php');
+        $this->assertAwareOfComponentExists('Traits/Floats/PriceTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Integers/AgeAware.php');
+        $this->assertAwareOfComponentExists('Traits/Integers/AgeTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Iterators/PersonsAware.php');
+        $this->assertAwareOfComponentExists('Traits/Iterators/PersonsTrait.php');
+
+        $this->assertAwareOfComponentExists('Contracts/Strings/NameAware.php');
+        $this->assertAwareOfComponentExists('Traits/Strings/NameTrait.php');
+    }
+}
---
+++ tests/_data/configs/aware-of-command/demo.php
@@ -0,0 +1,105 @@
+<?php
+
+use Aedart\Contracts\Utils\DataTypes;
+
+return [
+    /*****************************************************************
+     * Aware-Of Properties Generator Configuration
+     ****************************************************************/
+
+    /*
+     * Author and email
+     */
+    'author'    => 'John Doe',
+    'email'     => 'john.doe@example.org',
+
+    /*
+     * Location where files are to be generated
+     *
+     * E.g. Psr-4 namespace location
+     */
+    'output'    => 'tests/_output/aware-of/',
+
+    /*****************************************************************
+     * Templates location
+     ****************************************************************/
+
+    /*
+     * If none is given, then default twig templates are used
+     *
+     * @var string|array
+     */
+    //'templates-path' => '',
+
+    /*****************************************************************
+     * Namespaces to use
+     ****************************************************************/
+
+    'namespaces' => [
+
+        /*
+         * Vendor namespace to apply on all generated
+         * namespaces.
+         */
+        'vendor' => 'Acme\\',
+
+        /*
+         * Namespaces for interfaces
+         */
+        'interfaces' => [
+
+            /*
+             * Prefix for all interfaces
+             */
+            'prefix'  => 'Contracts\\',
+
+            /*
+             * Namespaces for various data types
+             */
+            DataTypes::STRING_TYPE      => 'Strings\\',
+            DataTypes::INT_TYPE         => 'Integers\\',
+            DataTypes::FLOAT_TYPE       => 'Floats\\',
+            DataTypes::BOOL_TYPE        => 'Booleans\\',
+            DataTypes::ARRAY_TYPE       => 'Arrays\\',
+            DataTypes::CALLABLE_TYPE    => 'Callables\\',
+            DataTypes::ITERABLE_TYPE    => 'Iterators\\',
+        ],
+
+        /*
+         * Namespaces for interfaces
+         */
+        'traits' => [
+
+            /*
+             * Prefix for all traits
+             */
+            'prefix'  => 'Traits\\',
+
+            /*
+             * Namespaces for various data types
+             */
+            DataTypes::STRING_TYPE      => 'Strings\\',
+            DataTypes::INT_TYPE         => 'Integers\\',
+            DataTypes::FLOAT_TYPE       => 'Floats\\',
+            DataTypes::BOOL_TYPE        => 'Booleans\\',
+            DataTypes::ARRAY_TYPE       => 'Arrays\\',
+            DataTypes::CALLABLE_TYPE    => 'Callables\\',
+            DataTypes::ITERABLE_TYPE    => 'Iterators\\',
+        ],
+    ],
+
+    /*****************************************************************
+     * List of Aware-Of Components to generate
+     ****************************************************************/
+
+    'aware-of-properties' => [
+
+        stringProperty('name', 'Name of a person', 'name'),
+        integerProperty('age', 'Age of a person', 'years'),
+        floatProperty('price', 'Price of something', 'price'),
+        booleanProperty('is destroyed', 'State of a player', 'state'),
+        arrayProperty('categories', 'List of categories', 'categories'),
+        callableProperty('callback', 'A callback method', 'callback'),
+        iterableProperty('persons', 'List of persons', 'persons'),
+    ]
+];
