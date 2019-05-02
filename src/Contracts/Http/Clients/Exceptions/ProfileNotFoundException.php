f48e5bfa8bb80f818989fe20a5b91a1629e07591
Alin Eugen Deac <aedart@gmail.com>
2019-05-02 19:25:10 +0200

    Add profile not found exception

---
+++ src/Contracts/Http/Clients/Exceptions/ProfileNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Contracts\Http\Clients\Exceptions;
+
+use Throwable;
+
+/**
+ * Profile Not Found Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients\Exceptions
+ */
+interface ProfileNotFoundException extends Throwable
+{
+
+}
