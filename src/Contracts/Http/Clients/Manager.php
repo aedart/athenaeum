d3c646677b2680ae5567d401a4b2b744df1da363
Alin Eugen Deac <aedart@gmail.com>
2019-05-02 19:26:01 +0200

    Change interface, add manager api methods

--- src/Contracts/Http/Clients/Manager.php
+++ src/Contracts/Http/Clients/Manager.php
@@ -2,8 +2,25 @@
 
 namespace Aedart\Contracts\Http\Clients;
 
+use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
 
+/**
+ * Http Clients Manager
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients
+ */
 interface Manager
 {
-
+    /**
+     * Create or obtain a Http Client
+     *
+     * @param string|null $profile [optional] Name of Http Client profile to obtain or create
+     * @param array $options [optional] Http Client options
+     *
+     * @return Client
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function profile(?string $profile = null, array $options = []) : Client ;
 }
