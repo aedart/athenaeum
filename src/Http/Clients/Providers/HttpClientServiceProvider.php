3a7afcb30167b0eab8e1a2cc1ee5faf095d241ce
Alin Eugen Deac <aedart@gmail.com>
2019-05-02 20:37:15 +0200

    Add service provider

---
+++ src/Http/Clients/Providers/HttpClientServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Aedart\Http\Clients\Providers;
+
+use Aedart\Contracts\Http\Clients\Manager as HttpClientsManager;
+use Aedart\Http\Clients\Manager;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Http Client Service Provider
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Providers
+ */
+class HttpClientServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public $singletons = [
+        HttpClientsManager::class => Manager::class
+    ];
+
+    /**
+     * Bootstrap this service
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../../../configs/http-clients.php' => config_path('http-clients.php')
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provides()
+    {
+        return [ HttpClientsManager::class ];
+    }
+}
