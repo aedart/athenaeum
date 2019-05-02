893cab65f9e271dee16b0e88c45f19a0e24ba9a0
Alin Eugen Deac <aedart@gmail.com>
2019-05-02 20:06:53 +0200

    Add profile not found exception

---
+++ src/Http/Clients/Exceptions/ProfileNotFound.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Http\Clients\Exceptions;
+
+use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
+
+/**
+ * Profile Not Found
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Exceptions
+ */
+class ProfileNotFound extends \RuntimeException implements ProfileNotFoundException
+{
+
+}
