3860037e6dca989dc63ec0bd38e6766fd1bb096c
Alin Eugen Deac <aedart@gmail.com>
2019-05-04 17:11:49 +0200

    Change options description for profiles

--- configs/http-clients.php
+++ configs/http-clients.php
@@ -17,7 +17,11 @@ return [
      | Http Client Profiles
      |--------------------------------------------------------------------------
      |
-     | List of available Http Client "profiles".
+     | List of available Http Client "profiles". Each has a driver, which is a
+     | class path to an actual client that must be used. All Guzzle Http Client
+     | are supported, by the default client(s).
+     |
+     | See http://docs.guzzlephp.org/en/stable/request-options.html
     */
 
     'profiles' => [
