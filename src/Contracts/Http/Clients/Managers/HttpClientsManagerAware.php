9106184d3026b20c78ac6c14cf927dc1c8d38e6d
Alin Eugen Deac <aedart@gmail.com>
2019-05-04 16:32:31 +0200

    Add http clients manager aware component

---
+++ src/Contracts/Http/Clients/Managers/HttpClientsManagerAware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aedart\Contracts\Http\Clients\Managers;
+
+use Aedart\Contracts\Http\Clients\Manager;
+
+/**
+ * Http Clients Manager
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Traits
+ */
+interface HttpClientsManagerAware
+{
+    /**
+     * Set http clients manager
+     *
+     * @param Manager|null $manager Http Clients Manager
+     *
+     * @return self
+     */
+    public function setHttpClientsManager(?Manager $manager);
+
+    /**
+     * Get http clients manager
+     *
+     * If no http clients manager has been set, this method will
+     * set and return a default http clients manager, if any such
+     * value is available
+     *
+     * @return Manager|null http clients manager or null if none http clients manager has been set
+     */
+    public function getHttpClientsManager(): ?Manager;
+
+    /**
+     * Check if http clients manager has been set
+     *
+     * @return bool True if http clients manager has been set, false if not
+     */
+    public function hasHttpClientsManager(): bool;
+
+    /**
+     * Get a default http clients manager value, if any is available
+     *
+     * @return Manager|null A default http clients manager value or Null if no default value is available
+     */
+    public function getDefaultHttpClientsManager(): ?Manager;
+}
---
+++ src/Http/Clients/Traits/HttpClientsManagerTrait.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Aedart\Http\Clients\Traits;
+
+use Aedart\Container\IoC;
+use Aedart\Contracts\Http\Clients\Manager;
+
+/**
+ * Http Clients Manager Trait
+ *
+ * @see \Aedart\Contracts\Http\Clients\Managers\HttpClientsManagerAware
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Traits
+ */
+trait HttpClientsManagerTrait
+{
+    /**
+     * Http Clients Manager
+     *
+     * @var Manager|null
+     */
+    protected $httpClientsManager = null;
+
+    /**
+     * Set http clients manager
+     *
+     * @param Manager|null $manager Http Clients Manager
+     *
+     * @return self
+     */
+    public function setHttpClientsManager(?Manager $manager)
+    {
+        $this->httpClientsManager = $manager;
+
+        return $this;
+    }
+
+    /**
+     * Get http clients manager
+     *
+     * If no http clients manager has been set, this method will
+     * set and return a default http clients manager, if any such
+     * value is available
+     *
+     * @return Manager|null http clients manager or null if none http clients manager has been set
+     */
+    public function getHttpClientsManager(): ?Manager
+    {
+        if (!$this->hasHttpClientsManager()) {
+            $this->setHttpClientsManager($this->getDefaultHttpClientsManager());
+        }
+        return $this->httpClientsManager;
+    }
+
+    /**
+     * Check if http clients manager has been set
+     *
+     * @return bool True if http clients manager has been set, false if not
+     */
+    public function hasHttpClientsManager(): bool
+    {
+        return isset($this->httpClientsManager);
+    }
+
+    /**
+     * Get a default http clients manager value, if any is available
+     *
+     * @return Manager|null A default http clients manager value or Null if no default value is available
+     */
+    public function getDefaultHttpClientsManager(): ?Manager
+    {
+        $container = IoC::getInstance();
+        if($container->has(Manager::class)){
+            return $container->make(Manager::class);
+        }
+
+        return null;
+    }
+}
