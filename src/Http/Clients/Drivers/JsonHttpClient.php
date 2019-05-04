4ed3de67ebc275464b51a2e71c019c87d8169ebc
Alin Eugen Deac <aedart@gmail.com>
2019-05-04 17:17:55 +0200

    Add Json Http Client driver

---
+++ src/Http/Clients/Drivers/JsonHttpClient.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Aedart\Http\Clients\Drivers;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Json Http Client
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Drivers
+ */
+class JsonHttpClient extends DefaultHttpClient
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get($uri): ResponseInterface
+    {
+        return $this->request('GET', $uri, [ 'json' => [] ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function post($uri, array $body = []): ResponseInterface
+    {
+        return $this->request('POST', $uri, [ 'json' => $body ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function put($uri, array $body = []): ResponseInterface
+    {
+        return $this->request('PUT', $uri, [ 'json' => $body ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($uri, array $body = []): ResponseInterface
+    {
+        return $this->request('DELETE', $uri, [ 'json' => $body ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function patch($uri, array $body = []): ResponseInterface
+    {
+        return $this->request('PATCH', $uri, [ 'json' => $body ]);
+    }
+}
