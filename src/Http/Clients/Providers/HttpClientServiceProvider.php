282aad2a2870a135938fc6b6b8f5fed91b152b8c
Alin Eugen Deac <aedart@gmail.com>
2019-05-05 11:07:28 +0200

    Add config tag to service's resources

--- src/Http/Clients/Providers/HttpClientServiceProvider.php
+++ src/Http/Clients/Providers/HttpClientServiceProvider.php
@@ -26,7 +26,7 @@ class HttpClientServiceProvider extends ServiceProvider implements DeferrablePro
     {
         $this->publishes([
             __DIR__ . '/../../../../configs/http-clients.php' => config_path('http-clients.php')
-        ]);
+        ], 'config');
     }
 
     /**
