0c049ba11c9a93f59207d3a1ef4de8c6bed7fc6c
Alin Eugen Deac <aedart@gmail.com>
2019-12-08 16:22:01 +0100

    Use IoC Facade to resolve default

--- src/Http/Clients/Traits/HttpClientsManagerTrait.php
+++ src/Http/Clients/Traits/HttpClientsManagerTrait.php
@@ -2,6 +2,7 @@
 
 namespace Aedart\Http\Clients\Traits;
 
+use Aedart\Container\Facades\IoCFacade;
 use Aedart\Container\IoC;
 use Aedart\Contracts\Http\Clients\Manager;
 
@@ -70,11 +71,6 @@ trait HttpClientsManagerTrait
      */
     public function getDefaultHttpClientsManager(): ?Manager
     {
-        $container = IoC::getInstance();
-        if($container->has(Manager::class)){
-            return $container->make(Manager::class);
-        }
-
-        return null;
+        return IoCFacade::make(Manager::class);
     }
 }
