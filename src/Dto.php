6943029288a76e3660de828317711d97c6b1b822
Alin Eugen Deac <aedart@gmail.com>
2019-12-08 10:10:33 +0100

    Replace root level Dto classes
    At the off chance that this package is ever going to be converted into a true mono-repository with multiple smaller packages, then the Dto abstractions need to be within their own separate namespace. It might not be that fancy naming, but will not hinder future possibilities

--- src/Dto.php
+++ src/Dto.php
@@ -2,99 +2,17 @@
 
 namespace Aedart;
 
-use Aedart\Contracts\Dto as DtoInterface;
-use Aedart\Dto\Partials\DtoPartial;
-use Aedart\Dto\Partials\IoCPartial;
-use Aedart\Properties\Overload;
-use Aedart\Utils\Helpers\MethodHelper;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Contracts\Container\Container;
-use ReflectionClass;
-use ReflectionException;
-use Throwable;
+use Aedart\Dto\Dto as DtoBase;
 
 /**
- * Abstract Data Transfer Object
+ * @deprecated Since v4.0, Replaced by Aedart\Dto\Dto
  *
- * <br />
- *
- * This DTO abstraction offers default implementation of the following;
- *
- * <ul>
- *      <li>Overloading of properties, if they have getters and setters defined</li>
- *      <li>Array-accessibility of properties, if properties have getters and setters defined</li>
- *      <li>Population of properties via array</li>
- *      <li>Resolving nested dependencies, via a IoC service container, if one is available</li>
- *      <li>Exportation of properties to an array</li>
- *      <li>Serialization of properties to json</li>
- * </ul>
- *
- * @see \Aedart\Contracts\Dto
+ * @see \Aedart\Dto\Dto
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart
  */
-abstract class Dto implements DtoInterface
+abstract class Dto extends DtoBase
 {
-    use Overload {
-        __set as __setFromOverload;
-    }
-    use IoCPartial;
-    use DtoPartial;
 
-    /**
-     * Dto constructor.
-     *
-     * @param array $properties [optional]
-     * @param Container|null $container [optional]
-     *
-     * @throws Throwable
-     */
-    public function __construct(array $properties = [], ?Container $container = null)
-    {
-        $this
-            ->setContainer($container)
-            ->populate($properties);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @throws ReflectionException
-     * @throws BindingResolutionException
-     * @throws Throwable
-     */
-    public function __set(string $name, $value)
-    {
-        $resolvedValue = $value;
-
-        $methodName = MethodHelper::makeSetterName($name);
-        if ($this->hasInternalMethod($methodName)) {
-            $resolvedValue = $this->resolveValue($methodName, $value);
-        }
-
-        return $this->__setFromOverload($name, $resolvedValue);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @throws ReflectionException
-     */
-    public function populatableProperties(): array
-    {
-        $reflection = new ReflectionClass($this);
-        $properties = $reflection->getProperties();
-
-        $output = [];
-        foreach ($properties as $reflectionProperty){
-            $name = $reflectionProperty->getName();
-            $method = MethodHelper::makeGetterName($name);
-
-            if($this->hasInternalMethod($method)){
-                $output[] = $name;
-            }
-        }
-        return $output;
-    }
 }
