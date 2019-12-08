1b5ad5d7b152fc6f71ff506adea72eec1829a1a7
Alin Eugen Deac <aedart@gmail.com>
2019-12-08 16:06:51 +0100

    Add facade test

---
+++ tests/Integration/Container/IoCFacadeTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Aedart\Tests\Integration\Container;
+
+use Aedart\Container\Facades\IoCFacade;
+use Aedart\Testing\TestCases\IntegrationTestCase;
+use Aedart\Tests\Helpers\Dummies\Box;
+
+/**
+ * IoCFacadeTest
+ *
+ * @group ioc
+ * @group container
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Container
+ */
+class IoCFacadeTest extends IntegrationTestCase
+{
+    /**
+     * @test
+     */
+    public function canResolveBinding()
+    {
+        $abstract = 'my_binding';
+        $this->ioc->bind($abstract, fn() => new Box());
+
+        // -------------------------------------------- //
+
+        $resolved = IoCFacade::make($abstract);
+
+        $this->assertInstanceOf(Box::class, $resolved, 'unable to resolve binding');
+    }
+
+    /**
+     * @test
+     */
+    public function returnsDefaultWhenBindingDoesNotExist()
+    {
+        $abstract = 'my_binding';
+        $default = 'nothing there...';
+
+        $resolved = IoCFacade::make($abstract, $default);
+
+        $this->assertSame($default, $resolved, 'incorrect default resolved');
+    }
+}
