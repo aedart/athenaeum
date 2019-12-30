9c34617c28eaa1e89ba9d083e733ed803d6b7a38
Alin Eugen Deac <aedart@gmail.com>
2019-12-30 14:55:14 +0100

    Add paths helper
    This is only required if running outside a typical Larvel application (which is also the intent).

---
+++ helpers/paths.php
@@ -0,0 +1,132 @@
+<?php
+
+use Aedart\Container\Facades\IoCFacade;
+use Aedart\Contracts\Core\Helpers\PathsContainer;
+use Illuminate\Contracts\Foundation\Application;
+
+if( ! function_exists('paths')){
+
+    /**
+     * Get he paths container
+     *
+     * @return mixed|PathsContainer|Application
+     */
+    function paths() : ?PathsContainer
+    {
+        // Try to resolve the Paths Container if possible. If not,
+        // try to default to Laravel's application. This is in case
+        // that this file somehow is loaded first within a full
+        // Laravel application and thus might otherwise cause a
+        // conflict with the path-helpers defined within Laravel's
+        // foundation package.
+        return IoCFacade::tryMake(
+            PathsContainer::class,
+            IoCFacade::tryMake(Application::class)
+        );
+    }
+}
+
+if( ! function_exists('base_path')){
+
+    /**
+     * Get a path within the "base" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function base_path(string $path = '') : string
+    {
+        return paths()->basePath($path);
+    }
+}
+
+if( ! function_exists('bootstrap_path')){
+
+    /**
+     * Get a path within the "bootstrap" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function bootstrap_path(string $path = '') : string
+    {
+        return paths()->bootstrapPath($path);
+    }
+}
+
+if( ! function_exists('config_path')){
+
+    /**
+     * Get a path within the "config" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function config_path(string $path = '') : string
+    {
+        return paths()->configPath($path);
+    }
+}
+
+if( ! function_exists('database_path')){
+
+    /**
+     * Get a path with the "database" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function database_path(string $path = '') : string
+    {
+        return paths()->databasePath($path);
+    }
+}
+
+if( ! function_exists('environment_path')){
+
+    /**
+     * Get a path within the "environment" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function environment_path(string $path = '') : string
+    {
+        return paths()->environmentPath($path);
+    }
+}
+
+if( ! function_exists('resource_path')){
+
+    /**
+     * Get a path within the "resources" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function resource_path(string $path = '') : string
+    {
+        return paths()->resourcePath($path);
+    }
+}
+
+if( ! function_exists('storage_path')){
+
+    /**
+     * Get a path within the "storage" directory
+     *
+     * @param string $path [optional]
+     *
+     * @return string
+     */
+    function storage_path(string $path = '') : string
+    {
+        return paths()->storagePath($path);
+    }
+}
