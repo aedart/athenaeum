47404a0a66575a427ddddf7539965a09f901849b
Alin Eugen Deac <aedart@gmail.com>
2019-12-30 15:06:22 +0100

    Change provider, publish config and register listeners
    Provider will now register listeners and subscribers that are defined in the "events" configuration.

--- src/Events/Providers/EventServiceProvider.php
+++ src/Events/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Aedart\Events\Providers;
 
+use Aedart\Support\Helpers\Config\ConfigTrait;
+use Aedart\Support\Helpers\Events\DispatcherTrait;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherInterface;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Events\EventServiceProvider as LaravelEventServiceProvider;
@@ -14,6 +16,9 @@ use Illuminate\Events\EventServiceProvider as LaravelEventServiceProvider;
  */
 class EventServiceProvider extends LaravelEventServiceProvider
 {
+    use ConfigTrait;
+    use DispatcherTrait;
+
     /**
      * @inheritdoc
      */
@@ -23,8 +28,70 @@ class EventServiceProvider extends LaravelEventServiceProvider
 
         // Register aliases
         $key = 'events';
-
         $this->app->alias($key, Dispatcher::class);
         $this->app->alias($key, DispatcherInterface::class);
+    }
+
+    /**
+     * Boot this service
+     */
+    public function boot()
+    {
+        $this
+            ->publishConfiguration()
+            ->registerEventListener()
+            ->registerSubscribers();
+    }
+
+    /*****************************************************************
+     * Internals
+     ****************************************************************/
+
+    /**
+     * Register event listeners that are defined in configuration
+     *
+     * @return self
+     */
+    protected function registerEventListener()
+    {
+        $listeners = $this->getConfig()->get('events.listeners', []);
+        $dispatcher = $this->getDispatcher();
+
+        foreach ($listeners as $event => $listener){
+            $dispatcher->listen($event, $listener);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Register event subscribers that are defined in configuration
+     *
+     * @return self
+     */
+    protected function registerSubscribers()
+    {
+        $subscribers = $this->getConfig()->get('events.subscribers', []);
+        $dispatcher = $this->getDispatcher();
+
+        foreach ($subscribers as $subscriber){
+            $dispatcher->subscribe($subscriber);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Register configuration to be published
+     *
+     * @return self
+     */
+    protected function publishConfiguration()
+    {
+        $this->publishes([
+            __DIR__ . '/../../../configs/events.php' => config_path('events.php')
+        ], 'config');
+
+        return $this;
     }
 }
