912749b2bc1a35518860868b6fcfea38b327eed6
Alin Eugen Deac <aedart@gmail.com>
2020-01-04 14:03:05 +0100

    Add custom applications for testing exception handling
    Added a few custom application that can / should be invoked as external processes, each showing that error, exception or shutdown failure handling is performed correct.

---
+++ tests/_data/exceptions/handles-shutdown.php
@@ -0,0 +1,17 @@
+<?php
+
+/*****************************************************************
+ * FOR TESTING ONLY
+ *
+ * This application runs and logs a single entry.
+ ****************************************************************/
+
+/** @var \Aedart\Contracts\Core\Application $app */
+$app = require_once 'bootstrap.php';
+
+$app->run();
+
+// Easiest way to trigger a failure during shutdown, is to trigger an error,
+// whilst suppressing it's output. This will be picked up by the "error_get_last()"
+// method...
+@trigger_error('Shutdown error captured', E_USER_ERROR);
