6945a6980aed6b5160b74e9092b540360a9c77fb
Alin Eugen Deac <aedart@gmail.com>
2020-01-23 21:25:16 +0100

    Deprecate create command

--- src/Console/CreateAwareOfCommand.php
+++ src/Console/CreateAwareOfCommand.php
@@ -2,13 +2,7 @@
 
 namespace Aedart\Console;
 
-use Aedart\Support\AwareOf\Documenter;
-use Aedart\Support\AwareOf\Generator;
-use Aedart\Support\Helpers\Config\ConfigTrait;
-use Illuminate\Config\Repository;
-use InvalidArgumentException;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
+use Aedart\Support\AwareOf\Console\CreateCommand;
 
 /**
  * Create Aware Of Properties Command
@@ -16,24 +10,8 @@ use Symfony\Component\Console\Input\InputOption;
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Console
  */
-class CreateAwareOfCommand extends CommandBase
+class CreateAwareOfCommand extends CreateCommand
 {
-    use ConfigTrait;
-
-    /**
-     * The "aware-of" generator
-     *
-     * @var Generator
-     */
-    protected Generator $generator;
-
-    /**
-     * The "aware-of" documenter
-     *
-     * @var Documenter
-     */
-    protected Documenter $documenter;
-
     /*****************************************************************
      * Command Configuration
      ****************************************************************/
@@ -43,12 +21,11 @@ class CreateAwareOfCommand extends CommandBase
      */
     protected function configure()
     {
+        parent::configure();
+
         $this
             ->setName('dto:create-aware-of')
-            ->setDescription('Generates a series of aware-of components, based on given configuration')
-            ->addArgument('config', InputArgument::REQUIRED, 'Path to php configuration file')
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force overwrite existing aware-of components')
-            ->setHelp($this->formatHelp());
+            ->setDescription('DEPRECATED - use aware-of create command instead');
     }
 
     /*****************************************************************
@@ -60,173 +37,8 @@ class CreateAwareOfCommand extends CommandBase
      */
     public function runCommand(): ?int
     {
-        $this->output->title('Creating Aware-Of Properties');
+        trigger_error('Deprecated since v4.0 - use use aware-of scaffold command instead', E_USER_DEPRECATED);
 
-        $this
-            ->loadConfiguration()
-            ->setupGenerator()
-            ->setupDocumenter()
-            ->build();
-
-        $this->output->success('done');
-
-        return 0;
-    }
-
-    /*****************************************************************
-     * Internals
-     ****************************************************************/
-
-    /**
-     * Builds the aware-of components
-     *
-     * @throws \Throwable
-     */
-    protected function build()
-    {
-        // Get list of components to build
-        $list = $this->getConfig()->get('aware-of-properties', []);
-
-        // Create new process bar
-        $this->output->progressStart(count($list) + 1);
-
-        // Build each aware-of component
-        $awareOfComponents = [];
-        foreach ($list as $component){
-            $result = $this->buildAwareOfComponent($component);
-            $awareOfComponents[] = $result;
-
-            // Debug
-            $this->debug($result);
-
-            $this->output->progressAdvance();
-        }
-
-        // Build markdown doc(s)
-        $this->buildDocs($awareOfComponents);
-        $this->output->progressAdvance();
-
-        // Complete the progress bar
-        $this->output->progressFinish();
-    }
-
-    /**
-     * Generates the given "aware-of" component
-     *
-     * @param array $component
-     *
-     * @throws \Throwable
-     *
-     * @return array
-     */
-    protected function buildAwareOfComponent(array $component) : array
-    {
-        // Get the force flag, if set
-        $force = $this->input->getOption('force');
-
-        return $this->generator->generate($component, $force);
-    }
-
-    /**
-     * Generates markdown documentation for given "aware-of" components.
-     *
-     * NOTE: Only if "docs-output" config set !
-     *
-     * @param array $awareOfComponents [optional]
-     *
-     * @throws \Throwable
-     */
-    protected function buildDocs(array $awareOfComponents = [])
-    {
-        // Get the force flag, if set
-        $force = $this->input->getOption('force');
-
-        $this->documenter->makeDocs($awareOfComponents, $force);
-    }
-
-    /**
-     * Load the configuration
-     *
-     * @return self
-     *
-     * @throws InvalidArgumentException If path is invalid
-     */
-    protected function loadConfiguration()
-    {
-        $path = $this->input->getArgument('config');
-        if( ! file_exists($path)){
-            throw new InvalidArgumentException($path . ' is an invalid path. Configuration file does not exist');
-        }
-
-        // Load the configuration file
-        /** @var array $content */
-        $content = require $path;
-
-        // Set repository
-        $this->setConfig(new Repository($content));
-
-        return $this;
-    }
-
-    /**
-     * Setup the "aware-of" generator
-     *
-     * @return self
-     */
-    protected function setupGenerator()
-    {
-        $this->generator = new Generator($this->getConfig());
-
-        return $this;
-    }
-
-    /**
-     * Setup the "aware-of" documenter
-     *
-     * @return self
-     */
-    protected function setupDocumenter()
-    {
-        $this->documenter = new Documenter($this->getConfig());
-
-        return $this;
-    }
-
-    /**
-     * Outputs given component, if verbose
-     *
-     * @param array $awareOfComponentData
-     */
-    protected function debug(array $awareOfComponentData = [])
-    {
-        if($this->output->isVerbose()){
-            $this->output->newLine();
-            $this->output->text(var_export($awareOfComponentData, true));
-            $this->output->newLine();
-        }
-    }
-
-    /**
-     * Formats and returns this commands help text
-     *
-     * @return string
-     */
-    protected function formatHelp()
-    {
-        return <<<EOT
-Generates a series of aware-of components, based on given configuration
-
-Usage:
-
-<info>php athenaeum dto:create-aware-of my-dto-list.php</info>
-
-Force Flag:
-
-If you set the force flag, then all existing interfaces and traits are overwritten.
-This also applies to the generated markdown documentation!
-
-<info>php athenaeum dto:create-aware-of aware-of-properties.php --force</info>
-
-EOT;
+        return parent::runCommand();
     }
 }
