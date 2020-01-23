4d22f8d2b14eb037ea4b0ee07ad0eb48993b405b
Alin Eugen Deac <aedart@gmail.com>
2020-01-23 20:29:51 +0100

    Migrate command base abstract
    Originally from console namespace

---
+++ packages/Support/src/AwareOf/Console/CommandBase.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Aedart\Support\AwareOf\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Command Base
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Support\AwareOf\Console
+ */
+abstract class CommandBase extends Command
+{
+    /**
+     * This command's input
+     *
+     * @var InputInterface
+     */
+    protected InputInterface $input;
+
+    /**
+     * This command's output
+     *
+     * @var OutputInterface|StyleInterface
+     */
+    protected $output;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Throwable
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Set input
+        $this->input = $input;
+
+        // Resolve output
+        if($output instanceof StyleInterface){
+            $this->output = $output;
+        } else {
+            $this->output = new SymfonyStyle($this->input, $output);
+        }
+
+        // Finally, run the command
+        return $this->runCommand();
+    }
+
+    /*****************************************************************
+     * Abstract Methods
+     ****************************************************************/
+
+    /**
+     * Execute this command
+     *
+     * @throws \Throwable
+     *
+     * @return int|null
+     */
+    abstract public function runCommand() : ?int ;
+}
