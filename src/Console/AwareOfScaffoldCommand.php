d18c0e7d3a275e5dcdb5ba69e2a0c32c1a92d539
Alin Eugen Deac <aedart@gmail.com>
2020-01-23 20:48:38 +0100

    Deprecate aware of command
    Replaced with new scaffold command

--- src/Console/AwareOfScaffoldCommand.php
+++ src/Console/AwareOfScaffoldCommand.php
@@ -2,17 +2,17 @@
 
 namespace Aedart\Console;
 
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Str;
-use Symfony\Component\Console\Input\InputOption;
+use Aedart\Support\AwareOf\Console\ScaffoldCommand;
 
 /**
+ * @deprecated Since version 4.0 - use \Aedart\Support\AwareOf\Console\ScaffoldCommand
+ *
  * Aware Of Scaffold Command
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Console
  */
-class AwareOfScaffoldCommand extends CommandBase
+class AwareOfScaffoldCommand extends ScaffoldCommand
 {
     /*****************************************************************
      * Command Configuration
@@ -23,11 +23,11 @@ class AwareOfScaffoldCommand extends CommandBase
      */
     protected function configure()
     {
+        parent::configure();
+
         $this
             ->setName('dto:scaffold')
-            ->setDescription('Creates a configuration file that can e used by the "dto:create-aware-of" command')
-            ->addOption('output', 'o', InputOption::VALUE_OPTIONAL, 'Directory where to create configuration file', getcwd())
-            ->setHelp($this->formatHelp());
+            ->setDescription('DEPRECATED - use aware-of scaffold command instead');
     }
 
     /*****************************************************************
@@ -39,55 +39,8 @@ class AwareOfScaffoldCommand extends CommandBase
      */
     public function runCommand(): ?int
     {
-        $fs = new Filesystem();
-        $target = __DIR__ . '/../../configs/aware-of-properties.php';
-        $output = $this->input->getOption('output');
+        trigger_error('Deprecated since v4.0 - use use aware-of scaffold command instead', E_USER_DEPRECATED);
 
-        // Add trailing slash if needed
-        if( ! Str::endsWith($output, DIRECTORY_SEPARATOR)){
-            $output .= DIRECTORY_SEPARATOR;
-        }
-
-        // Abort if config file already exists
-        $destination = $output . 'aware-of-properties.php';
-        if($fs->exists($destination)){
-            $this->output->warning($destination . ' already exists. Aborting!');
-            return 1;
-        }
-
-        // Create nested directories, if required
-        $fs->makeDirectory($output, 0755, true);
-
-        // Copy the configuration file
-        $fs->copy($target, $destination);
-        $this->output->success('Created ' . $destination);
-
-        return 0;
-    }
-
-    /*****************************************************************
-     * Internals
-     ****************************************************************/
-
-    /**
-     * Formats and returns this commands help text
-     *
-     * @return string
-     */
-    protected function formatHelp()
-    {
-        return <<<EOT
-Creates a configuration file that can e used by the "dto:create-aware-of" command
-
-Usage:
-
-<info>php athenaeum dto:scaffold</info>
-
-You can optionally also specify the output directory where the configuration file
-should be created.
-
-<info>php athenaeum dto:scaffold --output configs/dto</info>
-
-EOT;
+        return parent::runCommand();
     }
 }
