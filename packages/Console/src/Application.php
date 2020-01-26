4c5ae711fabc7b63ef392ffc50f09ac765ac15b9
Alin Eugen Deac <aedart@gmail.com>
2020-01-26 18:42:28 +0100

    Add adapted Laravel Console Application

---
+++ packages/Console/src/Application.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Aedart\Console;
+
+use Aedart\Utils\Version;
+use Illuminate\Console\Application as LaravelConsoleApplication;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+
+/**
+ * Console Application
+ *
+ * Extended version of Laravel's Artisan Console Application
+ *
+ * @see \Illuminate\Console\Application
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Console
+ */
+class Application extends LaravelConsoleApplication
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct(Container $laravel, Dispatcher $events, $version)
+    {
+        parent::__construct($laravel, $events, $version);
+
+        $this->setName(sprintf('Athenaeum Console Application (via. Laravel Console v.%s)', $this->laravelVersion()));
+    }
+
+    /*****************************************************************
+     * Internals
+     ****************************************************************/
+
+    /**
+     * Returns the current installed Laravel Console package version
+     *
+     * @return string
+     */
+    protected function laravelVersion() : string
+    {
+        return Version::package('illuminate/console');
+    }
+}
