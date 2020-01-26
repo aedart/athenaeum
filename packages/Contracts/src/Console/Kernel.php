96a8cf639d95e40e2f379b7ca600961c5c5d7460
Alin Eugen Deac <aedart@gmail.com>
2020-01-26 18:31:52 +0100

    Change interface, add get / set artisan

--- packages/Contracts/src/Console/Kernel.php
+++ packages/Contracts/src/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Aedart\Contracts\Console;
 
+use Illuminate\Console\Application;
 use Illuminate\Contracts\Console\Kernel as LaravelConsoleKernel;
 
 /**
@@ -19,5 +20,19 @@ use Illuminate\Contracts\Console\Kernel as LaravelConsoleKernel;
 interface Kernel extends LaravelConsoleKernel,
     CoreApplicationAware
 {
+    /**
+     * Set Laravel's Artisan Console Application instance
+     *
+     * @param \Illuminate\Console\Application $artisan
+     *
+     * @return void
+     */
+    public function setArtisan($artisan);
 
+    /**
+     * Returns Laravel's Artisan Console Application instance
+     *
+     * @return \Illuminate\Console\Application
+     */
+    public function getArtisan();
 }
