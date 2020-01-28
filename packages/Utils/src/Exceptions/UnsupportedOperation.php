44d80563bc713e297c3b323871d50d5956b3663d
Alin Eugen Deac <aedart@gmail.com>
2020-01-28 18:56:35 +0100

    Add concrete implementation of unsupported operation exception

---
+++ packages/Utils/src/Exceptions/UnsupportedOperation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Utils\Exceptions;
+
+use Aedart\Contracts\Exceptions\UnsupportedOperationException;
+use LogicException;
+
+/**
+ * Unsupported Operation Exception
+ *
+ * @see \Aedart\Contracts\Exceptions\UnsupportedOperationException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Utils\Exceptions
+ */
+class UnsupportedOperation extends LogicException implements UnsupportedOperationException
+{
+
+}
