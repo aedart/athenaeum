b41883f6963282294d8566cd4cb48e7d3bded59b
Alin Eugen Deac <aedart@gmail.com>
2020-01-28 20:04:38 +0100

    Add example configuration

---
+++ packages/Console/configs/commands.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Commands
+    |--------------------------------------------------------------------------
+    |
+    | List of your application's console commands. State the full class path to
+    | the commands that you require to be available via the console.
+    */
+
+    //\Acme\Console\DatabaseDumpCommand::class,
+    //\Acme\Console\ExportPicturesCommand::class,
+    //\Acme\Console\RestoreUserCommand::class,
+];
