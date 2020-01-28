9deec56b3c2070a81a5362d634831bf0ac7bd67f
Alin Eugen Deac <aedart@gmail.com>
2020-01-28 19:24:03 +0100

    Add console service provider (incomplete)

---
+++ packages/Console/src/Providers/ConsoleServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aedart\Console\Providers;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Console Service Provider
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Console\Providers
+ */
+class ConsoleServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+
+}
