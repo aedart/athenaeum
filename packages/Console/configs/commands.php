e1d993d75e9623bcdcf37034718ebf32918c9abf
Alin Eugen Deac <aedart@gmail.com>
2020-01-31 18:35:00 +0100

    Change example config, add dummy command
    Just for fun :)

--- packages/Console/configs/commands.php
+++ packages/Console/configs/commands.php
@@ -11,6 +11,8 @@ return [
     | the commands that you require to be available via the console.
     */
 
+    \Aedart\Console\Commands\PirateTalkCommand::class,
+
     //\Acme\Console\DatabaseDumpCommand::class,
     //\Acme\Console\ExportPicturesCommand::class,
     //\Acme\Console\RestoreUserCommand::class,
