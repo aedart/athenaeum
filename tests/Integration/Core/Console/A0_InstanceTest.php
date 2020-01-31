45501cd1906e330d9a009dd4a5e5fee2c0ba9450
Alin Eugen Deac <aedart@gmail.com>
2020-01-31 18:43:09 +0100

    Add instance test for core console

---
+++ tests/Integration/Core/Console/A0_InstanceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Aedart\Tests\Integration\Core\Console;
+
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\TestCases\AthenaeumCoreTestCase;
+
+/**
+ * A0_InstanceTest
+ *
+ * @group application
+ * @group application-console
+ * @group application-console-a0
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Core\Console
+ */
+class A0_InstanceTest extends AthenaeumCoreTestCase
+{
+    /**
+     * @test
+     */
+    public function canObtainInstance()
+    {
+        $console = $this->getArtisan();
+
+        ConsoleDebugger::output($console);
+
+        $this->assertNotNull($console);
+    }
+}
