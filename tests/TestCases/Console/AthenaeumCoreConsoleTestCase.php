0f64b80f262f17c2150b93946999e7c4b1322cb5
Alin Eugen Deac <aedart@gmail.com>
2020-01-31 19:25:17 +0100

    Add specialized test-case for core console

---
+++ tests/TestCases/Console/AthenaeumCoreConsoleTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Aedart\Tests\TestCases\Console;
+
+use Aedart\Tests\TestCases\AthenaeumCoreTestCase;
+use Codeception\Configuration;
+
+/**
+ * Athenaeum Core Console Test Case
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\TestCases\Console
+ */
+abstract class AthenaeumCoreConsoleTestCase extends AthenaeumCoreTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * @inheritdoc
+     */
+    protected function applicationPaths(): array
+    {
+        return array_merge(parent::applicationPaths(), [
+            'configPath'    => Configuration::dataDir() . 'configs' . DIRECTORY_SEPARATOR . 'console'
+        ]);
+    }
+}
