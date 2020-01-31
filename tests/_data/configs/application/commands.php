9ff66afa183341104023a5776ea358a66f96254a
Alin Eugen Deac <aedart@gmail.com>
2020-01-31 18:42:56 +0100

    Add testing config for commands

---
+++ tests/_data/configs/application/commands.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Commands
+    |--------------------------------------------------------------------------
+    |
+    | List of your application's console commands. State the full class path to
+    | the commands that you require to be available via the console.
+    */
+
+    \Aedart\Console\Commands\PirateTalkCommand::class,
+
+    //\Acme\Console\DatabaseDumpCommand::class,
+    //\Acme\Console\ExportPicturesCommand::class,
+    //\Acme\Console\RestoreUserCommand::class,
+];
