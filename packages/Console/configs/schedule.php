819cacd1961544a232e833c243326d5c4c84b74e
Alin Eugen Deac <aedart@gmail.com>
2020-02-06 18:07:43 +0100

    Add example schedule config

---
+++ packages/Console/configs/schedule.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Schedule Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Default timezone for your scheduled events. If "null" is provided, then
+    | the timezone will default to your application's timezone.
+    | See app.timezone
+    */
+
+    'timezone' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Schedule Cache
+    |--------------------------------------------------------------------------
+    |
+    | The cache store (driver) to use by scheduled events.
+    */
+
+    'cache' => env('SCHEDULE_CACHE_DRIVER', 'file'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Schedule Tasks
+    |--------------------------------------------------------------------------
+    |
+    | Each provided component defines one ore more tasks that are to be executed
+    | when they are due. Components MUST inherit from the "Schedules Tasks" interface.
+    | @see \Aedart\Contracts\Console\Scheduling\SchedulesTasks
+    */
+
+    'tasks' => [
+
+        // \Acme\Tasks\DefinesCacheCleanupTasks::class,
+        // \Acme\Tasks\DefinesUserTasks::class,
+        // \Acme\Tasks\DefinesPeriodicNotificationTasks::class
+
+    ]
+];
