24919511d237d56017fd8627b41cfe01e22eaa06
Alin Eugen Deac <aedart@gmail.com>
2020-02-06 18:09:34 +0100

    Add schedules tasks interface

---
+++ packages/Contracts/src/Console/Scheduling/SchedulesTasks.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aedart\Contracts\Console\Scheduling;
+
+/**
+ * Schedules Tasks
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Console\Scheduling
+ */
+interface SchedulesTasks
+{
+    /**
+     * Defines tasks to be executed when they are due
+     *
+     * @see https://laravel.com/docs/6.x/scheduling
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     */
+    public function schedule($schedule) : void ;
+}
