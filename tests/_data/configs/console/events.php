99b1aae02dc3da6fdbff65a8fcb72e2ca7beb843
Alin Eugen Deac <aedart@gmail.com>
2020-02-07 17:05:05 +0100

    Add events config for testing
    A side effect of the publish command test... But it shouldn't be a problem,

---
+++ tests/_data/configs/console/events.php
@@ -0,0 +1,38 @@
+<?php
+return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | Events and Listeners
+     |--------------------------------------------------------------------------
+     |
+     | List of events and their associated event listeners.
+    */
+
+    'listeners' => [
+
+        // UserCreated::class => [
+        //     LogNewUser::class,
+        //     SendWelcomeEmail::class,
+        // ],
+        // 'payments.*' => [
+        //     VerifyPaymentSession::class
+        // ],
+
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Subscribers
+     |--------------------------------------------------------------------------
+     |
+     | List of event subscribers
+    */
+
+    'subscribers' => [
+
+        // OrderEventsSubscriber::class,
+        // TrialPeriodSubscriber::class,
+
+    ]
+];
