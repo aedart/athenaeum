c8ed37405005a889046a3624751e721b4a469868
Alin Eugen Deac <aedart@gmail.com>
2020-02-15 21:02:25 +0100

    Prevent the IoC from registering  as application
    This wasn't really an issue because Laravel's application overwrites this on it's own. However, better to make sure of it; make this explicit!

--- packages/Testing/src/TestCases/LaravelTestCase.php
+++ packages/Testing/src/TestCases/LaravelTestCase.php
@@ -18,6 +18,16 @@ abstract class LaravelTestCase extends IntegrationTestCase
 {
     use LaravelTestHelper;
 
+    /**
+     * WARNING: Whilst testing using this abstraction,
+     * you should avoid allowing the Service Container
+     * from the `IntegrationTestCase` to register as
+     * "application" - it might cause unwanted behaviour!
+     *
+     * @var bool
+     */
+    protected bool $registerAsApplication = false;
+
     /*****************************************************************
      * Setup Methods
      ****************************************************************/
