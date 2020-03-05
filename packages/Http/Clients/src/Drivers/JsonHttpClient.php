c79fae87992180f21f1da4e7b147f9474cded7fa
Alin Eugen Deac <aedart@gmail.com>
2020-03-05 22:00:06 +0100

    Simplify client
    With the default data format resolving, there is no longer need for anything but a single property!

--- packages/Http/Clients/src/Drivers/JsonHttpClient.php
+++ packages/Http/Clients/src/Drivers/JsonHttpClient.php
@@ -2,7 +2,7 @@
 
 namespace Aedart\Http\Clients\Drivers;
 
-use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\RequestOptions;
 
 /**
  * Json Http Client
@@ -13,46 +13,9 @@ use Psr\Http\Message\ResponseInterface;
 class JsonHttpClient extends DefaultHttpClient
 {
     /**
-     * @inheritdoc
-     */
-    public function __construct(array $options = [])
-    {
-        parent::__construct($options);
-
-        $this
-            ->jsonFormat()
-            ->setInitialHeadersIfRequired();
-    }
-
-    /*****************************************************************
-     * Internals
-     ****************************************************************/
-
-    /**
-     * Sets the Accept and Content-Type headers for next request,
-     * if not already specified.
+     * The default data format to use for requests
      *
-     * @return self
+     * @var string
      */
-    protected function setInitialHeadersIfRequired()
-    {
-        $headers = [];
-
-        if (!$this->hasInitialHeader('Accept')) {
-            $headers['Accept'] = $this->jsonAccept;
-        }
-
-        if (!$this->hasInitialHeader('Content-Type')) {
-            $headers['Content-Type'] = $this->jsonContentType;
-        }
-
-        // Merge into the initial options
-        $this->initialOptions['headers'] = $this->initialOptions['headers'] ?? [];
-        $this->initialOptions['headers'] = array_merge_recursive($this->initialOptions['headers'], $headers);
-
-        // Ensure that next request has these initial headers set.
-        $this->resetOptionsForNextRequest();
-
-        return $this;
-    }
+    protected string $defaultDataFormat = RequestOptions::JSON;
 }
