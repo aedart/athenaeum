07c2d47d0e86b57b1797d748e8d561aff90327c3
Alin Eugen Deac <aedart@gmail.com>
2020-03-07 09:48:30 +0100

    Add http client abstraction (incomplete)

---
+++ packages/Http/Clients/src/Drivers/BaseClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Http\Clients\Drivers;
+
+use Aedart\Contracts\Http\Clients\Client;
+
+/**
+ * Base Http Client
+ *
+ * Abstraction for Http Clients
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Drivers
+ */
+abstract class BaseClient implements Client
+{
+
+}
