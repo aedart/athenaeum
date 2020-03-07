eb636e42b222ad4b6bcf90bc6523bf78455735eb
Alin Eugen Deac <aedart@gmail.com>
2020-03-07 19:14:19 +0100

    Change description of options

--- packages/Contracts/src/Http/Clients/Requests/Builder.php
+++ packages/Contracts/src/Http/Clients/Requests/Builder.php
@@ -122,6 +122,7 @@ interface Builder extends HttpClientAware,
      * @param string|null $method [optional] Http method name. If none given, the previous set is applied
      * @param string|UriInterface|null $uri [optional] If none given, then set Uri is used
      * @param array $options [optional] Given options are merged with this builder already set options.
+     *                      However, these options are not applied on the builder's options!
      *
      * @return ResponseInterface
      */
