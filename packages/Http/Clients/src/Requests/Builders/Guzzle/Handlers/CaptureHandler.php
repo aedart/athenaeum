3b4b3ddc2db9ef0ce0527acc2bd8cc99fd3e71b5
Alin Eugen Deac <aedart@gmail.com>
2020-03-10 21:05:43 +0100

    Add capture handler
    Custom handler to capture built request and processed options from Guzzle

---
+++ packages/Http/Clients/src/Requests/Builders/Guzzle/Handlers/CaptureHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Aedart\Http\Clients\Requests\Builders\Guzzle\Handlers;
+
+use GuzzleHttp\Promise\PromiseInterface;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Capture Handler
+ *
+ * Captures a built request and options. Handler does NOT actually send the
+ * request!
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests\Builders\Guzzle\Handlers
+ */
+class CaptureHandler
+{
+    /**
+     * The captured request
+     *
+     * @var RequestInterface|null
+     */
+    protected ?RequestInterface $request;
+
+    /**
+     * The processed options
+     *
+     * @var array
+     */
+    protected array $options = [];
+
+    /**
+     * Returns the captured request
+     *
+     * @return RequestInterface|null
+     */
+    public function request(): ?RequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * Returns the processed options
+     *
+     * @return array
+     */
+    public function options(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * Captures the built request and aborts the send process, by resolving to
+     * an empty response.
+     *
+     * @param RequestInterface $request
+     * @param array $options
+     *
+     * @return PromiseInterface
+     */
+    public function __invoke(RequestInterface $request, array $options): PromiseInterface
+    {
+        $this->request = $request;
+        $this->options = $options;
+
+        // Resolve with empty response
+        return \GuzzleHttp\Promise\promise_for(new Response());
+    }
+}
