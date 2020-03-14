fc4d00c9679486d101d6ce474fb60431d57fe0dd
Alin Eugen Deac <aedart@gmail.com>
2020-03-14 22:17:05 +0100

    Fix payload resolve pipe
    Now the array data is handled much more correct and the raw payload is respected.

--- packages/Http/Clients/src/Requests/Builders/Guzzle/Pipes/ResolvesRequestPayload.php
+++ packages/Http/Clients/src/Requests/Builders/Guzzle/Pipes/ResolvesRequestPayload.php
@@ -2,6 +2,7 @@
 
 namespace Aedart\Http\Clients\Requests\Builders\Guzzle\Pipes;
 
+use Aedart\Contracts\Http\Clients\Requests\Builder;
 use Aedart\Http\Clients\Requests\Builders\Guzzle\PayloadData;
 use Aedart\Http\Clients\Requests\Builders\ProcessedOptions;
 use GuzzleHttp\RequestOptions;
@@ -24,29 +25,81 @@ class ResolvesRequestPayload
      */
     public function handle(ProcessedOptions $processed, $next)
     {
-        // Get options set from the builder
         $options = $processed->options();
+        $builder = $processed->builder();
 
-        // Obtain evt. appended data (e.g. via the post, patch, ...etc)
-        // Or perhaps via the withOptions() method on the builder.
-        $data = PayloadData::extract($options);
+        // Determine format
+        $format = $options['data_format'] ?? $builder->getDataFormat();
 
-        // Unset the payload data on the options (will be added again later)
+        // Obtain data, based on format
+        $data = $this->obtainData($format, $options, $builder);
+
+        // Unset format and data from options
         unset(
+            $options['data_format'],
             $options[RequestOptions::BODY],
             $options[RequestOptions::JSON],
             $options[RequestOptions::MULTIPART],
             $options[RequestOptions::FORM_PARAMS],
         );
 
-        // Determine the desired data format
-        $format = $options['data_format'] ?? $processed->builder()->getDataFormat();
-        unset($options['data_format']);
-
         // Finally, apply the data
         $options[$format] = $data;
         $processed->setOptions($options);
 
         return $next($processed);
+    }
+
+    /**
+     * Obtains the data to be used by the request.
+     *
+     * @param string $format
+     * @param array $options
+     * @param Builder $builder
+     *
+     * @return mixed
+     */
+    protected function obtainData(string $format, array $options, Builder $builder)
+    {
+        switch ($format){
+            // Array formats
+            case RequestOptions::JSON:
+            case RequestOptions::FORM_PARAMS:
+                return $this->mergeData($options, $builder);
+
+            case RequestOptions::MULTIPART:
+                // TODO: Files...
+                return null;
+
+            // None-array format, e.g. streams, plain text... etc.
+            case RequestOptions::BODY:
+            default:
+                // First, try to obtain "raw" payload from given options
+                if( ! empty($options[RequestOptions::BODY])){
+                    return PayloadData::extract($options);
+                }
+
+                // Otherwise, default to raw payload from the builder.
+                return $builder->getRawPayload();
+        }
+    }
+
+    /**
+     * Returns the array data from options and builder
+     *
+     * Method will merge the builder's data with that from
+     * given options.
+     *
+     * @param array $options
+     * @param Builder $builder
+     *
+     * @return array
+     */
+    protected function mergeData(array $options, Builder $builder): array
+    {
+        return array_merge(
+            $builder->getData(),
+            PayloadData::extract($options)
+        );
     }
 }
