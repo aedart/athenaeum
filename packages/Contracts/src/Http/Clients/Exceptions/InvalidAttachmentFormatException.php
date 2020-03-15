0dd8e24661c2c3852aef8f866eb65c3fa54f5f0d
Alin Eugen Deac <aedart@gmail.com>
2020-03-15 11:45:56 +0100

    Add invalid attachment exception

---
+++ packages/Contracts/src/Http/Clients/Exceptions/InvalidAttachmentFormatException.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Aedart\Contracts\Http\Clients\Exceptions;
+
+/**
+ * Invalid Attachment Format Exception
+ *
+ * Should be thrown when an invalid attachment format is attempted used.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients\Exceptions
+ */
+interface InvalidAttachmentFormatException
+{
+
+}
---
+++ packages/Http/Clients/src/Exceptions/InvalidAttachmentFormat.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Http\Clients\Exceptions;
+
+use Aedart\Contracts\Http\Clients\Exceptions\InvalidAttachmentFormatException;
+use InvalidArgumentException;
+
+/**
+ * Invalid Attachment Format Exception
+ *
+ * @see \Aedart\Contracts\Http\Clients\Exceptions\InvalidAttachmentFormatException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Exceptions
+ */
+class InvalidAttachmentFormat extends InvalidArgumentException implements InvalidAttachmentFormatException
+{
+
+}
