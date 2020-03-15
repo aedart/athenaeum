92479e7177e67faf6e9fa221356f9476a05da29d
Alin Eugen Deac <aedart@gmail.com>
2020-03-15 10:21:29 +0100

    Add invalid path exception

---
+++ packages/Http/Clients/src/Exceptions/InvalidFilePath.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Http\Clients\Exceptions;
+
+use Aedart\Contracts\Http\Clients\Exceptions\InvalidFilePathException;
+use InvalidArgumentException;
+
+/**
+ * Invalid File Path Exception
+ *
+ * @see \Aedart\Contracts\Http\Clients\Exceptions\InvalidFilePathException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Exceptions
+ */
+class InvalidFilePath extends InvalidArgumentException implements InvalidFilePathException
+{
+
+}
