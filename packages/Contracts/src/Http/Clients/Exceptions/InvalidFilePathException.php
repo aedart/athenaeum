febe755589bec1eaf42a745ee99e261bead809a5
Alin Eugen Deac <aedart@gmail.com>
2020-03-15 10:19:05 +0100

    Add exception interface for attachment

---
+++ packages/Contracts/src/Http/Clients/Exceptions/InvalidFilePathException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Contracts\Http\Clients\Exceptions;
+
+use Throwable;
+
+/**
+ * Invalid File Path Exception
+ *
+ * Should be thrown when an invalid file path has been provided
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients\Exceptions
+ */
+interface InvalidFilePathException extends Throwable
+{
+
+}
