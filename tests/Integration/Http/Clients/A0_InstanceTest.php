e449ad63ade967edc37ffd5657c69f7d66984880
Alin Eugen Deac <aedart@gmail.com>
2020-03-18 17:33:26 +0100

    Add instance test

---
+++ tests/Integration/Http/Clients/A0_InstanceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Clients;
+
+use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
+use Aedart\Tests\TestCases\Http\HttpClientsTestCase;
+
+/**
+ * A0_InstanceTest
+ *
+ * @group http-clients
+ * @group http-clients-a0
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Http\Clients
+ */
+class A0_InstanceTest extends HttpClientsTestCase
+{
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function canObtainInstance(string $profile)
+    {
+        $client = $this->getHttpClientsManager()->profile($profile);
+
+        $this->assertNotNull($client);
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function canObtainDriver(string $profile)
+    {
+        $client = $this->getHttpClientsManager()->profile($profile);
+        $driver = $client->driver();
+
+        $this->assertNotNull($driver);
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function canCreateBuilder(string $profile)
+    {
+        $client = $this->getHttpClientsManager()->profile($profile);
+        $builder = $client->makeBuilder();
+
+        $this->assertNotNull($builder);
+    }
+}
