da4d43c1bb304eca299bae912fdb7dabe22c60eb
Alin Eugen Deac <aedart@gmail.com>
2020-03-18 17:50:25 +0100

    Add http protocol version test

---
+++ tests/Integration/Http/Clients/D1_HttpProtocolVersionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Clients;
+
+use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
+use Aedart\Tests\TestCases\Http\HttpClientsTestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * D1_HttpProtocolVersionTest
+ *
+ * @group http-clients
+ * @group http-clients-d1
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Http\Clients
+ */
+class D1_HttpProtocolVersionTest extends HttpClientsTestCase
+{
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function extractsHttpProtocolVersionFromOptions(string $profile)
+    {
+        $version = '2.0';
+
+        $client = $this->client($profile, [
+            'version' => $version
+        ]);
+
+        $this->assertSame($version, $client->getProtocolVersion(), 'Did not extract Http protocol version from options');
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function usesHttpProtocolVersion(string $profile)
+    {
+        $client = $this->client($profile);
+
+        $version = '2.0';
+
+        /** @var ResponseInterface $response */
+        $client
+            ->withOption('handler', $this->makeRespondsOkMock())
+            ->useProtocolVersion($version)
+            ->request('get', '/users');
+
+        $versionFromRequest = $this->lastRequest->getProtocolVersion();
+        $this->assertSame($version, $versionFromRequest, 'Custom Http protocol version not set on request');
+    }
+}
