a8f4a5dc95bdb968e9b1fda0f601e35b28baf129
Alin Eugen Deac <aedart@gmail.com>
2020-03-21 12:12:42 +0100

    Add cookie unit test

---
+++ tests/Unit/Http/Cookies/CookieTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Aedart\Tests\Unit\Http\Cookies;
+
+use Aedart\Contracts\Http\Cookies\Cookie as CookieInterface;
+use Aedart\Http\Cookies\Cookie;
+use Aedart\Testing\TestCases\UnitTestCase;
+use Throwable;
+
+/**
+ * CookieTest
+ *
+ * @group http-cookies
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Unit\Http\Cookies
+ */
+class CookieTest extends UnitTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Creates a new Cookie instance
+     *
+     * @param array $data [optional]
+     *
+     * @return CookieInterface
+     *
+     * @throws Throwable
+     */
+    protected function makeCookie(array $data = []): CookieInterface
+    {
+        return new Cookie($data);
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     *
+     * @throws Throwable
+     */
+    public function canPopulate()
+    {
+        $name = 'token';
+        $value = $this->getFaker()->sha1;
+
+        $cookie = $this->makeCookie([
+            'name' => $name,
+            'value' => $value
+        ]);
+
+        $this->assertSame($name, $cookie->getName(), 'Incorrect name');
+        $this->assertSame($value, $cookie->getValue(), 'Incorrect value');
+    }
+
+    /**
+     * @test
+     *
+     * @throws Throwable
+     */
+    public function canExportToArray()
+    {
+        $name = 'token';
+        $value = $this->getFaker()->sha1;
+
+        $cookie = $this->makeCookie([
+            'name' => $name,
+            'value' => $value
+        ]);
+
+        $this->assertSame([
+            'name' => $name,
+            'value' => $value
+        ], $cookie->toArray());
+    }
+}
