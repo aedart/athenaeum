9b28fe31de1c82037df357e32abe871920972834
Alin Eugen Deac <aedart@gmail.com>
2020-03-21 16:40:54 +0100

    Add invalid cookie format exception

---
+++ packages/Contracts/src/Http/Clients/Exceptions/InvalidCookieFormatException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Contracts\Http\Clients\Exceptions;
+
+use Throwable;
+
+/**
+ * Invalid Cookie Format Exception
+ *
+ * Should be thrown when an invalid Cookie format is attempted used
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients\Exceptions
+ */
+interface InvalidCookieFormatException extends Throwable
+{
+
+}
---
+++ packages/Http/Clients/src/Exceptions/InvalidCookieFormat.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aedart\Http\Clients\Exceptions;
+
+use Aedart\Contracts\Http\Clients\Exceptions\InvalidCookieFormatException;
+use InvalidArgumentException;
+
+/**
+ * Invalid Cookie Format Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Exceptions
+ */
+class InvalidCookieFormat extends InvalidArgumentException implements InvalidCookieFormatException
+{
+
+}
