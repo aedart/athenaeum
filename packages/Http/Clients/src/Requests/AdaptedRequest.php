aaf3cb0873062161950096f2531c2537d97d6156
Alin Eugen Deac <aedart@gmail.com>
2020-03-25 19:47:39 +0100

    Add adapted request
    Placeholder for the driver-specific options.

---
+++ packages/Http/Clients/src/Requests/AdaptedRequest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Aedart\Http\Clients\Requests;
+
+use Aedart\Contracts\Http\Clients\Client;
+use Aedart\Contracts\Http\Clients\Requests\HasDriverOptions;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Adapted Request
+ *
+ * Contains driver specific options that allows a {@see Client}'s native
+ * driver to send this given requests.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests
+ */
+class AdaptedRequest implements RequestInterface,
+    HasDriverOptions
+{
+    /**
+     * The actual request
+     *
+     * @var RequestInterface
+     */
+    protected RequestInterface $request;
+
+    /**
+     * Driver specific options
+     *
+     * @var array
+     */
+    protected array $driverOptions = [];
+
+    /**
+     * AdaptedRequest constructor.
+     *
+     * @param RequestInterface $request
+     * @param array $driverOptions [optional]
+     */
+    public function __construct(RequestInterface $request, array $driverOptions = [])
+    {
+        $this->request = $request;
+        $this->driverOptions = $driverOptions;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDriverOptions(): array
+    {
+        return $this->driverOptions;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getProtocolVersion()
+    {
+        return $this->request->getProtocolVersion();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withProtocolVersion($version)
+    {
+        return new static(
+            $this->request->withProtocolVersion($version),
+            $this->getDriverOptions()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHeaders()
+    {
+        return $this->request->getHeaders();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasHeader($name)
+    {
+        return $this->request->hasHeader($name);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHeader($name)
+    {
+        return $this->request->getHeader($name);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHeaderLine($name)
+    {
+        return $this->request->getHeaderLine($name);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withHeader($name, $value)
+    {
+        return new static(
+            $this->request->withHeader($name, $value),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withAddedHeader($name, $value)
+    {
+        return new static(
+            $this->request->withAddedHeader($name, $value),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withoutHeader($name)
+    {
+        return new static(
+            $this->request->withoutHeader($name),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBody()
+    {
+        return $this->request->getBody();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withBody(StreamInterface $body)
+    {
+        return new static(
+            $this->request->withBody($body),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRequestTarget()
+    {
+        return $this->request->getRequestTarget();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withRequestTarget($requestTarget)
+    {
+        return new static(
+            $this->request->withRequestTarget($requestTarget),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMethod()
+    {
+        return $this->request->getMethod();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withMethod($method)
+    {
+        return new static(
+            $this->request->withMethod($method),
+            $this->getDriverOptions(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUri()
+    {
+        return $this->request->getUri();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function withUri(UriInterface $uri, $preserveHost = false)
+    {
+        return new static(
+            $this->request->withUri($uri, $preserveHost),
+            $this->getDriverOptions(),
+        );
+    }
+}
---
+++ tests/Unit/Http/Clients/Requests/AdaptedRequestTest.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Aedart\Tests\Unit\Http\Clients\Requests;
+
+use Aedart\Contracts\Http\Clients\Requests\HasDriverOptions;
+use Aedart\Http\Clients\Requests\AdaptedRequest;
+use Aedart\Testing\TestCases\UnitTestCase;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\Uri;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * AdaptedRequestTest
+ *
+ * @group http-clients
+ * @group adapted-request
+ * @group psr-7
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Unit\Http\Clients\Requests
+ */
+class AdaptedRequestTest extends UnitTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Creates a new Adapted Request
+     *
+     * @param string                               $method  HTTP method
+     * @param string|UriInterface                  $uri     URI
+     * @param array                                $headers [optional] Request headers
+     * @param array $driverOptions [optional]
+     * @param string|null|resource|StreamInterface $body [optional] body
+     * @param string                               $version [optional] version
+     *
+     * @return RequestInterface|HasDriverOptions
+     */
+    public function makeRequest(
+        string $method,
+        $uri,
+        array $headers = [],
+        array $driverOptions = [],
+        $body = null,
+        $version = '1.1'
+    )
+    {
+        return new AdaptedRequest(
+            new Request($method, $uri, $headers, $body, $version),
+            $driverOptions
+        );
+    }
+
+    /*****************************************************************
+     * Actual Test
+     ****************************************************************/
+
+    /**
+     * @test
+     */
+    public function canObtainDriverOptions()
+    {
+        $options = ['a', 'b', 'c'];
+
+        $request = $this->makeRequest('get', '/users', [], $options);
+
+        $this->assertSame($options, $request->getDriverOptions());
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainProtocolVersion()
+    {
+        $version = '2.0';
+
+        $request = $this
+            ->makeRequest('get', '/users')
+            ->withProtocolVersion($version);
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+        $this->assertSame($version, $request->getProtocolVersion());
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainHeaders()
+    {
+        $request = $this
+            ->makeRequest('get', '/users', [
+                'Content-Type' => 'application/json'
+            ])
+            ->withHeader('Accept', 'application/xml')
+            ->withAddedHeader('X-Foo', 'bar');
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $this->assertTrue($request->hasHeader('Accept'), 'Accept header not set');
+        $this->assertTrue($request->hasHeader('Content-Type'), 'Content-Type header not set');
+
+        $this->assertSame('application/json', $request->getHeader('Content-Type')[0], 'incorrect content-type');
+        $this->assertSame('application/xml', $request->getHeaderLine('Accept'), 'incorrect accept');
+
+        $this->assertSame([
+            'Content-Type' => [ 'application/json' ],
+            'Accept' => [ 'application/xml' ],
+            'X-Foo' => [ 'bar' ],
+        ], $request->getHeaders(), 'Incorrect headers');
+    }
+
+    /**
+     * @test
+     */
+    public function canRemoveHeader()
+    {
+        $request = $this
+            ->makeRequest('get', '/users', [
+                'Content-Type' => 'application/json',
+                'X-Foo' => 'jam'
+            ])
+            ->withoutHeader('X-Foo');
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $this->assertSame([
+            'Content-Type' => [ 'application/json' ],
+        ], $request->getHeaders(), 'Incorrect headers');
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainBody()
+    {
+        $stream = new Stream(fopen('php://memory', 'r+'));
+        $stream->write('Foo');
+        $stream->rewind();
+
+        $request = $this
+            ->makeRequest('post', '/users')
+            ->withBody($stream);
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $content = $request->getBody()->getContents();
+        $this->assertSame('Foo', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainRequestTarget()
+    {
+        $target = 'https://acme.org';
+
+        $request = $this
+            ->makeRequest('post', '/users')
+            ->withRequestTarget($target);
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $this->assertSame($target, $request->getRequestTarget());
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndGetHttpMethod()
+    {
+        $request = $this
+            ->makeRequest('post', '/users')
+            ->withMethod('patch');
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $this->assertSame('PATCH', $request->getMethod());
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainUri()
+    {
+        $uri = new Uri('/users/relations/notes');
+
+        $request = $this
+            ->makeRequest('post', '/users')
+            ->withUri($uri);
+
+        $this->assertInstanceOf(AdaptedRequest::class, $request);
+
+        $this->assertSame($uri, $request->getUri());
+    }
+}
