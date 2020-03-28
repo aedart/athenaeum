814c00fecea99ff750ed00a036d50f43aae97ca3
Alin Eugen Deac <aedart@gmail.com>
2020-03-28 15:05:14 +0100

    Add response expectation tests

--- tests/Integration/Http/Clients/I0_ResponseExpectationsTest.php
+++ tests/Integration/Http/Clients/I0_ResponseExpectationsTest.php
@@ -2,9 +2,11 @@
 
 namespace Aedart\Tests\Integration\Http\Clients;
 
+use Aedart\Contracts\Http\Clients\Exceptions\ExpectationNotMetException;
 use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
 use Aedart\Contracts\Http\Clients\Responses\Status;
 use Aedart\Tests\TestCases\Http\HttpClientsTestCase;
+use GuzzleHttp\Psr7\Response;
 
 /**
  * I0_ResponseExpectationsTest
@@ -80,5 +82,95 @@ class I0_ResponseExpectationsTest extends HttpClientsTestCase
         $this->assertTrue($hasAppliedA, 'expectation A not applied');
         $this->assertTrue($hasAppliedB, 'expectation B not applied');
         $this->assertTrue($hasAppliedC, 'expectation C not applied');
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function canExpectViaCallback(string $profile)
+    {
+        $hasApplied = false;
+
+        $expectation = function() use(&$hasApplied) {
+            $hasApplied = true;
+        };
+
+        $this->client($profile)
+            ->withOption('handler', $this->makeRespondsOkMock())
+            ->expect($expectation)
+            ->get('/users');
+
+        $this->assertTrue($hasApplied, 'has not applied callback expectation via "expect" method');
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function failsWithExceptionWhenExpectationNotMet(string $profile)
+    {
+        $this->expectException(ExpectationNotMetException::class);
+
+        $mock = $this->makeResponseMock([
+            new Response(301)
+        ]);
+
+        $this->client($profile)
+            ->withOption('handler', $mock)
+            ->expect(200)
+            ->get('/users');
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function appliesOtherwiseCallbackWhenExpectationNotMet(string $profile)
+    {
+        $mock = $this->makeResponseMock([
+            new Response(301)
+        ]);
+
+        $hasApplied = false;
+
+        $otherwise = function() use(&$hasApplied) {
+            $hasApplied = true;
+        };
+
+        $this->client($profile)
+            ->withOption('handler', $mock)
+            ->expect(200, $otherwise)
+            ->get('/users');
+
+        $this->assertTrue($hasApplied, 'Otherwise callback not invoked');
+    }
+
+    /**
+     * @test
+     * @dataProvider providesClientProfiles
+     *
+     * @param string $profile
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function doesNothingWhenExpectationIsMet(string $profile)
+    {
+        $this->client($profile)
+            ->withOption('handler', $this->makeRespondsOkMock())
+            ->expect([201, 204, 200])
+            ->get('/users');
     }
 }
