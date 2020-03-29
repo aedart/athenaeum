b71ac5c120ea1d8d25ba6d2943623d031beabc47
Alin Eugen Deac <aedart@gmail.com>
2020-03-29 13:09:26 +0200

    Add more identifiers
    These are actually mostly for internal use, but figured it's no harm to expose them publicly

--- packages/Contracts/src/Http/Clients/Requests/Query/Identifiers.php
+++ packages/Contracts/src/Http/Clients/Requests/Query/Identifiers.php
@@ -19,4 +19,34 @@ interface Identifiers
      * Descending order
      */
     public const DESCENDING = 'desc';
+
+    /**
+     * Bindings identifier
+     */
+    public const BINDINGS = '@:bindings:@';
+
+    /**
+     * Type identifier
+     */
+    public const TYPE = '@:type:@';
+
+    /**
+     * Fields identifier
+     */
+    public const FIELDS = '@:fields:@';
+
+    /**
+     * Selects identifier
+     */
+    public const SELECTS = '@:selects:@';
+
+    /**
+     * Regular field selection type identifier
+     */
+    public const SELECT_TYPE_REGULAR = '@:select_regular:@';
+
+    /**
+     * Raw field selection type identifier
+     */
+    public const SELECT_TYPE_RAW = '@:select_raw:@';
 }
