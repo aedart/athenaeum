ed04d6d90130e70e2c8b6c29cae6358da862ea00
Alin Eugen Deac <aedart@gmail.com>
2020-03-29 13:08:39 +0200

    Add default grammar (incomplete)

---
+++ packages/Http/Clients/src/Requests/Query/Grammars/DefaultGrammar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aedart\Http\Clients\Requests\Query\Grammars;
+
+/**
+ * Default Http Query Grammar
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests\Query\Grammars
+ */
+class DefaultGrammar extends BaseGrammar
+{
+
+}
