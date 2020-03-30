be186c571d7db33c6ac8bc8eeeff4936bac41565
Alin Eugen Deac <aedart@gmail.com>
2020-03-30 19:34:36 +0200

    Add http query exceptions

---
+++ packages/Contracts/src/Http/Clients/Exceptions/HttpQueryBuilderException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Contracts\Http\Clients\Exceptions;
+
+use Throwable;
+
+/**
+ * Http Query Builder Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Clients\Exceptions
+ */
+interface HttpQueryBuilderException extends Throwable
+{
+
+}
---
+++ packages/Http/Clients/src/Exceptions/UnableToBuildHttpQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Http\Clients\Exceptions;
+
+use Aedart\Contracts\Http\Clients\Exceptions\HttpQueryBuilderException;
+use RuntimeException;
+
+/**
+ * Unable To Build Http Query Exception
+ *
+ * Should be thrown when unable to assemble / build a http query
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Exceptions
+ */
+class UnableToBuildHttpQuery extends RuntimeException implements HttpQueryBuilderException
+{
+
+}
