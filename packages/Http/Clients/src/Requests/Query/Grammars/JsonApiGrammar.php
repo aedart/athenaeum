240fca0b0bbdf568a182e2cda9967389682ce979
Alin Eugen Deac <aedart@gmail.com>
2020-04-02 19:19:00 +0200

    Add Json API grammar (incomplete)

---
+++ packages/Http/Clients/src/Requests/Query/Grammars/JsonApiGrammar.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Aedart\Http\Clients\Requests\Query\Grammars;
+
+/**
+ * Json Api Grammar
+ *
+ * Offers basic support for the Json API grammar.
+ *
+ * @see https://jsonapi.org/
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests\Query\Grammars
+ */
+class JsonApiGrammar extends BaseGrammar
+{
+
+}
