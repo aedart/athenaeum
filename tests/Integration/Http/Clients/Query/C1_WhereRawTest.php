8f81bd5bdd19f9e87cd20cd76770e5f7275d6d8e
Alin Eugen Deac <aedart@gmail.com>
2020-04-03 21:28:22 +0200

    Add where raw tests for odata grammar
    NOTE: All of these produce incorrect syntax for odata, but has to be allowed in it's current form, so that advanced filters can be built

--- tests/Integration/Http/Clients/Query/C1_WhereRawTest.php
+++ tests/Integration/Http/Clients/Query/C1_WhereRawTest.php
@@ -41,7 +41,14 @@ class C1_WhereRawTest extends HttpClientsTestCase
             'json api' => [
                 'json_api',
                 $expected
-            ]
+            ],
+
+            // NOTE: Here too the syntax is wrong, but have to allow it, e.g. in order
+            // to allow advanced filters via OData.
+            'odata' => [
+                'odata',
+                '?$filter=user=john'
+            ],
         ];
     }
 
@@ -62,7 +69,14 @@ class C1_WhereRawTest extends HttpClientsTestCase
             'json api' => [
                 'json_api',
                 $expected
-            ]
+            ],
+
+            // NOTE: This too provides a wrong syntax,... still, it has to be allowed,
+            // for building advanced filtering.
+            'odata' => [
+                'odata',
+                '?$filter=filter=user eq 10'
+            ],
         ];
     }
 
@@ -81,7 +95,14 @@ class C1_WhereRawTest extends HttpClientsTestCase
             'json api' => [
                 'json_api',
                 '?filter[name][like]=john&filter=age gt 25'
-            ]
+            ],
+
+            // NOTE: Same as previous, generates invalid Odata query syntax, but still
+            // have to allow it, for the benefit of advanced filters.
+            'odata' => [
+                'odata',
+                '?$filter=name like \'john\' and filter=age gt 25'
+            ],
         ];
     }
 
