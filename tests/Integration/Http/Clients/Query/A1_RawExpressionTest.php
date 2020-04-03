be413a835d8e6b05b9860683cef0f71d4f8c833c
Alin Eugen Deac <aedart@gmail.com>
2020-04-03 22:26:46 +0200

    Add raw expression test

---
+++ tests/Integration/Http/Clients/Query/A1_RawExpressionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Clients\Query;
+
+use Aedart\Contracts\Http\Clients\Exceptions\HttpQueryBuilderException;
+use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\TestCases\Http\HttpClientsTestCase;
+
+/**
+ * A1_RawExpressionTest
+ *
+ * @group http-clients
+ * @group http-query
+ * @group http-query-a1
+ * @group http-query-grammars
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Http\Clients\Query
+ */
+class A1_RawExpressionTest extends HttpClientsTestCase
+{
+    /*****************************************************************
+     * Data Providers
+     ****************************************************************/
+
+    /**
+     * Provides raw expression test data
+     *
+     * @return array
+     */
+    public function providesWhereRawData(): array
+    {
+        $expected = '?search=person from (a,b,c)';
+
+        // NOTE: We do NOT care about if the syntax is correct or not for each
+        // grammar. We only care that the expression is added exactly as stated.
+        return [
+            'default' => [ 'default', $expected ],
+            'json api' => [ 'json_api', $expected ],
+            'odata' => [ 'odata', $expected ],
+        ];
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     * @dataProvider providesWhereRawData
+     *
+     * @param string $grammar
+     * @param string $expected
+     *
+     * @throws ProfileNotFoundException
+     * @throws HttpQueryBuilderException
+     */
+    public function canAddRawExpressions(string $grammar, string $expected)
+    {
+        $result = $this
+            ->query($grammar)
+            ->raw('search=person from (:list)', [ 'list' => 'a,b,c' ])
+            ->build();
+
+        ConsoleDebugger::output($result);
+
+        $this->assertSame($expected, $result);
+    }
+}
