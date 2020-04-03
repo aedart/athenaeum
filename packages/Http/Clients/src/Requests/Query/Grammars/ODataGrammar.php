5197d1b8ff80903e0ac3f5c5e6732208dbeb0f81
Alin Eugen Deac <aedart@gmail.com>
2020-04-03 19:47:40 +0200

    Add OData grammar (incomplete)

---
+++ packages/Http/Clients/src/Requests/Query/Grammars/ODataGrammar.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Http\Clients\Requests\Query\Grammars;
+
+/**
+ * Open Data Protocol Grammar
+ *
+ * Offers basic support for OData v4.x grammar.
+ *
+ * @see https://www.odata.org/
+ * @see http://docs.oasis-open.org/odata/odata/v4.0/odata-v4.0-part2-url-conventions.html
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests\Query\Grammars
+ */
+class ODataGrammar extends BaseGrammar
+{
+
+}
