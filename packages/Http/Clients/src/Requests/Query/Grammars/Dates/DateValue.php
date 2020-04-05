41f7ef5e0244b2d18908fca63642230ecab53ee0
Alin Eugen Deac <aedart@gmail.com>
2020-04-05 11:02:52 +0200

    Add dto for holding date and format

---
+++ packages/Http/Clients/src/Requests/Query/Grammars/Dates/DateValue.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Aedart\Http\Clients\Requests\Query\Grammars\Dates;
+
+use DateTimeInterface;
+
+/**
+ * Date Value
+ *
+ * Wrapper for date-based values in where expressions
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Clients\Requests\Query\Grammars\Dates
+ */
+class DateValue
+{
+    /**
+     * The date that was given
+     *
+     * @var string|DateTimeInterface|null
+     */
+    protected $date;
+
+    /**
+     * The date format
+     *
+     * @var string
+     */
+    protected string $format;
+
+    /**
+     * DateValue constructor.
+     *
+     * @param string|DateTimeInterface|null $date
+     * @param string $format
+     */
+    public function __construct($date, string $format)
+    {
+        $this->date = $date;
+        $this->format = $format;
+    }
+
+    /**
+     * Get the date that was set as value
+     *
+     * @return DateTimeInterface|string|null
+     */
+    public function date()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Get the date format
+     *
+     * @return string
+     */
+    public function format(): string
+    {
+        return $this->format;
+    }
+}
