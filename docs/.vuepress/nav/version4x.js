4f88e2398d944d0a24dc1ddd16b0f9a687eff425
Alin Eugen Deac <aedart@gmail.com>
2020-04-09 21:29:24 +0200

    Add cookies doc (incomplete)

--- docs/.vuepress/nav/version4x.js
+++ docs/.vuepress/nav/version4x.js
@@ -123,6 +123,7 @@ module.exports.sidebar = function(){
                                 'http/clients/methods/data_format',
                                 'http/clients/methods/data',
                                 'http/clients/methods/attachments',
+                                'http/clients/methods/cookies',
                                 'http/clients/methods/conditions',
                                 'http/clients/methods/criteria',
                                 'http/clients/methods/redirects',
---
+++ docs/archive/current/http/clients/methods/cookies.md
@@ -0,0 +1,17 @@
+---
+description: Request Cookies
+sidebarDepth: 0
+---
+
+# Cookies
+
+TODO: ...coming soon...
+
+[[TOC]]
+
+## Add Cookie
+
+## Alternative Method
+
+## Remove Cookie
+
