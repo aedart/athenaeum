02f9357851eaf13775c7a4c3c4459df6f2ee390d
Alin Eugen Deac <aedart@gmail.com>
2020-04-19 18:58:57 +0200

    Add unable to change state exception
    #9

---
+++ packages/Contracts/src/Circuits/Exceptions/UnableToChangeStateException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aedart\Contracts\Circuits\Exceptions;
+
+use Aedart\Contracts\Circuits\CircuitBreaker;
+
+/**
+ * Unable To Change State Exception
+ *
+ * Should be thrown when a circuit breaker is requested to change state,
+ * yet unable to.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Circuits\Exceptions
+ */
+interface UnableToChangeStateException extends CircuitBreaker
+{
+
+}
