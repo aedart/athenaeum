4ed8554336f85968bf8146bc4d5527c1cd7537c1
Alin Eugen Deac <aedart@gmail.com>
2020-04-19 18:15:29 +0200

    Add circuit breaker aware component
    #9

---
+++ tests/Unit/Circuits/Traits/CircuitsTraitsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Aedart\Tests\Unit\Circuits\Traits;
+
+use Aedart\Circuits\Traits\CircuitBreakerTrait;
+use Aedart\Tests\TestCases\TraitTestCase;
+
+/**
+ * CircuitsTraitsTest
+ *
+ * @group circuits
+ * @group circuits-traits
+ * @group traits
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Unit\Circuits\Traits
+ */
+class CircuitsTraitsTest extends TraitTestCase
+{
+    /*****************************************************************
+     * Providers
+     ****************************************************************/
+
+    /**
+     * @return array
+     */
+    public function awareOfComponentsProvider()
+    {
+        return [
+            'CircuitBreakerTrait' => [ CircuitBreakerTrait::class ],
+        ];
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     * @dataProvider awareOfComponentsProvider
+     *
+     * @param string $awareOfTrait
+     *
+     * @throws \ReflectionException
+     */
+    public function canInvokeAwareOfMethods(string $awareOfTrait)
+    {
+        $this->assertTraitMethods($awareOfTrait, null, null);
+    }
+}
