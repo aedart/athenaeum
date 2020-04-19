88e92744e3c317c91dcc9eee98d37a54dcf0dcf3
Alin Eugen Deac <aedart@gmail.com>
2020-04-19 18:39:56 +0200

    Add general circuit breaker exception interface
    #9

---
+++ packages/Contracts/src/Circuits/Exceptions/CircuitBreakerException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Contracts\Circuits\Exceptions;
+
+use Throwable;
+
+/**
+ * Circuit Breaker Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Circuits\Exceptions
+ */
+interface CircuitBreakerException extends Throwable
+{
+
+}
