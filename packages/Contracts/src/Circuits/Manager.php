6eb066f6e01e1a25dc9e84384454926cebf980e5
Alin Eugen Deac <aedart@gmail.com>
2020-04-23 20:54:58 +0200

    Change manager, rename profile to create
    Also, a circuit breaker must always have a name. Furthermore, it should be allowed to create a new circuit breaker instance, despite when no profile exists for requested name.
    
    #9

--- packages/Contracts/src/Circuits/Manager.php
+++ packages/Contracts/src/Circuits/Manager.php
@@ -15,12 +15,33 @@ interface Manager
     /**
      * Create or obtain Circuit Breaker
      *
-     * @param string|null $profile [optional] Name of Circuit Breaker profile to obtain or create
+     * @param string $name [optional] Name (profile) of Circuit Breaker to obtain or create
      * @param array $options [optional] Circuit Breaker options
+     * @param bool $autoCreate [optional] When true, new circuit breaker is created, even
+     *                       if a "profile" does not exist.
      *
      * @return CircuitBreaker
      *
-     * @throws ProfileNotFoundException
+     * @throws ProfileNotFoundException Only when `$autoCreate` is set to false and requested
+     *                                  circuit breaker does not exist.
      */
-    public function profile(?string $profile = null, array $options = []): CircuitBreaker;
+    public function create(string $name, array $options = [], bool $autoCreate = false): CircuitBreaker;
+
+    /**
+     * Creates a new store instance
+     *
+     * @param string|null $driver [optional] Class path to store "driver". If none given,
+     *                            then {@see defaultStore} is used.
+     * @param array $options [optional] Store options
+     *
+     * @return Store
+     */
+    public function store($driver = null, array $options = []): Store;
+
+    /**
+     * Returns a default store
+     *
+     * @return Store
+     */
+    public function defaultStore(): Store;
 }
