abc1d22e19e848d30b45d0ebde924c7a506bd971
Alin Eugen Deac <aedart@gmail.com>
2020-04-24 17:11:19 +0200

    Add locked interface
    Intended to be used for half-open state.
    
    #9

---
+++ packages/Contracts/src/Circuits/States/Locked.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aedart\Contracts\Circuits\States;
+
+/**
+ * Locked
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Circuits\States
+ */
+interface Locked
+{
+}
