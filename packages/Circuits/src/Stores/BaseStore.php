bb67a9a1a64e03e85654875af9eee5dc6211c3b2
Alin Eugen Deac <aedart@gmail.com>
2020-04-26 15:12:35 +0200

    Add key utility methods
    Perhaps not useful for all types of stores, but will do good enough for the Cache Store.
    
    #9

--- packages/Circuits/src/Stores/BaseStore.php
+++ packages/Circuits/src/Stores/BaseStore.php
@@ -9,6 +9,7 @@ use Aedart\Circuits\Traits\StateFactoryTrait;
 use Aedart\Contracts\Circuits\Failures\FailureFactoryAware;
 use Aedart\Contracts\Circuits\States\StateFactoryAware;
 use Aedart\Contracts\Circuits\Store;
+use Illuminate\Support\Str;
 
 /**
  * Base Store
@@ -32,6 +33,13 @@ abstract class BaseStore implements Store,
     protected string $service;
 
     /**
+     * Key prefix
+     *
+     * @var string
+     */
+    protected string $keyPrefix = '';
+
+    /**
      * BaseStore constructor.
      *
      * @param string $service
@@ -42,7 +50,8 @@ abstract class BaseStore implements Store,
             ->setService($service)
             ->withOptions($options->options)
             ->setStateFactory($options->stateFactory)
-            ->setFailureFactory($options->failureFactory);
+            ->setFailureFactory($options->failureFactory)
+            ->setKeyPrefix($service);
     }
 
     /*****************************************************************
@@ -61,5 +70,31 @@ abstract class BaseStore implements Store,
         $this->service = $name;
 
         return $this;
+    }
+
+    /**
+     * Set key prefix
+     *
+     * @param string $prefix [optional]
+     *
+     * @return self
+     */
+    protected function setKeyPrefix(string $prefix = '')
+    {
+        $this->keyPrefix = $prefix;
+
+        return $this;
+    }
+
+    /**
+     * Creates a prefixed key name
+     *
+     * @param string $name
+     *
+     * @return string Prefixed key
+     */
+    protected function key(string $name): string
+    {
+        return Str::snake($this->keyPrefix . '_' . $name);
     }
 }
