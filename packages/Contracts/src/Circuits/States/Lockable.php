75ae5d521ea08dcaaadb7a75cb7651233ccbd750
Alin Eugen Deac <aedart@gmail.com>
2020-04-28 17:39:48 +0200

    Change description
    #9

--- packages/Contracts/src/Circuits/States/Lockable.php
+++ packages/Contracts/src/Circuits/States/Lockable.php
@@ -5,6 +5,9 @@ namespace Aedart\Contracts\Circuits\States;
 /**
  * Lockable
  *
+ * Ensures that when requested, the state can only be set
+ * or obtain by a single circuit breaker.
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Contracts\Circuits\States
  */
