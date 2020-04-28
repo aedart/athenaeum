ab62a62c6591a4240eef8f2e8ae55cfb89941fb2
Alin Eugen Deac <aedart@gmail.com>
2020-04-28 17:44:58 +0200

    Add cannot be locked exception
    #9

---
+++ packages/Contracts/src/Circuits/Exceptions/StateCannotBeLockedException.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Aedart\Contracts\Circuits\Exceptions;
+
+/**
+ * State Cannot Be Locked Exception
+ *
+ * Should be thrown when an unsupported state is attempted locked.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Circuits\Exceptions
+ */
+interface StateCannotBeLockedException extends StoreException
+{
+
+}
