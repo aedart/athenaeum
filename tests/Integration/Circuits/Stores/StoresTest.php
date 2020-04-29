00a4ad209e76877b406e8a24fdd241ff0dce2750
Alin Eugen Deac <aedart@gmail.com>
2020-04-29 19:27:06 +0200

    Add stores test (incomplete)
    #9

---
+++ tests/Integration/Circuits/Stores/StoresTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aedart\Tests\Integration\Circuits\Stores;
+
+use Aedart\Tests\TestCases\Circuits\CircuitBreakerTestCase;
+
+/**
+ * StoresTest
+ *
+ * @group circuits
+ * @group circuits-stores
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Circuits\Stores
+ */
+class StoresTest extends CircuitBreakerTestCase
+{
+    /*****************************************************************
+     * Data Providers
+     ****************************************************************/
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    // TODO: ...coming soon
+}
