255b1f660bb91cf8a735057344eafe0ec5a102ff
Alin Eugen Deac <aedart@gmail.com>
2020-05-01 19:27:55 +0200

    Add concrete service unavailable exception
    #9

---
+++ packages/Circuits/src/Exceptions/ServiceUnavailable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Aedart\Circuits\Exceptions;
+
+use Aedart\Contracts\Circuits\Exceptions\ServiceUnavailableException;
+use Aedart\Contracts\Circuits\Failure;
+use Aedart\Contracts\Circuits\State;
+
+/**
+ * Service Unavailable Exception
+ *
+ * @see \Aedart\Contracts\Circuits\Exceptions\ServiceUnavailableException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Circuits\Exceptions
+ */
+class ServiceUnavailable extends CircuitBreakerException implements ServiceUnavailableException
+{
+    /**
+     * Creates a new instance with a predefined message,
+     * based on given arguments
+     *
+     * @param string $service
+     * @param State $lastState
+     * @param Failure|null $lastFailure [optional]
+     *
+     * @return ServiceUnavailableException
+     */
+    public static function make(string $service, State $lastState, ?Failure $lastFailure = null): ServiceUnavailableException
+    {
+        $reason = isset($lastState)
+            ? $lastFailure->reason()
+            : 'unknown';
+
+        $message = sprintf(
+            '%s is unavailable. Circuit Breaker\'s state was %s. Last detected failure was: %s',
+            $service,
+            $lastState->name(),
+            $reason
+        );
+
+        return new static($message);
+    }
+}
