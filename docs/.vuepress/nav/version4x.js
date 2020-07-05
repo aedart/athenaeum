7b42866d422b898ef38025d81c92ae0590aa9e0a
Alin Eugen Deac <aedart@gmail.com>
2020-07-05 15:47:57 +0200

    Add events to navigation

--- docs/.vuepress/nav/version4x.js
+++ docs/.vuepress/nav/version4x.js
@@ -18,6 +18,7 @@ module.exports.sidebar = function(){
                 'circuits/install',
                 'circuits/setup',
                 'circuits/usage',
+                'circuits/events',
             ]
         },
         {
