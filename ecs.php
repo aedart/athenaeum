10bbc2ef5a181a2f8870097a69d557be5a34e470
Alin Eugen Deac <aedart@gmail.com>
2020-09-06 14:28:49 +0200

    Replace esc.yaml with php version
    Yaml appears to be unsupported in future versions of easy-coding-standards.

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symplify\EasyCodingStandard\Configuration\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // A. standalone rule
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+
+    // B. full sets
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SETS, [
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ]);
+};
