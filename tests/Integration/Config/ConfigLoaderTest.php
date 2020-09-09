dfdff2ebedb1b428adfc8550ff5590b26f4237b2
Alin Eugen Deac <aedart@gmail.com>
2020-09-09 19:29:51 +0200

    Add test for toml paser
    #14

--- tests/Integration/Config/ConfigLoaderTest.php
+++ tests/Integration/Config/ConfigLoaderTest.php
@@ -105,12 +105,14 @@ class ConfigLoaderTest extends LaravelTestCase
         $this->assertSame('http://www.example.com/~username', $config->get('ini.second_section.URL'));
         $this->assertSame('mail.test.com', $config->get('json.email.server'));
         $this->assertSame('tests/_envs', $config->get('yaml.paths.envs'));
+        $this->assertSame('192.168.1.1', $config->get('toml.database.server'));
 
         // Assert nested level
         $this->assertSame('Nested message', $config->get('nested.array.message'));
         $this->assertSame(true, $config->get('nested.ini.first_section.my_state'));
         $this->assertSame('Rick Johnson', $config->get('nested.json.name'));
         $this->assertSame('James Brown Jr.', $config->get('nested.yaml.actor'));
+        $this->assertSame('10.0.0.5', $config->get('nested.toml.servers.omega.ip'));
 
         // Assert deep nested ... (just to be sure!)
         $this->assertSame('Deep nested message', $config->get('nested.deep.module.message'));
