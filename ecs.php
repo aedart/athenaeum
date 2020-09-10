86502d5c2ad7818fca08736c4bd2c735692410c1
Alin Eugen Deac <aedart@gmail.com>
2020-09-10 20:33:40 +0200

    Fix code style not triggering
    After migrating to *.php file, no paths were provided. Also forgot to migrate custom skip.

--- ecs.php
+++ ecs.php
@@ -21,4 +21,23 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         SetList::CLEAN_CODE,
         SetList::PSR_12,
     ]);
+
+    // Set paths
+    $parameters->set(Option::PATHS, [
+        'packages',
+        'tests/Helpers',
+        'tests/TestCases',
+        'tests/Integration',
+        'tests/Unit',
+        //'src',
+    ]);
+
+    // Skip
+    $parameters->set(Option::SKIP, [
+
+        SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class . '.UnusedProperty' => [
+            # The "private int $height" is used by Dto, via magic methods...
+            'tests/Helpers/Dummies/Properties/Accessibility/Person.php'
+        ]
+    ]);
 };
