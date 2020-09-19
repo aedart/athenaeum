d6a686c577fe55fd7314be79853f98f2b580fa88
Steffen Brummer <steffen@kernelguy.dk>
2020-09-19 17:04:42 +0200

    Started class

--- packages/Utils/src/Dates/Duration.php
+++ packages/Utils/src/Dates/Duration.php
@@ -2,7 +2,29 @@
 
 namespace Aedart\Utils\Dates;
 
-// TODO
+
 class Duration
 {
+    /**
+     * @var \DateInterval
+     */
+    protected $duration;
+
+    static public function from($time) : self {
+        return new static($time);
+    }
+
+    public function __construct($time) {
+        
+        if ($time instanceof \DateTime) {
+            $dt = new \DateTime("@0");
+            $this->duration = $dt->diff($time);
+        }
+        elseif ($time instanceof \DateInterval) {
+            $this->duration = $time;
+        }
+        elseif (is_integer($time)) {
+            $this->duration = new \DateInterval("PT$timeS");
+        }
+    }
 }
