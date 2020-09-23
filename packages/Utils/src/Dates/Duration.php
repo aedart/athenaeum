96f79c50618089803d9dffba05e412911fe06d9f
Steffen Brummer <steffen@kernelguy.dk>
2020-09-23 21:10:18 +0200

    Add add and subtract, completed tests.

--- packages/Utils/src/Dates/Duration.php
+++ packages/Utils/src/Dates/Duration.php
@@ -3,36 +3,78 @@
 namespace Aedart\Utils\Dates;
 
 
+/**
+ * Duration class to easen usage of the DateInterval class.
+ *
+ * @author steffen
+ *
+ */
 class Duration
 {
     /**
+     * Note:
+     *  The DateInterval object is a bit funny:
+     *   It has internal properties for year, month, day, hours, minutes, seconds and microseconds.
+     *   They are populated as one would expect if instantiated from a DateTime or DateTime->diff() call.
+     *   But if instantiated from seconds, all seconds are simply put into the seconds property.
+     *   E.g. dd(new \DateInterval('PT4200S'));
+     *   The same will happen if instatiated from any other direct value.
      * @var \DateInterval
      */
     protected $duration;
 
+    /**
+     * Create instance from either a DateTime, a DateInterval or integer seconds
+     *
+     * @param mixed $time
+     * @return self
+     */
     static public function from($time) : self
     {
         return new static($time);
     }
 
+    /**
+     * Create instance from time string.
+     *
+     * @param string $timeStr
+     * @return self
+     */
     static public function fromString(string $timeStr) : self
     {
         return new static(strtotime($timeStr));
     }
 
+    /**
+     * Create instance from integer seconds
+     *
+     * @param int $seconds
+     * @return self
+     */
     static public function fromSeconds(int $seconds) : self
     {
         return new static($seconds);
     }
 
+    /**
+     * Create instance from difference of two DateTime objects.
+     *
+     * @param \DateTime $start
+     * @param \DateTime $stop
+     * @return self
+     */
     static public function fromDifference(\DateTime $start, \DateTime $stop) : self
     {
         return new static($start->diff($stop));
     }
 
-    public function __construct($time)
+    /**
+     * Constructor that cane take a DateTime, a DateInterval or integer seconds
+     *
+     * @param mixed|null $time
+     */
+    public function __construct($time=null)
     {
-
         if ($time instanceof \DateTime) {
             $dt = new \DateTime("@0");
             $this->duration = $dt->diff($time);
@@ -43,6 +85,66 @@ class Duration
         elseif (is_integer($time)) {
             $this->duration = new \DateInterval("PT{$time}S");
         }
+    }
+
+    /**
+     * Add another Duration to this.
+     *
+     * @param Duration $duration
+     * @return self
+     */
+    public function add(Duration $duration) : self
+    {
+        $temp = new \DateTime('@0');
+        $temp->add($this->duration);
+
+        $temp->add($duration->duration);
+
+        $this->duration = $temp->diff(new \DateTime('@0'));
+
+        return $this;
+    }
+
+    /**
+     * Subtract another Duration from this.
+     *
+     * @param Duration $duration
+     * @return self
+     */
+    public function subtract(Duration $duration) : self
+    {
+        $temp = new \DateTime('@0');
+        $temp->add($this->duration);
+
+        $duration->duration->invert = 1;
+        $temp->add($duration->duration);
+
+        $this->duration = $temp->diff(new \DateTime('@0'));
+
+        return $this;
+    }
+
+    /**
+     * Return duration as integer seconds.
+     *
+     * @return int
+     */
+    public function asSeconds() : int
+    {
+        return (($this->duration->d * 24 * 3600)
+            + ($this->duration->h * 3600)
+            + ($this->duration->i * 60)
+            + ($this->duration->s));
+    }
+
+    /**
+     * Return duration as decimal seconds.
+     *
+     * @return float
+     */
+    public function asFloatSeconds() : float
+    {
+        return $this->asSeconds() + $this->duration->f;
     }
 
     /**
--- tests/Unit/Utils/Dates/DurationTest.php
+++ tests/Unit/Utils/Dates/DurationTest.php
@@ -29,4 +29,83 @@ class DurationTest extends UnitTestCase
         $this->assertSame($duration->format("%S"), "42");
     }
 
+    /**
+     * @test
+     */
+    public function longDuration()
+    {
+        $seconds = 10*365*24*3600; // 10 years of seconds
+        $duration = Duration::fromSeconds($seconds);
+
+        $this->assertSame($duration->format("%S"), strval($seconds));
+        $this->assertSame($duration->asSeconds(), $seconds);
+        $this->assertSame($duration->asFloatSeconds(), floatval($seconds));
+    }
+
+    /**
+     * @test
+     */
+    public function instantiateFromDateInterval()
+    {
+        $duration = Duration::from(new \DateInterval('P10Y7DT4H5M34S'));
+
+        $this->assertSame($duration->format('%Y-%M-%D %H:%I:%S'), '10-00-07 04:05:34');
+    }
+
+    /**
+     * @test
+     */
+    public function instantiateFromDateTime()
+    {
+        $duration = Duration::from(new \DateTime('@'.(42*60)));
+
+        $this->assertSame($duration->asSeconds(), 42*60);
+    }
+
+    /**
+     * @test
+     */
+    public function instantiateFromString()
+    {
+        $duration = Duration::fromString('@'.(42*60));
+
+        $this->assertSame($duration->asSeconds(), 42*60);
+    }
+
+    /**
+     * @test
+     */
+    public function instantiateFromDifference()
+    {
+        $now = '2020-09-23';
+        $then = new \DateTime("$now - 5 hours - 6 minutes");
+        $when = new \DateTime("$now + 42 seconds + 23456 microseconds");
+
+        $duration = Duration::fromDifference($then, $when);
+
+        $this->assertSame($duration->asSeconds(), (5 * 3600) + (6 * 60) + 42);
+        $this->assertSame($duration->format('%Y-%M-%D %H:%I:%S.%F'), '00-00-00 05:06:42.023456');
+    }
+
+    /**
+     * @test
+     */
+    public function addInterval()
+    {
+        $duration = Duration::fromString('@40');
+        $duration->add(Duration::fromString('@2'));
+
+        $this->assertSame($duration->asSeconds(), 42);
+    }
+
+    /**
+     * @test
+     */
+    public function subtractInterval()
+    {
+        $duration = Duration::fromString('@44');
+        $duration->subtract(Duration::fromString('@2'));
+
+        $this->assertSame($duration->asSeconds(), 42);
+    }
 }
