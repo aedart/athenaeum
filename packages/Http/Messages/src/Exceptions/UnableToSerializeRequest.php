1b0f83d252dc353b04d9918f50502787debbb820
Alin Eugen Deac <aedart@gmail.com>
2020-09-27 10:58:41 +0200

    Add serialization exceptions
    #17

---
+++ packages/Http/Messages/src/Exceptions/UnableToSerializeRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Http\Messages\Exceptions;
+
+/**
+ * Unable To Serialize Request
+ *
+ * Should be thrown when unable to serialize a Http Request Message
+ *
+ * @see \Aedart\Http\Messages\Exceptions\HttpMessageSerializationException
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Messages\Exceptions
+ */
+class UnableToSerializeRequest extends HttpMessageSerializationException
+{
+
+}
