b12b98d353ed88f75d9b62ae7cca48afcba827ae
Alin Eugen Deac <aedart@gmail.com>
2020-10-04 14:01:43 +0200

    Add Http Message types
    Predefined types of request / response. To be used for debugging contexts.
    
    #17

--- packages/Http/Clients/src/Middleware/RequestResponseDebugging.php
+++ packages/Http/Clients/src/Middleware/RequestResponseDebugging.php
@@ -6,25 +6,15 @@ use Aedart\Contracts\Http\Clients\Middleware;
 use Aedart\Contracts\Http\Clients\Requests\Builder;
 use Aedart\Contracts\Http\Clients\Requests\Builders\HttpRequestBuilderAware;
 use Aedart\Contracts\Http\Clients\Requests\Handler;
-use Aedart\Contracts\Http\Messages\Serializers\Factory;
+use Aedart\Contracts\Http\Messages\Types;
 use Aedart\Http\Clients\Traits\HttpRequestBuilderTrait;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Symfony\Component\VarDumper\VarDumper;
 
 /**
- * Request Response Debugging
- *
- * Middleware requires a Http Message Serializer {@see Factory} to be
- * available via Service Container (or set manually).
- *
- * Also, Symfony's {@see VarDumper} MUST be available, for this
- * middleware to work as intended.
+ * Request Response Debugging Middleware
  *
  * WARNING: This middleware SHOULD NOT be used in a production environment!
- *
- * @see \Aedart\Contracts\Http\Messages\Serializers\Factory
- * @see \Symfony\Component\VarDumper\VarDumper
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Http\Clients\Middleware
@@ -34,16 +24,6 @@ class RequestResponseDebugging implements
     HttpRequestBuilderAware
 {
     use HttpRequestBuilderTrait;
-
-    /**
-     * Request type Http Message
-     */
-    public const TYPE_REQUEST = 'request';
-
-    /**
-     * Response type Http Message
-     */
-    public const TYPE_RESPONSE = 'response';
 
     /**
      * {@inheritDoc}
@@ -58,13 +38,13 @@ class RequestResponseDebugging implements
         $callback = $builder->debugCallback();
 
         // Invoke debug for request...
-        $callback(static::TYPE_REQUEST, $request, $builder);
+        $callback(Types::TYPE_REQUEST, $request, $builder);
 
         // Perform request and obtain response.
         $response = $handler->handle($request);
 
         // Invoke debug for response...
-        $callback(static::TYPE_RESPONSE, $response, $builder);
+        $callback(Types::TYPE_RESPONSE, $response, $builder);
 
         return $response;
     }
