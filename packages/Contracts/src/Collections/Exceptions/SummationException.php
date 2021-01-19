76a3620b96ee2561109474c31b3d841ff9e1f8b0
Alin Eugen Deac <aedart@gmail.com>
2021-01-19 17:59:52 +0100

    Add summation exception
    #27

---
+++ packages/Contracts/src/Collections/Exceptions/SummationException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Contracts\Collections\Exceptions;
+
+use Throwable;
+
+/**
+ * Summation Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Collections\Exceptions
+ */
+interface SummationException extends Throwable
+{
+
+}
