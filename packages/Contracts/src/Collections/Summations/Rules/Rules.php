92bd37c403fa9f51493a12a89e03b8cb0bbcdc4c
Alin Eugen Deac <aedart@gmail.com>
2021-01-20 18:49:46 +0100

    Add immutable collection of processing rules interface
    With this type of object, a single item may actually be processed by multiple rules, rather than just a single rule.
    
    #27

---
+++ packages/Contracts/src/Collections/Summations/Rules/Rules.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Aedart\Contracts\Collections\Summations\Rules;
+
+use Aedart\Contracts\Collections\Summation;
+use Countable;
+use Illuminate\Contracts\Support\Arrayable;
+use IteratorAggregate;
+
+/**
+ * Summation Processing Rules Collection
+ *
+ * An immutable collection of processing rules for assigned item.
+ *
+ * @see ProcessingRule
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Collections\Summations\Rules
+ */
+interface Rules extends
+    Countable,
+    Arrayable,
+    IteratorAggregate
+{
+    /**
+     * Process item and return the resulting summation
+     *
+     * Method invokes all assigned processing rules
+     *
+     * @return Summation Summation with results from having processed
+     *                   the assigned item
+     */
+    public function process(): Summation;
+
+    /**
+     * Returns this collection's assigned processing
+     * rules
+     *
+     * @return ProcessingRule[]
+     */
+    public function rules(): array;
+
+    /**
+     * Returns the item to be processed by this
+     * collection's processing rules
+     *
+     * @return mixed
+     */
+    public function item();
+
+    /**
+     * Returns the summation assigned for this
+     * collection of processing rules
+     *
+     * @return Summation
+     */
+    public function summation(): Summation;
+}
