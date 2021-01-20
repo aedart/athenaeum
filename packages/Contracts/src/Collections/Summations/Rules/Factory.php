79284c4f6f31fd27f6db721e6520e91395f40c01
Alin Eugen Deac <aedart@gmail.com>
2021-01-20 18:56:01 +0100

    Add rules collection factory interface
    #27

---
+++ packages/Contracts/src/Collections/Summations/Rules/Factory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Aedart\Contracts\Collections\Summations\Rules;
+
+use Aedart\Contracts\Collections\Exceptions\SummationException;
+use Aedart\Contracts\Collections\Summation;
+
+/**
+ * Summation Rules Collection Factory
+ *
+ * Responsible for determining processing rules to use for
+ * an item and create a matching collection with appropriate
+ * rules.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Collections\Summations\Rules
+ */
+interface Factory
+{
+    /**
+     * Creates a collection of processing rules to process
+     * given item.
+     *
+     * @param mixed $item
+     * @param  Summation  $summation
+     *
+     * @return Rules
+     *
+     * @throws SummationException
+     */
+    public function make($item, Summation $summation): Rules;
+}
