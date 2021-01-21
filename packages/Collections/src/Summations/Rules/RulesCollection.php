225fa0189b959e9d1b22f50fdf4b1d420f6572db
Alin Eugen Deac <aedart@gmail.com>
2021-01-21 20:01:46 +0100

    Add Processing Rules Collection
    #27

---
+++ packages/Collections/src/Summations/Rules/RulesCollection.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Aedart\Collections\Summations\Rules;
+
+use Aedart\Contracts\Collections\Summation;
+use Aedart\Contracts\Collections\Summations\Rules\ProcessingRule;
+use Aedart\Contracts\Collections\Summations\Rules\Rules;
+use ArrayIterator;
+
+/**
+ * Rules Collection
+ *
+ * @see \Aedart\Contracts\Collections\Summations\Rules\Rules
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Collections\Summations\Rules
+ */
+class RulesCollection implements Rules
+{
+    /**
+     * The item in question
+     *
+     * @var mixed
+     */
+    protected $item;
+
+    /**
+     * The Summation Collection instance
+     *
+     * @var Summation
+     */
+    protected Summation $summation;
+
+    /**
+     * List of processing rules
+     *
+     * @var ProcessingRule[]
+     */
+    protected array $rules = [];
+
+    /**
+     * RulesCollection constructor.
+     *
+     * @param  mixed $item
+     * @param  ProcessingRule[]  $rules
+     * @param  Summation  $summation
+     */
+    public function __construct($item, array  $rules, Summation $summation)
+    {
+        $this->item = $item;
+        $this->rules = $rules;
+        $this->summation = $summation;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(): Summation
+    {
+        $item = $this->item();
+        $rules = $this->rules();
+        $summation = $this->summation();
+
+        foreach ($rules as $rule) {
+            $summation = $rule->process($item, $summation);
+        }
+
+        return $summation;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function item()
+    {
+        return $this->item;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function summation(): Summation
+    {
+        return $this->summation;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count()
+    {
+        return count($this->rules);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->rules);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return $this->rules();
+    }
+}
