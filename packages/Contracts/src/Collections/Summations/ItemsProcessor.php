ed7e6dac7f6c25cf6862ba7c775a67c271d518e0
Alin Eugen Deac <aedart@gmail.com>
2021-01-23 11:26:22 +0100

    Change processor, allow a summation instance to be given
    In some situations, an existing summation might be used instead of a new one.
    
    #27

--- packages/Contracts/src/Collections/Summations/ItemsProcessor.php
+++ packages/Contracts/src/Collections/Summations/ItemsProcessor.php
@@ -19,12 +19,14 @@ interface ItemsProcessor
      * Process given items, by using processing rules created from
      * given factory.
      *
-     * @param array|Traversable $items
+     * @param  array|Traversable  $items
      * @param  ProcessingRulesFactory  $factory
+     * @param  Summation|null  $summation  [optional] If none given, then a new Summation instance
+     *                                     will be created.
      *
      * @return Summation
      *
      * @throws SummationCollectionException
      */
-    public static function process($items, ProcessingRulesFactory $factory): Summation;
+    public static function process($items, ProcessingRulesFactory $factory, ?Summation $summation = null): Summation;
 }
