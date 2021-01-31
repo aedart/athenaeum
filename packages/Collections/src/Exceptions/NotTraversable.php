25bbc91f6fffe83f72d7460f1332b1800dcc7d95
Alin Eugen Deac <aedart@gmail.com>
2021-01-31 10:45:26 +0100

    Add not traversable exception
    #27

---
+++ packages/Collections/src/Exceptions/NotTraversable.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Aedart\Collections\Exceptions;
+
+/**
+ * Not Traversable Exception
+ *
+ * Should be thrown when a list of none-traversable items is attempted
+ * processed.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Collections\Exceptions
+ */
+class NotTraversable extends SummationCollectionException
+{
+
+}
