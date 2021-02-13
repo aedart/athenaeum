3fd7ba05c4cc733b39810de5f642f1be0fad2842
Alin Eugen Deac <aedart@gmail.com>
2021-02-13 16:36:14 +0100

    Add integration test for items processor component
    #27

---
+++ tests/Helpers/Dummies/Collections/Summations/Rules/PauseRule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules;
+
+use Aedart\Contracts\Collections\Summation;
+use Aedart\Contracts\Collections\Summations\Rules\Determinable;
+use Aedart\Contracts\Collections\Summations\Rules\ProcessingRule;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+
+/**
+ * Pause Rule
+ *
+ * FOR TESTING PURPOSES ONLY
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules
+ */
+class PauseRule implements
+    ProcessingRule,
+    Determinable
+{
+    /**
+     * @inheritDoc
+     */
+    public function canProcess($item): bool
+    {
+        return isset($item['activity']) && $item['activity'] === 'pause';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process($item, Summation $summation): Summation
+    {
+        $result = $summation->decrease('points', 1);
+
+        ConsoleDebugger::output('Pause Rule (-1 points).', $item['activity'], 'Points: ' . $result->get('points'));
+
+        return $result;
+    }
+}
---
+++ tests/Integration/Collections/Summations/ItemsProcessorTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Aedart\Tests\Integration\Collections\Summations;
+
+use Aedart\Collections\Summations\ItemsProcessor;
+use Aedart\Collections\Summations\Rules\NullProcessingRule;
+use Aedart\Contracts\Collections\Summation;
+use Aedart\Contracts\Collections\Summations\ItemsProcessor as ItemsProcessorInterface;
+use Aedart\Contracts\Collections\Summations\Rules\ProcessingRule;
+use Aedart\Contracts\Collections\Summations\Rules\Repository;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Testing\TestCases\IntegrationTestCase;
+use Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules\AmountRule;
+use Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules\PauseRule;
+use Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules\RunningRule;
+use Aedart\Tests\Helpers\Dummies\Collections\Summations\Rules\WalkingRule;
+
+/**
+ * ItemsProcessorTest
+ *
+ * @group collections
+ * @group summations
+ * @group items-processor
+ * 
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Collections\Summations
+ */
+class ItemsProcessorTest extends IntegrationTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Creates a new Items Processor instance
+     *
+     * @param  ProcessingRule[]|string[]|Repository  $rules Processing Rules instances, class paths or Repository of
+     *                                                processing rules.
+     * @param  Summation|null  $summation  [optional]
+     *
+     * @return ItemsProcessorInterface
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function makeProcessor($rules, ?Summation $summation = null): ItemsProcessorInterface
+    {
+        return new ItemsProcessor($rules, $summation);
+    }
+
+    /**
+     * Returns a list of "activity" records
+     *
+     * @see makeActivityRecord
+     *
+     * @return array
+     */
+    public function activities(): array
+    {
+        // See rules to be applied in tests...
+        return [
+            $this->makeActivityRecord('running'), // 5 points
+            $this->makeActivityRecord('walking'), // 2 points
+            $this->makeActivityRecord('pause'),   // -1 points
+            $this->makeActivityRecord('running'), // 5 points
+            $this->makeActivityRecord('running'), // 5 points
+            $this->makeActivityRecord('walking'), // 2 points
+            $this->makeActivityRecord('pause'),   // -1 points
+            $this->makeActivityRecord('running'), // 5 points
+            $this->makeActivityRecord('running'), // 5 points
+            $this->makeActivityRecord('walking'), // 2 points (29 points total)
+        ];
+    }
+
+    /**
+     * Returns an "activity record" of sorts.
+     *
+     * Intended to represent a "typical" record to process, from
+     * an external data source, like a database or csv file.
+     *
+     * @param  string  $activity
+     *
+     * @return string[]
+     */
+    public function makeActivityRecord(string $activity): array
+    {
+        return [
+            'activity' => $activity,
+        ];
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     */
+    public function canObtainInstance()
+    {
+        $processor = $this->makeProcessor([ new NullProcessingRule() ]);
+
+        $this->assertNotNull($processor);
+    }
+
+    /**
+     * @test
+     */
+    public function canProcessItems()
+    {
+        // Create new processor with given rules (testing if they can be resolved...)
+        $processor = $this->makeProcessor([
+            WalkingRule::class,
+            new AmountRule(),
+            RunningRule::class,
+            new PauseRule()
+        ]);
+
+        // Process items... apply before / after callbacks
+        $results = $processor
+            ->before(function(Summation $summation) {
+                return $summation
+                    ->set('amount', 0)
+                    ->set('points', 0);
+            })
+            ->after(function(Summation $summation) {
+                return $summation
+                    ->set('average', function($value, Summation $summation) {
+                        return $summation->get('points') / $summation->get('amount');
+                    });
+            })
+            ->process($this->activities());
+
+        ConsoleDebugger::output($results);
+
+        $this->assertSame(10, $results->get('amount'));
+        $this->assertSame(29, $results->get('points'));
+        $this->assertSame(2.9, $results->get('average'));
+    }
+}
