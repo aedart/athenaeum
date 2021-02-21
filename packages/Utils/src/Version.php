b69b01b1dce2a34d674e7ea318a0b09cdf0d7d7a
Alin Eugen Deac <aedart@gmail.com>
2021-02-21 12:40:02 +0100

    Change version, add application version method
    Util is now able to determine the current application's version, much more precise than the "PrettyVersions" component, since it relies on git instead of composer.
    
    #25

--- packages/Utils/src/Version.php
+++ packages/Utils/src/Version.php
@@ -21,6 +21,36 @@ class Version
     protected static array $versions = [];
 
     /**
+     * Returns the application's version
+     *
+     * @return \Jean85\Version
+     */
+    public static function application()
+    {
+        $name = 'root@' . PrettyVersions::getRootPackageName();
+
+        if (isset(static::$versions[$name])) {
+            return static::$versions[$name];
+        }
+
+        // Obtain the current commit, tag and tag's commit from git
+        $commit = trim(shell_exec('git rev-parse HEAD'));
+        $tag = trim(shell_exec('git describe --tags $(git rev-list --tags --max-count=1)'));
+        $tagCommit = trim(shell_exec("git rev-list -n 1 {$tag}"));
+
+        // Append a "-dev" if current commit does not match the tag's commit
+        if ($commit !== $tagCommit) {
+            $tag = "{$tag}-dev";
+        }
+
+        // Usually, we should be able to obtain the application's version via "PrettyVersions::getRootPackageVersion".
+        // But it's seems sometimes to be unreliable - sadly - so we just create a new version instance manually,
+        // using the information available from git.
+
+        return static::$versions[$name] = new \Jean85\Version($name, "{$tag}@{$commit}");
+    }
+
+    /**
      * Returns the version of the given package
      *
      * @see \Jean85\PrettyVersions::getVersion
