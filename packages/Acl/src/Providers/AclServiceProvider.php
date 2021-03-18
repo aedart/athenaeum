7d2ebeca71b578828245ca66d8ec17767eecd898
alin <alin@rspsystems.com>
2021-03-18 11:17:27 +0100

    Add service provider (incomplete)

---
+++ packages/Acl/src/Providers/AclServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Aedart\Acl\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Acl Service Provider
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Acl\Providers
+ */
+class AclServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap this service
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../configs/acl.php' => config_path('acl.php')
+        ], 'config');
+    }
+}
