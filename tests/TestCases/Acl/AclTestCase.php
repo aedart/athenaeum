4945f15ee4f6e37f8c333e6881f66b6239b63c93
alin <alin@rspsystems.com>
2021-03-18 12:53:10 +0100

    Change test-case, add setup for acl config and database migrations

--- tests/TestCases/Acl/AclTestCase.php
+++ tests/TestCases/Acl/AclTestCase.php
@@ -2,7 +2,10 @@
 
 namespace Aedart\Tests\TestCases\Acl;
 
+use Aedart\Config\Providers\ConfigLoaderServiceProvider;
+use Aedart\Config\Traits\ConfigLoaderTrait;
 use Aedart\Testing\TestCases\LaravelTestCase;
+use Codeception\Configuration;
 
 /**
  * AclTestCase
@@ -12,12 +15,82 @@ use Aedart\Testing\TestCases\LaravelTestCase;
  */
 abstract class AclTestCase extends LaravelTestCase
 {
+    use ConfigLoaderTrait;
+
     /*****************************************************************
      * Setup
      ****************************************************************/
 
+    /**
+     * @inheritDoc
+     */
+    protected function _before()
+    {
+        parent::_before();
+
+        $this->getConfigLoader()
+            ->setDirectory($this->configDir())
+            ->load();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ConfigLoaderServiceProvider::class,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+    }
+
+    /*****************************************************************
+     * Paths
+     ****************************************************************/
+
+    /**
+     * Returns path to configuration directory
+     *
+     * @return string
+     */
+    public function configDir(): string
+    {
+        return Configuration::dataDir() . 'configs/acl/';
+    }
+
+    /**
+     * Returns paths to where database migrations are located
+     *
+     * @return string
+     */
+    public function migrationsDir(): string
+    {
+        return __DIR__ . '/../../../packages/Acl/database/migrations';
+    }
+
     /*****************************************************************
      * Helpers
      ****************************************************************/
+
+    /**
+     * Runs the database migrations for the ACL package
+     *
+     * @return self
+     */
+    public function installAclMigrations(): self
+    {
+        $this->loadMigrationsFrom(
+            $this->migrationsDir()
+        );
+
+        return $this;
+    }
 
 }
