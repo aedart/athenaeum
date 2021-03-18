eed9a114b3ee5c405af024c27c77b9d48cfb263e
alin <alin@rspsystems.com>
2021-03-18 10:54:54 +0100

    Add Acl test-case abstraction (incomplete)

---
+++ tests/TestCases/Acl/AclTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aedart\Tests\TestCases\Acl;
+
+use Aedart\Testing\TestCases\LaravelTestCase;
+
+/**
+ * AclTestCase
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\TestCases\Acl
+ */
+abstract class AclTestCase extends LaravelTestCase
+{
+    /*****************************************************************
+     * Setup
+     ****************************************************************/
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+}
