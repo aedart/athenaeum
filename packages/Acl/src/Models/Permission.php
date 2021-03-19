4fadcd34b53d007d0ee83f54bc717e88079f83a9
alin <alin@rspsystems.com>
2021-03-19 11:10:58 +0100

    Replace eloquent inheritance with new abstraction

--- packages/Acl/src/Models/Permission.php
+++ packages/Acl/src/Models/Permission.php
@@ -5,9 +5,11 @@ namespace Aedart\Acl\Models;
 use Aedart\Acl\Models\Permissions\Group;
 use Aedart\Acl\Traits\AclComponentsTrait;
 use Aedart\Contracts\Database\Models\Sluggable;
+use Aedart\Database\Model;
 use Aedart\Database\Models\Concerns;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -22,6 +24,7 @@ use Illuminate\Support\Carbon;
  * @property Carbon $updated_at Date and time of when record was last updated
  *
  * @property Group $group The group this permission belongs to
+ * @property Role[]|Collection $roles The roles that have been granted this permission
  *
  * @author Alin Eugen Deac <ade@rspsystems.com>
  * @package Aedart\Acl\Models
@@ -60,5 +63,23 @@ class Permission extends Model implements Sluggable
     public function group(): BelongsTo
     {
         return $this->belongsTo($this->aclPermissionsGroupModel());
+    }
+
+    /**
+     * The roles that are granted this permission
+     *
+     * @return BelongsToMany
+     */
+    public function roles(): BelongsToMany
+    {
+        /** @var Model $role */
+        $role = $this->aclRoleModel()::newInstance();
+
+        return $this->belongsToMany(
+            $this->aclRoleModel(),
+            $this->aclTable('roles_permissions'),
+            $this->getForeignKey(),
+            $role->getForeignKey()
+        );
     }
 }
--- packages/Acl/src/Models/Role.php
+++ packages/Acl/src/Models/Role.php
@@ -4,8 +4,10 @@ namespace Aedart\Acl\Models;
 
 use Aedart\Acl\Traits\AclComponentsTrait;
 use Aedart\Contracts\Database\Models\Sluggable;
+use Aedart\Database\Model;
 use Aedart\Database\Models\Concerns;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
@@ -19,6 +21,8 @@ use Illuminate\Support\Carbon;
  * @property Carbon $created_at Date and time of when record was created
  * @property Carbon $updated_at Date and time of when record was last updated
  * @property Carbon|null $deleted_at Evt. date and time of when record was soft-deleted
+ *
+ * @property Permission[]|Collection $permissions Permissions that are granted to this role
  *
  * @author Alin Eugen Deac <ade@rspsystems.com>
  * @package Aedart\Acl\Models
@@ -47,6 +51,89 @@ class Role extends Model implements Sluggable
     }
 
     /*****************************************************************
+     * Operations
+     ****************************************************************/
+
+    /**
+     * Grant given permissions to this role
+     *
+     * @param string|int|\Aedart\Acl\Models\Permission ...$permissions Permission slugs, ids or Permission instances
+     *
+     * @return self
+     */
+    public function grantPermissions(...$permissions)
+    {
+        // TODO: Obtain permission ids
+
+        //return $this->permissions()->withTimestamps()->save($permission);
+    }
+
+    /**
+     * Revokes all existing permissions and grants given permissions
+     * to this role
+     *
+     * @param string|int|\Aedart\Acl\Models\Permission ...$permissions Permission slugs, ids or Permission instances
+     *
+     * @return self
+     */
+    public function syncPermissions(...$permissions)
+    {
+        $this->permissions()->detach();
+
+        return $this->grantPermissions($permissions);
+    }
+
+    /**
+     * Revoke all given permissions
+     *
+     * @param string|int|\Aedart\Acl\Models\Permission ...$permissions Permission slugs, ids or Permission instances
+     *
+     * @return self
+     */
+    public function revokePermissions(...$permissions)
+    {
+        // $this->permissions()->detach($ids); // TODO: Obtain permissions ids
+
+        return $this;
+    }
+
+    /*****************************************************************
      * Relations
      ****************************************************************/
+
+    /**
+     * Permissions that are granted to this role
+     *
+     * @return BelongsToMany
+     */
+    public function permissions(): BelongsToMany
+    {
+        /** @var Model $permission */
+        $permission = $this->aclPermissionsModel()::newInstance();
+
+        return $this->belongsToMany(
+            $this->aclRoleModel(),
+            $this->aclTable('roles_permissions'),
+            $this->getForeignKey(),
+            $permission->getForeignKey()
+        );
+    }
+
+    /**
+     * Users that have this role are assigned to them
+     *
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        /** @var Model $user */
+        $user = $this->aclUserModel()::newInstance();
+
+        return $this->belongsToMany(
+            $this->aclUserModel(),
+            $this->aclTable('users_roles'),
+            $this->getForeignKey(),
+            $user->getForeignKey()
+        );
+    }
 }
