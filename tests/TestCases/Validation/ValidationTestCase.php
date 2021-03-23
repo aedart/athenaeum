ea93db9f6ed7c29628eaa1a96e5d4fb598280fe3
alin <alin@rspsystems.com>
2021-03-23 08:58:49 +0100

    Change test-case, add helper method for creating new validator instance

--- tests/TestCases/Validation/ValidationTestCase.php
+++ tests/TestCases/Validation/ValidationTestCase.php
@@ -2,8 +2,10 @@
 
 namespace Aedart\Tests\TestCases\Validation;
 
+use Aedart\Support\Helpers\Validation\ValidatorFactoryTrait;
 use Aedart\Testing\TestCases\LaravelTestCase;
 use Aedart\Validation\Providers\ValidationServiceProvider;
+use Illuminate\Contracts\Validation\Validator;
 
 /**
  * Validation Test-Case
@@ -13,6 +15,12 @@ use Aedart\Validation\Providers\ValidationServiceProvider;
  */
 abstract class ValidationTestCase extends LaravelTestCase
 {
+    use ValidatorFactoryTrait;
+
+    /*****************************************************************
+     * Setup
+     ****************************************************************/
+
     /**
      * {@inheritdoc}
      */
@@ -21,5 +29,24 @@ abstract class ValidationTestCase extends LaravelTestCase
         return [
             ValidationServiceProvider::class
         ];
+    }
+
+    /*****************************************************************
+     * Helper
+     ****************************************************************/
+
+    /**
+     * Creates a new validator instance
+     *
+     * @param array $data
+     * @param array $rules
+     * @param array $messages [optional]
+     * @param array $customAttributes [optional]
+     *
+     * @return Validator
+     */
+    public function validator(array $data, array $rules, array $messages = [], array $customAttributes = []): Validator
+    {
+        return $this->getValidatorFactory()->make($data, $rules, $messages, $customAttributes);
     }
 }
