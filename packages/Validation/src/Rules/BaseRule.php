f332d4782c2d3384dc393d2c9e58fada17c08448
alin <alin@rspsystems.com>
2021-03-23 08:48:47 +0100

    Add validation rule abstraction

---
+++ packages/Validation/src/Rules/BaseRule.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Aedart\Validation\Rules;
+
+use Aedart\Contracts\Support\Helpers\Translation\TranslatorAware;
+use Aedart\Support\Helpers\Translation\TranslatorTrait;
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * Base Validation Rule
+ *
+ * Abstraction that offers common utility methods for
+ * validation rules.
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Acl\Rules
+ */
+abstract class BaseRule implements
+    Rule,
+    TranslatorAware
+{
+    use TranslatorTrait;
+
+    /**
+     * Get the translation for a given key.
+     *
+     * Method ensures to vendor-prefix the given translation key, so that
+     * the package's translations are used.
+     *
+     * @param string $key
+     * @param string[] $replace [optional]
+     * @param string|null $locale [optional]
+     *
+     * @return string
+     */
+    protected function trans($key, array $replace = [], $locale = null): string
+    {
+        // Vendor prefix key
+        $key = "athenaeum-validation::messages.{$key}";
+
+        return $this->getTranslator()->get($key, $replace, $locale);
+    }
+}
