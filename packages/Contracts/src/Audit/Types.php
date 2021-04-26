8e2680675d9faeb707e35d9ca46e6933c4f59181
alin <alin@rspsystems.com>
2021-04-26 11:52:38 +0200

    Add predefined audit trail entry types

---
+++ packages/Contracts/src/Audit/Types.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Aedart\Contracts\Audit;
+
+/**
+ * Audit Trail Entry Types
+ *
+ * Contains a few predefined Audit Trail Entry types.
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Audit
+ */
+interface Types
+{
+    /**
+     * Model has been created
+     */
+    public const CREATED = 'created';
+
+    /**
+     * Model has been updated
+     */
+    public const UPDATED = 'updated';
+
+    /**
+     * Model has been deleted
+     */
+    public const DELETED = 'deleted';
+
+    /**
+     * Model has been restored (from soft-deleted state)
+     */
+    public const RESTORED = 'restored';
+
+    /**
+     * Model has been force-deleted
+     */
+    public const FORCE_DELETED = 'force-deleted';
+}
