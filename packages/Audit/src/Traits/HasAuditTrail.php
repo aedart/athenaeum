89f3d39d33c6b73d193ac3ad75460f966bfeea89
alin <alin@rspsystems.com>
2021-04-26 15:01:13 +0200

    Change has audit trail, add data extraction for audit entry
    Trait now respects Laravel's own "hidden" attributes, as well as a custom set of "hidden for audit" attributes.

--- packages/Audit/src/Traits/HasAuditTrail.php
+++ packages/Audit/src/Traits/HasAuditTrail.php
@@ -27,6 +27,13 @@ trait HasAuditTrail
     use AuditTrailConfiguration;
 
     /**
+     * The attributes that should be hidden for Audit Trail Entries
+     *
+     * @var string[]
+     */
+    protected $hiddenInAuditTrail = [];
+
+    /**
      * Boots has audit trail functionality for this model
      *
      * @return void
@@ -40,12 +47,104 @@ trait HasAuditTrail
     }
 
     /*****************************************************************
+     * Original & Changed data
+     ****************************************************************/
+
+    /**
+     * Returns the original data (attributes) to be saved
+     * in Audit Trail Entry
+     *
+     * @return array|null
+     */
+    public function originalData(): ?array
+    {
+        return $this->filterAuditData($this->getOriginal());
+    }
+
+    /**
+     * Returns the changed data (attributes) to be saved
+     * in Audit Trail Entry
+     *
+     * @return array|null
+     */
+    public function changedData(): ?array
+    {
+        return $this->filterAuditData($this->getAttributes());
+    }
+
+    /**
+     * Filters Audit Trail data
+     *
+     * Method removes any attribute that are not intended to be
+     * part of an Audit Trail Entry.
+     *
+     * @see getHiddenForAudit
+     * @see \Illuminate\Database\Eloquent\Concerns\HidesAttributes::getHidden
+     *
+     * @param array|null $attributes [optional]
+     *
+     * @return array|null
+     */
+    public function filterAuditData(?array $attributes = null): ?array
+    {
+        if (empty($attributes)) {
+            return null;
+        }
+
+        return collect($attributes)
+
+            // Filter off attributes not to be included in
+            // Audit Trail Entry
+            ->except($this->getHiddenForAudit())
+
+            // Filter off regular hidden attributes
+            ->except($this->getHidden())
+
+            ->toArray();
+    }
+
+    /**
+     * Get attributes that are hidden for Audit Trail Entries
+     *
+     * @return string[]
+     */
+    public function getHiddenForAudit(): array
+    {
+        return $this->hiddenInAuditTrail;
+    }
+
+    /**
+     * Mark given attributes hidden for Audit Trail Entries
+     *
+     * @param string|string[]|null $attributes
+     *
+     * @return self
+     */
+    public function makeHiddenForAudit($attributes)
+    {
+        $attributes = is_array($attributes)
+            ? $attributes
+            : func_get_args();
+
+        $this->hiddenInAuditTrail = array_merge(
+            $this->hiddenInAuditTrail,
+            $attributes
+        );
+
+        return $this;
+    }
+
+    /*****************************************************************
      * Audit Trail Message
      ****************************************************************/
 
     /**
      * Returns evt. message to be stored in Audit Trail Entry, for
      * the given entry type
+     *
+     * NOTE: This method is invoked BEFORE "model has changed" event is
+     * dispatched. You can therefore use this method to access resources
+     * that are NOT necessarily available during event listener processing.
      *
      * @param string $type Audit Trail Entry type, e.g. created, updated, deleted... etc
      *
