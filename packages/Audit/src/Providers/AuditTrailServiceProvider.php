a1a31d2ce90a7e8fa6a699aa5869646bcdcd46d9
alin <alin@rspsystems.com>
2021-04-26 09:01:25 +0200

    Add Audit trail service provider (incomplete)

---
+++ packages/Audit/src/Providers/AuditTrailServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Aedart\Audit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Audit Trail Service Provider
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Audit\Providers
+ */
+class AuditTrailServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap this service
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../configs/audit-trail.php' => config_path('audit-trail.php')
+        ], 'config');
+
+//        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+}
