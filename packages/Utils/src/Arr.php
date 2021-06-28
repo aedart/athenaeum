44e73712acf05fb7315445ac5c4cee1d95c6b522
alin <alin@rspsystems.com>
2021-06-28 14:54:10 +0200

    Add difference associative method
    Able to compute difference of multi-dimensional arrays.

--- packages/Utils/src/Arr.php
+++ packages/Utils/src/Arr.php
@@ -4,6 +4,7 @@
 namespace Aedart\Utils;
 
 use Illuminate\Support\Arr as ArrBase;
+use InvalidArgumentException;
 
 /**
  * Array Utility
@@ -51,5 +52,30 @@ class Arr extends ArrBase
         }
 
         return $output;
+    }
+
+    /**
+     * Computes the difference of multi-dimensional arrays
+     *
+     * @param array $array The array to compare from
+     * @param array ...$arrays Arrays to compare against
+     *
+     * @return array Array containing values that are not present in any of the other arrays.
+     *
+     * @throws InvalidArgumentException If less than 2 arguments are provided
+     */
+    public static function differenceAssoc(array $array, array ...$arrays): array
+    {
+        if (empty($arrays)) {
+            throw new InvalidArgumentException('You need at least 2 arrays for computing difference');
+        }
+
+        $array = static::dot($array);
+
+        $arrays = array_map(function($item) {
+            return static::dot($item);
+        }, $arrays);
+
+        return static::undot(array_diff_assoc($array, ...$arrays));
     }
 }
