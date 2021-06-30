33ff28a04de4d2849cd5c279ef069c6624e3f7a3
alin <alin@rspsystems.com>
2021-06-30 15:13:45 +0200

    Add integration test of semantic version validation rule

---
+++ tests/Integration/Validation/Rules/SemanticVersionRuleTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Aedart\Tests\Integration\Validation\Rules;
+
+use Aedart\Tests\TestCases\Validation\ValidationTestCase;
+use Aedart\Validation\Rules\SemanticVersion;
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * SemanticVersionRuleTest
+ *
+ * @group validation
+ * @group rules
+ * @group version
+ *
+ * Some of the valid and invalid data sets are found from the following regex example:
+ * https://regex101.com/r/Ly7O1x/3/
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\Validation\Rules
+ */
+class SemanticVersionRuleTest extends ValidationTestCase
+{
+    /*****************************************************************
+     * Data Providers
+     ****************************************************************/
+
+    /**
+     * Provides input that should pass validation
+     *
+     * @return \string[][]
+     */
+    public function validVersionNumbers(): array
+    {
+        return [
+            'major.minor.patch' => [ '6.3.14' ],
+            'major.minor.patch-[pre-release]' => [ '2.0.0-alpha' ],
+            'major.minor.patch-[pre-release]+[build-metadata]' => [ '1.0.0-stable+exp.sha.5114f85' ],
+        ];
+    }
+
+    /**
+     * Provides input that should not pass validation
+     *
+     * @return \string[][]
+     */
+    public function invalidVersionNumbers(): array
+    {
+        return [
+            'major only' => [ '1' ],
+            'major.minor only' => [ '3.1' ],
+            'invalid pre-release' => [ '1.2.3-0123' ],
+            'invalid build metadata' => [ '1.1.2+.123' ],
+            'invalid format' => [ 'invalid' ],
+            'negative version' => [ '-1.0.3-gamma+b7718' ],
+            'invalid prefix' => [ 'v1.0.3-gamma+b7718' ],
+        ];
+    }
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Creates new instance of validation rule
+     *
+     * @return Rule
+     */
+    public function makeRule(): Rule
+    {
+        return new SemanticVersion();
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     * @dataProvider validVersionNumbers
+     *
+     * @param mixed $input
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function passesOnValidInput($input)
+    {
+        $this->shouldPass($input, $this->makeRule());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidVersionNumbers
+     *
+     * @param mixed $input
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function failsOnInvalidInput($input)
+    {
+        $this->shouldNotPass($input, $this->makeRule());
+    }
+}
