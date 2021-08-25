a77dadf48eb0a738abbd4a27aca1f2583649529d
alin <alin@rspsystems.com>
2021-08-25 13:24:10 +0200

    Add a base collection for Redmin resources

---
+++ packages/Redmine/src/Collections/Collection.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Aedart\Redmine\Collections;
+
+use Aedart\Redmine\RedmineResource;
+use Illuminate\Support\Collection as BaseCollection;
+
+/**
+ * Resource Collection
+ *
+ * Contains one or more Redmine API Resources.
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Redmine\Collections
+ */
+class Collection extends BaseCollection
+{
+    /**
+     * Class path to the type of resource this collection
+     * contains
+     *
+     * @var string|null
+     */
+    public ?string $resourceClass = null;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct($items = [], ?string $resourceClass = null)
+    {
+        parent::__construct($items);
+
+        $this->resourceClass = $resourceClass;
+    }
+
+    /**
+     * Creates a new collection of Redmine API Resources, from given response payload
+     *
+     * @param array $payload Decoded response payload
+     * @param RedmineResource $resource The type of resource new collection must consist of
+     *
+     * @return Collection<RedmineResource>
+     *
+     * @throws \Throwable
+     */
+    static public function fromResponsePayload(array $payload, RedmineResource $resource): Collection
+    {
+        $list = $resource->extractFromPayload($resource->resourceName(), $payload);
+
+        $resources = array_map(function($item) use($resource) {
+            return $resource::make($item, $resource->getConnection());
+        }, $list);
+
+        return new static($resources, get_class($resource));
+    }
+}
