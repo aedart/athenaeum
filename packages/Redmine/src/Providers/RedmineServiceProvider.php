92370f970c35243ceed4081af719bd0036f20f45
alin <alin@rspsystems.com>
2021-08-25 13:34:19 +0200

    Add service provider

---
+++ packages/Redmine/src/Providers/RedmineServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Aedart\Redmine\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Redmine Service Provider
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Redmine\Providers
+ */
+class RedmineServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap this service
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../configs/redmine.php' => config_path('redmine.php')
+        ], 'config');
+    }
+}
