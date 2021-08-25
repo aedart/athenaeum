3f02b53f687a595bf14fc717c07c49bb30aa74e2
alin <alin@rspsystems.com>
2021-08-25 13:48:48 +0200

    Add project resource (incomplete)

---
+++ packages/Redmine/src/Project.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Aedart\Redmine;
+
+use Carbon\Carbon;
+
+/**
+ * Project
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $identifier
+ * @property string $description
+ * @property string $homepage
+ * @property int $status
+ * @property bool $is_public
+ * @property bool $inherit_members TODO: ??? where does this come from!
+ * @property Carbon $created_om
+ * @property Carbon $updated_on
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Redmine
+ */
+class Project extends RedmineResource
+{
+    protected array $allowed = [
+        'id' => 'int',
+        'name' => 'string',
+        'identifier' => 'string',
+        'description' => 'string',
+        'homepage' => 'string',
+        'status' => 'int',
+        'is_public' => 'bool',
+        'inherit_members' => 'bool', // TODO: Where does this come from????
+        // TODO: default_version
+        // TODO: default_assignee
+        // TODO: parent
+        'created_on' => 'date',
+        'updated_on' => 'date'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function resourceName(): string
+    {
+        return 'projects';
+    }
+}
