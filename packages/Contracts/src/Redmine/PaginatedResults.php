c8cc01f683724cae6e097e8453c05c715d001ae7
alin <alin@rspsystems.com>
2021-08-25 13:27:10 +0200

    Add paginated results component
    A wrapper for a collection of results with pagination information

---
+++ packages/Contracts/src/Redmine/PaginatedResults.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Aedart\Contracts\Redmine;
+
+use Aedart\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Enumerable;
+use IteratorAggregate;
+use JsonSerializable;
+use Psr\Http\Message\ResponseInterface;
+use Stringable;
+
+/**
+ * Paginated Results
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Redmine
+ */
+interface PaginatedResults extends Paginator,
+    IteratorAggregate,
+    Arrayable,
+    Jsonable,
+    JsonSerializable,
+    Stringable
+{
+    /**
+     * Creates a new paginated results set, from given response
+     *
+     * @param ResponseInterface $response
+     * @param \Aedart\Redmine\RedmineResource $resource
+     *
+     * @return static
+     *
+     * @throws \JsonException
+     * @throws \Throwable
+     */
+    static public function fromResponse(ResponseInterface $response, $resource): PaginatedResults;
+
+    /**
+     * Returns the result set
+     *
+     * @return Enumerable
+     */
+    public function results(): Enumerable;
+}
---
+++ packages/Redmine/src/Pagination/PaginatedResults.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Aedart\Redmine\Pagination;
+
+use Aedart\Contracts\Redmine\PaginatedResults as PaginatedResultsInterface;
+use Aedart\Pagination\Paginator;
+use Aedart\Redmine\Collections\Collection;
+use Aedart\Redmine\RedmineResource;
+use Aedart\Utils\Json;
+use Illuminate\Support\Enumerable;
+use JsonException;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Paginated Results
+ *
+ * @see \Aedart\Contracts\Redmine\PaginatedResults
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Redmine\Pagination
+ */
+class PaginatedResults extends Paginator implements PaginatedResultsInterface
+{
+    /**
+     * Results collection
+     *
+     * @var Collection<RedmineResource>
+     */
+    protected Collection $results;
+
+    /**
+     * PaginatedResults
+     *
+     * @param Collection<RedmineResource> $results
+     * @param int $total [optional] Total amount of results
+     * @param int $limit [optional] The results limit
+     * @param int $offset [optional] Results offset
+     */
+    public function __construct(
+        Collection $results,
+        int $total = 0,
+        int $limit = 10,
+        int $offset = 0
+    ) {
+        parent::__construct($total, $limit, $offset);
+
+        $this->results = $results;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    static public function fromResponse(ResponseInterface $response, $resource): PaginatedResultsInterface
+    {
+        // Decode entire payload
+        $payload =  $resource->decode($response);
+
+        // Extract list (found results)
+        $results = Collection::fromResponsePayload($payload, $resource);
+
+        // Extract pagination details
+        $total = $resource->extractFromPayload('total_count', $payload);
+        $limit = $resource->extractFromPayload('limit', $payload);
+        $offset = $resource->extractFromPayload('offset', $payload);
+
+        return new static($results, $total, $limit, $offset);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        return $this->results->getIterator();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return Collection<RedmineResource>
+     */
+    public function results(): Enumerable
+    {
+        return $this->results;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return [
+            'results' => $this->results()->toArray(),
+            'total' => $this->total(),
+            'limit' => $this->limit(),
+            'offset' => $this->offset(),
+            'per_page' => $this->perPage(),
+            'total_pages' => $this->lastPage(),
+            'page' => $this->currentPage(),
+            'first_page' => $this->firstPage(),
+            'last_page' => $this->lastPage(),
+            'previous_page' => $this->previousPage(),
+            'next_page' => $this->nextPage()
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws JsonException
+     */
+    public function toJson($options = 0)
+    {
+        return Json::encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Returns a string representation of this Data Transfer Object
+     *
+     * @return string
+     *
+     * @throws JsonException
+     */
+    public function __toString()
+    {
+        return $this->toJson();
+    }
+
+    /**
+     * Debug info
+     *
+     * @return array
+     */
+    public function __debugInfo(): array
+    {
+        return $this->toArray();
+    }
+}
