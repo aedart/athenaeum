9eb84f8accbb42e135ce052ac811339eabde573f
alin <alin@rspsystems.com>
2021-08-27 11:17:33 +0200

    Add delete resource test

---
+++ tests/Integration/Redmine/DeleteTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Aedart\Tests\Integration\Redmine;
+
+use Aedart\Contracts\Redmine\Exceptions\ConnectionException;
+use Aedart\Tests\TestCases\Redmine\RedmineTestCase;
+use Teapot\StatusCode\All as StatusCodes;
+
+/**
+ * DeleteTest
+ *
+ * @group redmine
+ * @group redmine-resource
+ * @group redmine-resource-delete
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\Redmine
+ */
+class DeleteTest extends RedmineTestCase
+{
+    /**
+     * @test
+     *
+     * @throws ConnectionException
+     * @throws \JsonException
+     * @throws \Throwable
+     */
+    public function canDeleteResource()
+    {
+        $connection = $this->connectionWithMock();
+
+        $resource = $this->makeDummyResource([ 'id' => 1234 ], $connection);
+
+        $result = $resource->delete();
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test
+     *
+     * @throws ConnectionException
+     * @throws \JsonException
+     * @throws \Throwable
+     */
+    public function returnsFalseWhenDoesNotExist()
+    {
+        // Mock response - if delete method sends a request, then
+        // this test will fail with an "unexpected response" exception
+        $connection = $this->connectionWithMock([], StatusCodes::SERVICE_UNAVAILABLE);
+
+        $resource = $this->makeDummyResource([], $connection);
+
+        $result = $resource->delete();
+        $this->assertFalse($result);
+    }
+}
