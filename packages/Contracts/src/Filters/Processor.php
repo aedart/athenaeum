3ae93cf408553bc29491b9299828c8bb103565c8
alin <alin@rspsystems.com>
2021-11-15 10:22:30 +0100

    Add processor's public methods to interface
    Intended to be invoked via a pipeline.

--- packages/Contracts/src/Filters/Processor.php
+++ packages/Contracts/src/Filters/Processor.php
@@ -2,6 +2,105 @@
 
 namespace Aedart\Contracts\Filters;
 
+use Aedart\Contracts\Filters\Exceptions\InvalidParameterException;
+use RuntimeException;
+use Throwable;
+
+/**
+ * Http Query Parameters Processor
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Filters
+ */
 interface Processor
 {
+    /**
+     * Creates a new filter factory instance
+     *
+     * @param array $options [optional]
+     *
+     * @return static
+     */
+    public static function make(array $options = []);
+
+    /**
+     * Process the assigned request and build query filters, based
+     * on the received http query parameters.
+     *
+     * @param BuiltFiltersMap $built
+     * @param callable $next
+     *
+     * @return mixed
+     *
+     * @throws InvalidParameterException If http query parameters are invalid
+     * @throws Throwable
+     */
+    public function process(BuiltFiltersMap $built, callable $next);
+
+    /**
+     * Set the current request from which the http query parameter
+     * must be processed
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return self
+     */
+    public function fromRequest($request);
+
+    /**
+     * Get the current request
+     *
+     * @return \Illuminate\Http\Request
+     *
+     * @throws RuntimeException If no request was assigned
+     */
+    public function request();
+
+    /**
+     * Set name of Http query parameter to be used for building
+     * one or more filters
+     *
+     * @param string $parameter
+     *
+     * @return self
+     */
+    public function usingParameter(string $parameter);
+
+    /**
+     * Get the name of the http query parameter
+     *
+     * @return string
+     *
+     * @throws RuntimeException If parameter was not assigned
+     */
+    public function parameter(): string;
+
+    /**
+     * Returns the http query parameters value
+     *
+     * @return array|string|null
+     */
+    public function value();
+
+    /**
+     * Set state of whether processors most be forced
+     * applied, regardless if a matching parameter was requested
+     * or not.
+     *
+     * @param bool $force [optional]
+     *
+     * @return self
+     */
+    public function force(bool $force = true);
+
+    /**
+     * Determine whether processors most be forced
+     * applied, regardless if a matching parameter was requested
+     * or not.
+     *
+     * @see force()
+     *
+     * @return bool
+     */
+    public function mustBeApplied(): bool;
 }
