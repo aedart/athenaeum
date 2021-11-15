3c80be97c4334ae6eb5b1555e9be22b7c0611280
alin <alin@rspsystems.com>
2021-11-15 09:34:26 +0100

    Add filters exceptions

---
+++ packages/Filters/src/Exceptions/InvalidParameter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Aedart\Filters\Exceptions;
+
+use Aedart\Contracts\Filters\Exceptions\InvalidParameterException;
+use Aedart\Contracts\Filters\Processor;
+use Throwable;
+
+/**
+ * Invalid Parameter
+ *
+ * @see \Aedart\Contracts\Filters\Exceptions\InvalidParameterException
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Filters\Exceptions
+ */
+class InvalidParameter extends ProcessorException implements InvalidParameterException
+{
+    /**
+     * The parameter in question
+     *
+     * @var string|null
+     */
+    protected ?string $param = null;
+
+    /**
+     * @inheritDoc
+     */
+    public static function forParameter(
+        string $parameter,
+        Processor $processor,
+        $message = "",
+        $code = 0,
+        Throwable $previous = null
+    ) {
+        return static::make($processor, $message, $code, $previous)
+            ->setParameter($parameter);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setParameter(string $parameter)
+    {
+        $this->param = $parameter;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parameter(): ?string
+    {
+        if (isset($this->param)) {
+            return $this->param;
+        }
+
+        return optional($this->processor())->parameter() ?? 'query';
+    }
+}
