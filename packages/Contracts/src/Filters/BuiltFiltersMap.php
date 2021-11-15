87a30d0ae4b731877f6e48f4470e82667511afa0
alin <alin@rspsystems.com>
2021-11-15 10:18:22 +0100

    Add built filters map

---
+++ packages/Contracts/src/Filters/BuiltFiltersMap.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Aedart\Contracts\Filters;
+
+use Aedart\Contracts\Database\Query\Criteria;
+
+/**
+ * Built Filters Map
+ *
+ * Map of http query parameters and corresponding query filters that
+ * must be applied on a database query.
+ *
+ * The map also offers the ability to store and retrieve additional
+ * "meta" data associated with http query parameters or query filters.
+ *
+ * @see Criteria
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Filters
+ */
+interface BuiltFiltersMap
+{
+    /**
+     * Add a built filter for given key
+     *
+     * @param string $key
+     * @param Criteria $filter
+     *
+     * @retrun self
+     */
+    public function add(string $key, Criteria $filter);
+
+    /**
+     * Get built filters that match given key
+     *
+     * @param string $key
+     * @param mixed $default [optional]
+     *
+     * @return Criteria[]|mixed
+     */
+    public function get(string $key, $default = []);
+
+    /**
+     * Determine if filters exist for given key
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has(string $key): bool;
+
+    /**
+     * Forget all filters for the given key
+     *
+     * @param string $key
+     *
+     * @return self
+     */
+    public function forget(string $key);
+
+    /**
+     * Get all built filters
+     *
+     * @return Criteria[]
+     */
+    public function all(): array;
+
+    /**
+     * Set arbitrary meta data for given key
+     *
+     * @param string $key
+     * @param mixed $meta
+     *
+     * @return self
+     */
+    public function setMeta(string $key, $meta);
+
+    /**
+     * Get arbitrary meta data for given key
+     *
+     * @param string $key
+     * @param mixed $default [optional]
+     *
+     * @return mixed
+     */
+    public function getMeta(string $key, $default = null);
+
+    /**
+     * Determine if arbitrary meta data exists for given key
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function hasMeta(string $key): bool;
+
+    /**
+     * Forget meta for given key
+     *
+     * @param string $key
+     *
+     * @return self
+     */
+    public function forgetMeta(string $key);
+
+    /**
+     * Get all arbitrary meta data
+     *
+     * @return array Key-value
+     */
+    public function meta(): array;
+
+    /**
+     * Clears the map of all filters and meta
+     *
+     * @return self
+     */
+    public function forgetAll();
+}
---
+++ tests/Unit/Filters/BuiltFiltersTest.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace Aedart\Tests\Unit\Filters;
+
+use Aedart\Contracts\Database\Query\Criteria;
+use Aedart\Contracts\Database\Query\Exceptions\CriteriaException;
+use Aedart\Contracts\Filters\BuiltFiltersMap;
+use Aedart\Filters\BuiltFilters;
+use Aedart\Testing\TestCases\UnitTestCase;
+use Aedart\Tests\Helpers\Dummies\Database\Query\Filters\GenericFilter;
+
+/**
+ * BuiltFiltersTest
+ *
+ * @group filters
+ * @group built-filters-map
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Unit\Filters
+ */
+class BuiltFiltersTest extends UnitTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Creates a new built filters map instance
+     *
+     * @param array $filters [optional]
+     * @param array $meta [optional]
+     *
+     * @return BuiltFiltersMap
+     */
+    public function makeMap(array $filters = [], array $meta = []): BuiltFiltersMap
+    {
+        return new BuiltFilters($filters, $meta);
+    }
+
+    /**
+     * Creates a new criteria (query filter) instance
+     *
+     * @return Criteria
+     *
+     * @throws CriteriaException
+     */
+    public function makeCriteria(): Criteria
+    {
+        return new GenericFilter($this->getFaker()->unique()->slug(3));
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     *
+     * @throws CriteriaException
+     */
+    public function canAddAndRetrieveFilter()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $filter = $this->makeCriteria();
+
+        $map->add($key, $filter);
+
+        $this->assertTrue($map->has($key));
+
+        // -------------------------------------------- //
+        // Output is a list of filters.
+        $result = $map->get($key);
+
+        $this->assertCount(1, $result);
+        $this->assertSame($filter, $result[0]);
+    }
+
+    /**
+     * @test
+     *
+     * @throws CriteriaException
+     */
+    public function returnsDefaultWhenNoFilterAdded()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $default = $this->makeCriteria();
+
+        $result = $map->get($key, $default);
+
+        $this->assertSame($default, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @throws CriteriaException
+     */
+    public function canObtainAllFilters()
+    {
+        $map = $this->makeMap();
+
+        $keyA = 'my-filter';
+        $keyB = 'my-filter';
+        $filterA = $this->makeCriteria();
+        $filterB = $this->makeCriteria();
+        $expected = [ $filterA, $filterB ];
+
+        $map
+            ->add($keyA, $filterA)
+            ->add($keyB, $filterB);
+
+        $result = $map->all();
+
+        $this->assertCount(2, $result);
+
+        foreach ($result as $filter) {
+            $this->assertTrue(in_array($filter, $expected));
+        }
+    }
+
+    /**
+     * @test
+     *
+     * @throws CriteriaException
+     */
+    public function canForgetFilters()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $filterA = $this->makeCriteria();
+        $filterB = $this->makeCriteria();
+
+        $map
+            ->add($key, $filterA)
+            ->add($key, $filterB);
+
+
+        $this->assertTrue($map->has($key));
+
+        // -------------------------------------------- //
+        // Forget should clear all filters
+
+        $map->forget($key);
+
+        $this->assertFalse($map->has($key));
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndObtainMeta()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $value = $this->getFaker()->slug;
+
+        $map->setMeta($key, $value);
+
+        $this->assertTrue($map->hasMeta($key));
+
+        $result = $map->getMeta($key);
+        $this->assertSame($value, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function returnsDefaultWhenNoMetaSet()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $default = $this->getFaker()->slug;
+
+        $result = $map->getMeta($key, $default);
+
+        $this->assertSame($default, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function canForgetMeta()
+    {
+        $map = $this->makeMap();
+
+        $key = 'my-filter';
+        $value = $this->getFaker()->slug;
+
+        $map->setMeta($key, $value);
+
+        $this->assertTrue($map->hasMeta($key));
+
+        // ------------------------------------------------ //
+
+        $map->forgetMeta($key);
+        $this->assertFalse($map->hasMeta($key));
+    }
+
+    /**
+     * @test
+     */
+    public function canObtainAllMeta()
+    {
+        $map = $this->makeMap();
+
+        $keyA = 'my-filter';
+        $keyB = 'my-other-filter';
+        $valueA = $this->getFaker()->slug;
+        $valueB = $this->getFaker()->slug;
+
+        $map
+            ->setMeta($keyA, $valueA)
+            ->setMeta($keyB, $valueB);
+
+        $meta = $map->meta();
+
+        $this->assertCount(2, $meta);
+        $this->assertArrayHasKey($keyA, $meta);
+        $this->assertArrayHasKey($keyB, $meta);
+    }
+
+    /**
+     * @test
+     *
+     * @throws CriteriaException
+     */
+    public function canForgetAll()
+    {
+        $map = $this->makeMap();
+
+        $keyA = 'my-filter';
+        $keyB = 'my-other-filter';
+
+        $filterA = $this->makeCriteria();
+        $filterB = $this->makeCriteria();
+
+        $valueA = $this->getFaker()->slug;
+        $valueB = $this->getFaker()->slug;
+
+        $map
+            ->add($keyA, $filterA)
+            ->add($keyB, $filterB)
+            ->setMeta($keyA, $valueA)
+            ->setMeta($keyB, $valueB);
+
+        $map->forgetAll();
+
+        $this->assertFalse($map->has($keyA));
+        $this->assertFalse($map->has($keyB));
+        $this->assertFalse($map->hasMeta($keyA));
+        $this->assertFalse($map->hasMeta($keyB));
+    }
+}
