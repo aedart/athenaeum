c10c7627a76c2ca069b0df55bb4251bf7c546c54
alin <alin@rspsystems.com>
2021-11-15 13:06:05 +0100

    Add filters builder interface

---
+++ packages/Contracts/src/Filters/Builder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Aedart\Contracts\Filters;
+
+/**
+ * Filters Builder
+ *
+ * Responsible for processing a request and built appropriate
+ * query filters
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Filters
+ */
+interface Builder
+{
+    /**
+     * Creates a new Request Filters Builder instance
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return static
+     */
+    public static function make($request);
+
+    /**
+     * Returns map of http query parameters and their
+     * corresponding processors to be applied.
+     *
+     * @return Processor[] Key-value, key = http query parameter, value = processor
+     */
+    public function processors(): array;
+
+    /**
+     * Build the query filters to be applied
+     *
+     * @return BuiltFiltersMap
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function build(): BuiltFiltersMap;
+}
