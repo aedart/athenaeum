86ba012bfb463ad17297c76583bdbd3393af91ac
alin <alin@rspsystems.com>
2021-12-02 14:25:59 +0100

    Add UTC Datetime filter

---
+++ packages/Filters/src/Query/Filters/Fields/UTCDatetimeFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aedart\Filters\Query\Filters\Fields;
+
+/**
+ * UTC Datetime Filter
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Filters\Query\Filters\Fields
+ */
+class UTCDatetimeFilter extends DatetimeFilter
+{
+    /**
+     * State whether given datetime must be converted
+     * to UTC or not
+     *
+     * @var bool
+     */
+    protected bool $utc = true;
+}
