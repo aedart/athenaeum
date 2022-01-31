1613ba870897843dc5a9b49d76f1485da93880da
alin <alin@rspsystems.com>
2022-01-31 13:48:11 +0100

    Change concern, add dispatch multiple models changed event

--- packages/Audit/src/Observers/Concerns/ModelChangedEvents.php
+++ packages/Audit/src/Observers/Concerns/ModelChangedEvents.php
@@ -4,10 +4,13 @@
 namespace Aedart\Audit\Observers\Concerns;
 
 use Aedart\Audit\Events\ModelHasChanged;
+use Aedart\Audit\Events\MultipleModelsChanged;
 use Aedart\Support\Helpers\Auth\AuthTrait;
 use Aedart\Support\Helpers\Events\DispatcherTrait;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Throwable;
 
 /**
  * Concerns Model Changed Events
@@ -45,6 +48,50 @@ trait ModelChangedEvents
     }
 
     /**
+     * Dispatches multiple models changed event
+     *
+     * @param Collection<Model>|Model[] $models The changed models
+     * @param string $type [optional] The event type
+     * @param array|null $original [optional] Original data (attributes) before change occurred.
+     *                                        Default's to given model's original data, if none given.
+     * @param array|null $changed [optional] Changed data (attributes) after change occurred.
+     *                                        Default's to given model's changed data, if none given.
+     * @param string|null $message [optional] Eventual user provided message associated with the event
+     *
+     * @return self
+     *
+     * @throws Throwable
+     */
+    public function dispatchMultipleModelsChanged(
+        $models,
+        string $type,
+        ?array $original = null,
+        ?array $changed = null,
+        ?string $message = null
+    ) {
+        // Determine if event dispatching should be aborted, based on first model's
+        // "must record next change" state.
+        $first = $models->first();
+
+        // Abort if model does not wish to record its next change
+        if (method_exists($first, 'mustRecordNextChange') && !$first->mustRecordNextChange()) {
+            return $this;
+        }
+
+        $event = $this->makeMultipleModelsChangedEvent(
+            $models,
+            $type,
+            $original,
+            $changed,
+            $message
+        );
+
+        $this->getDispatcher()->dispatch($event);
+
+        return $this;
+    }
+
+    /**
      * Creates a new "model has changed" event
      *
      * @param Model $model The model that has changed
@@ -63,6 +110,37 @@ trait ModelChangedEvents
             $type,
             null, // Original data is resolved from model in this context
             null, // Changed data is resolved from model in this context
+            $message
+        );
+    }
+
+    /**
+     * Creates a new "multiple models changed" event
+     *
+     * @param Collection<Model>|Model[] $models The changed models
+     * @param string $type [optional] The event type
+     * @param array|null $original [optional] Original data (attributes) before change occurred.
+     *                                        Default's to given model's original data, if none given.
+     * @param array|null $changed [optional] Changed data (attributes) after change occurred.
+     *                                        Default's to given model's changed data, if none given.
+     * @param string|null $message [optional] Eventual user provided message associated with the event
+     *
+     * @return MultipleModelsChanged
+     */
+    public function makeMultipleModelsChangedEvent(
+        $models,
+        string $type,
+        ?array $original = null,
+        ?array $changed = null,
+        ?string $message = null
+    ): MultipleModelsChanged
+    {
+        return new MultipleModelsChanged(
+            $models,
+            $this->user(),
+            $type,
+            $original,
+            $changed,
             $message
         );
     }
