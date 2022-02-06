5dd4aa8167f66672d8f3bcc520daa76a2e4c1bd8
Alin Eugen Deac <aedart@gmail.com>
2022-02-06 15:24:03 +0100

    Change orchestra testbench-dusk dependency to 7.x-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -37,7 +37,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             'codeception/codeception' => '5.0.0-alpha1',
             'codeception/module-asserts' => '*@dev',
             'orchestra/testbench' => '7.x-dev',
-            'orchestra/testbench-dusk' => 'dev-master',
+            'orchestra/testbench-dusk' => '7.x-dev',
         ],
 
         ComposerJsonSection::AUTOLOAD => [
