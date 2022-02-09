612f24a9da0d9f052002aa58f476ad1b3c14ac8a
Alin Eugen Deac <aedart@gmail.com>
2022-02-09 17:10:35 +0100

    Add fresh() method to interface
    #51

--- packages/Contracts/src/Http/Clients/Manager.php
+++ packages/Contracts/src/Http/Clients/Manager.php
@@ -13,7 +13,10 @@ use Aedart\Contracts\Http\Clients\Exceptions\ProfileNotFoundException;
 interface Manager
 {
     /**
-     * Create or obtain a Http Client
+     * Create or obtain a Http Client instance
+     *
+     * If client profile was previously instantiated, then this method will return the same
+     * client instance. Use {@see fresh()} if you wish to obtain a new instance.
      *
      * @param string|null $profile [optional] Name of Http Client profile to obtain or create
      * @param array $options [optional] Http Client options
@@ -23,4 +26,19 @@ interface Manager
      * @throws ProfileNotFoundException
      */
     public function profile(string|null $profile = null, array $options = []): Client;
+
+    /**
+     * Creates a new client instance for the given profile
+     *
+     * Unlike the {@see profile()} method, this method always returns a new client
+     * instance.
+     *
+     * @param string|null $profile [optional] Name of Http Client profile to obtain or create
+     * @param array $options [optional] Http Client options
+     *
+     * @return Client
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function fresh(string|null $profile = null, array $options = []): Client;
 }
