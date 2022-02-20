a184be87ff360d898d600052a3c092ef1ba972d1
Alin Eugen Deac <aedart@gmail.com>
2022-02-20 15:39:22 +0100

    Add warning widget when viewing outdated or next version docs
    #97

---
+++ docs/.vuepress/theme/components/VersionWarning.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="version-warning">
+    <label>
+      Warning
+    </label>
+
+    <slot></slot>
+  </div>
+</template>
+<script>
+
+/**
+ * Custom "version warning"
+ */
+export default {
+  name: 'Version Warning'
+}
+</script>
