304b8e0b7ac94b2047cbf9f120c6c55bf31df58c
Alin Eugen Deac <aedart@gmail.com>
2022-02-28 17:07:38 +0100

    Add general stream exception

---
+++ packages/Contracts/src/Streams/Exceptions/StreamException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Contracts\Streams\Exceptions;
+
+use Throwable;
+
+/**
+ * Stream Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Streams\Exceptions
+ */
+interface StreamException extends Throwable
+{
+}
