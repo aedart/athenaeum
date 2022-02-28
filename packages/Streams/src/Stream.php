eb51f5a59dd5bd7c7ddbae7e8a901f9af39c28f2
Alin Eugen Deac <aedart@gmail.com>
2022-02-28 18:31:03 +0100

    Add concrete stream component (incomplete)

---
+++ packages/Streams/src/Stream.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace Aedart\Streams;
+
+use Aedart\Contracts\Streams\Exceptions\StreamException;
+use Aedart\Contracts\Streams\Meta;
+use Aedart\Contracts\Streams\Stream as StreamInterface;
+
+/**
+ * Stream
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Streams
+ */
+class Stream implements StreamInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public static function make($stream, array|Meta|null $meta = null): static
+    {
+        // TODO: Implement make() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function open(
+        string $filename,
+        string $mode,
+        bool $useIncludePath = false,
+        $context = null
+    ): static
+    {
+        // TODO: Implement open() method.
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function close()
+    {
+        // TODO: Implement close() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function detach()
+    {
+        // TODO: Implement detach() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSize(): int|null
+    {
+        // TODO: Implement getSize() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function tell(): int
+    {
+        // TODO: Implement tell() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eof(): bool
+    {
+        // TODO: Implement eof() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isSeekable(): bool
+    {
+        // TODO: Implement isSeekable() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        // TODO: Implement seek() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rewind()
+    {
+        // TODO: Implement rewind() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isWritable(): bool
+    {
+        // TODO: Implement isWritable() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function write($string): int
+    {
+        // TODO: Implement write() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isReadable(): bool
+    {
+        // TODO: Implement isReadable() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function read($length): string
+    {
+        // TODO: Implement read() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getContents(): string
+    {
+        // TODO: Implement getContents() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMetadata($key = null)
+    {
+        // TODO: Implement getMetadata() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        // TODO: Implement __toString() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function position(): int
+    {
+        return $this->tell();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function end(): bool
+    {
+        return $this->eof();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isDetached(): bool
+    {
+        // TODO: Implement isDetached() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resource()
+    {
+        // TODO: Implement resource() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function meta(): Meta
+    {
+        // TODO: Implement meta() method.
+    }
+
+    /*****************************************************************
+     * Internals
+     ****************************************************************/
+
+}
