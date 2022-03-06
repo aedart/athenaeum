9c5b95669768c5828438ca20d1f3191f06eb12d4
Alin Eugen Deac <aedart@gmail.com>
2022-03-06 13:50:25 +0100

    Add stream exceptions

---
+++ packages/Streams/src/Exceptions/InvalidStreamResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aedart\Streams\Exceptions;
+
+/**
+ * Invalid Resource Exception
+ *
+ * Should be thrown whenever an invalid resource is provided or
+ * attempted used.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Streams\Exceptions
+ */
+class InvalidStreamResource extends StreamException
+{
+}
