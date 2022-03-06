a9189e717579c6dbb98ecb623c612e3334fa0c2b
Alin Eugen Deac <aedart@gmail.com>
2022-03-06 21:02:34 +0100

    Add stream transaction interfaces

---
+++ packages/Contracts/src/Streams/Exceptions/TransactionException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Contracts\Streams\Exceptions;
+
+use Throwable;
+
+/**
+ * Stream Transaction Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Streams\Exceptions
+ */
+interface TransactionException extends Throwable
+{
+}
---
+++ packages/Contracts/src/Streams/Transactions/Factory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aedart\Contracts\Streams\Transactions;
+
+use Aedart\Contracts\Streams\Exceptions\TransactionException;
+use Aedart\Contracts\Streams\Stream;
+
+/**
+ * Stream Transaction Factory
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Streams\Transactions
+ */
+interface Factory
+{
+    /**
+     * Creates a new transaction instance for given stream
+     *
+     * @param  Stream  $stream
+     * @param  string|null  $driver  [optional] Name of driver to use. If `null` is given,
+     *                               then a default driver will be used.
+     *
+     * @return Transaction
+     *
+     * @throws TransactionException
+     */
+    public function create(Stream $stream, string|null $driver = null): Transaction;
+}
