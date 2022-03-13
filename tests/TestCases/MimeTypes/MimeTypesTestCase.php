0dd4ce41c2d59135a7aec0b9d96e16b4621dd5fe
Alin Eugen Deac <aedart@gmail.com>
2022-03-13 20:57:59 +0100

    Add additional test util methods

--- tests/TestCases/MimeTypes/MimeTypesTestCase.php
+++ tests/TestCases/MimeTypes/MimeTypesTestCase.php
@@ -4,6 +4,9 @@ namespace Aedart\Tests\TestCases\MimeTypes;
 
 use Aedart\Config\Providers\ConfigLoaderServiceProvider;
 use Aedart\Config\Traits\ConfigLoaderTrait;
+use Aedart\Contracts\MimeTypes\Exceptions\MimeTypeDetectionException;
+use Aedart\Contracts\MimeTypes\MimeType;
+use Aedart\MimeTypes\Concerns\MimeTypeDetection;
 use Aedart\MimeTypes\Providers\MimeTypesDetectionServiceProvider;
 use Aedart\Support\Helpers\Config\ConfigTrait;
 use Aedart\Testing\TestCases\LaravelTestCase;
@@ -21,6 +24,21 @@ abstract class MimeTypesTestCase extends LaravelTestCase
 {
     use ConfigLoaderTrait;
     use ConfigTrait;
+    use MimeTypeDetection;
+
+    /**
+     * Mime-Type data to be sampled
+     *
+     * @var string|resource|null
+     */
+    protected $mimeTypeData = null;
+
+    /**
+     * @test
+     *
+     * @var resource|null
+     */
+    protected $fileStream = null;
 
     /*****************************************************************
      * Setup
@@ -45,6 +63,10 @@ abstract class MimeTypesTestCase extends LaravelTestCase
      */
     protected function _after()
     {
+        if (isset($this->fileStream) && is_resource($this->fileStream)) {
+            fclose($this->fileStream);
+        }
+
         parent::_after();
     }
 
@@ -67,5 +89,140 @@ abstract class MimeTypesTestCase extends LaravelTestCase
     public function configDir(): string
     {
         return Configuration::dataDir() . 'configs/mime-types';
+    }
+
+    /**
+     * Returns path to test files
+     *
+     * @return string
+     */
+    public function filesDir(): string
+    {
+        return Configuration::dataDir() . 'mime-types/files';
+    }
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Set the file data to be sampled for mime-type
+     *
+     * @param string|resource $data
+     *
+     * @return self
+     */
+    public function withMimeTypeData($data): static
+    {
+        $this->mimeTypeData = $data;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function mimeTypeData()
+    {
+        return $this->mimeTypeData;
+    }
+
+    /**
+     * Returns mime-type using string content
+     *
+     * @param  string  $ext File extension
+     * @param  string  $profile
+     * @param  array  $options  [optional]
+     *
+     * @return MimeType
+     * @throws MimeTypeDetectionException
+     */
+    protected function mimeTypeUsingStringContent(string $ext, string $profile, array $options = []): MimeType
+    {
+        return $this
+            ->withMimeTypeData($this->getFileContents($ext))
+            ->mimeType($profile, $options);
+    }
+
+    /**
+     * Returns mime-type using file stream
+     *
+     * @param  string  $ext File extension
+     * @param  string  $profile
+     * @param  array  $options  [optional]
+     *
+     * @return MimeType
+     * @throws MimeTypeDetectionException
+     */
+    protected function mimeTypeUsingStream(string $ext, string $profile, array $options = []): MimeType
+    {
+        return $this
+            ->withMimeTypeData($this->getFileStream($ext))
+            ->mimeType($profile, $options);
+    }
+
+    /**
+     * Returns path to test file
+     *
+     * @param  string  $ext File extension
+     *
+     * @return string
+     */
+    public function filePath(string $ext): string
+    {
+        return $this->filesDir() . DIRECTORY_SEPARATOR . "test.{$ext}";
+    }
+
+    /**
+     * Returns test file contents
+     *
+     * @param  string  $ext File extension
+     *
+     * @return string
+     */
+    public function getFileContents(string $ext): string
+    {
+        $path = $this->filePath($ext);
+
+        return file_get_contents($path);
+    }
+
+    /**
+     * Returns test file stream
+     *
+     * @param  string  $ext
+     *
+     * @return false|resource
+     */
+    public function getFileStream(string $ext)
+    {
+        $path = $this->filePath($ext);
+
+        return $this->fileStream = fopen($path, 'r+b');
+    }
+
+    /**
+     * Creates a new test file expectation
+     *
+     * @param  string  $ext
+     * @param  string  $type
+     * @param  string  $encoding
+     * @param  array  $extensions  [optional]
+     *
+     * @return array
+     */
+    public function makeTestFileExpectation(
+        string $ext,
+        string $type,
+        string $encoding,
+        array $extensions = []
+    ): array
+    {
+        return [[
+            $ext,
+            $type,
+            $encoding,
+            $extensions
+        ]];
     }
 }
