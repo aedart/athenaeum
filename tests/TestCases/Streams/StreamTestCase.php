77cea36f8256ec0dbb1ac71162d5e017e26260c9
Alin Eugen Deac <aedart@gmail.com>
2022-03-20 12:01:28 +0100

    Change test-case, load test specific configuration

--- tests/TestCases/Streams/StreamTestCase.php
+++ tests/TestCases/Streams/StreamTestCase.php
@@ -2,8 +2,14 @@
 
 namespace Aedart\Tests\TestCases\Streams;
 
+use Aedart\Config\Providers\ConfigLoaderServiceProvider;
+use Aedart\Config\Traits\ConfigLoaderTrait;
+use Aedart\Contracts\Config\Loaders\Exceptions\InvalidPathException;
+use Aedart\Contracts\Config\Parsers\Exceptions\FileParserException;
 use Aedart\Streams\Providers\StreamServiceProvider;
+use Aedart\Support\Helpers\Config\ConfigTrait;
 use Aedart\Testing\TestCases\LaravelTestCase;
+use Codeception\Configuration;
 
 /**
  * Stream Test-Case
@@ -13,9 +19,27 @@ use Aedart\Testing\TestCases\LaravelTestCase;
  */
 abstract class StreamTestCase extends LaravelTestCase
 {
+    use ConfigLoaderTrait;
+    use ConfigTrait;
+
     /*****************************************************************
      * Setup Methods
      ****************************************************************/
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws InvalidPathException
+     * @throws FileParserException
+     */
+    protected function _before()
+    {
+        parent::_before();
+
+        $this->getConfigLoader()
+            ->setDirectory($this->configDir())
+            ->load();
+    }
 
     /**
      * {@inheritdoc}
@@ -23,7 +47,24 @@ abstract class StreamTestCase extends LaravelTestCase
     protected function getPackageProviders($app)
     {
         return [
+            ConfigLoaderServiceProvider::class,
             StreamServiceProvider::class
         ];
     }
+
+    /**
+     * Returns the path to configuration files
+     *
+     * @return string
+     */
+    public function configDir(): string
+    {
+        return Configuration::dataDir() . 'configs/streams';
+    }
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+
 }
