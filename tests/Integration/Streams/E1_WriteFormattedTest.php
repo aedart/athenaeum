c149700d5f7739d37ae0eaca9c26786871ddfc6c
Alin Eugen Deac <aedart@gmail.com>
2022-03-20 14:37:36 +0100

    Add tests for stream's basic write methods

---
+++ tests/Integration/Streams/E1_WriteFormattedTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Aedart\Tests\Integration\Streams;
+
+use Aedart\Streams\Stream;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\TestCases\Streams\StreamTestCase;
+
+/**
+ * E1_WriteFormattedTest
+ *
+ * @group streams
+ * @group streams-e1
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Streams
+ */
+class E1_WriteFormattedTest extends StreamTestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canWriteFormatted()
+    {
+        $resource = fopen('php://memory', 'r+b');
+        $stream = Stream::make($resource);
+
+        $faker = $this->getFaker();
+        $a = $faker->word();
+        $b = $faker->word();
+        $c = $faker->word();
+
+        $bytesWritten = $stream->writeFormatted('<<%s, %s, %s>>', $a, $b, $c);
+        $expected = "<<{$a}, {$b}, {$c}>>";
+
+        ConsoleDebugger::output([
+            'a' => $a,
+            'b' => $b,
+            'c' => $c,
+            'expected' => $expected,
+            'stream' => (string) $stream
+        ]);
+
+        $this->assertSame(strlen($expected), $bytesWritten);
+        $this->assertSame($expected, (string) $stream);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canWriteFormattedViaPutFormatted()
+    {
+        $resource = fopen('php://memory', 'r+b');
+        $stream = Stream::make($resource);
+
+        $faker = $this->getFaker();
+        $a = $faker->word();
+        $b = $faker->word();
+        $c = $faker->word();
+        $d = $faker->word();
+
+        $stream
+            ->putFormatted('<<%s>>', $a)
+            ->putFormatted('|%s-%s|', $b, $c)
+            ->putFormatted('<<%s>>', $d);
+
+        $expected = "<<{$a}>>|{$b}-{$c}|<<{$d}>>";
+
+        ConsoleDebugger::output([
+            'a' => $a,
+            'b' => $b,
+            'c' => $c,
+            'd' => $d,
+            'expected' => $expected,
+            'stream' => (string) $stream
+        ]);
+
+        $this->assertSame($expected, (string) $stream);
+    }
+}
