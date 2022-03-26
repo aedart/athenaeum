3e361e51668a4baa313ed084c799b963180cd843
Alin Eugen Deac <aedart@gmail.com>
2022-03-26 12:05:37 +0100

    Add tests for stream meta

--- tests/Integration/Streams/X2_MetaTest.php
+++ tests/Integration/Streams/X2_MetaTest.php
@@ -2,9 +2,121 @@
 
 namespace Aedart\Tests\Integration\Streams;
 
+use Aedart\Contracts\Streams\Meta\Repository;
+use Aedart\Streams\Meta\Repository as DefaultMetaRepository;
+use Aedart\Streams\Stream;
+use Aedart\Testing\Helpers\ConsoleDebugger;
 use Aedart\Tests\TestCases\Streams\StreamTestCase;
+use Aedart\Utils\Arr;
 
+/**
+ * X2_MetaTest
+ *
+ * @group streams
+ * @group streams-x2
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Streams
+ */
 class X2_MetaTest extends StreamTestCase
 {
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canResolveNullMeta()
+    {
+        $resource = fopen('php://memory', 'rb');
+        $stream = Stream::make($resource);
 
+        $result = $stream->meta();
+
+        $this->assertInstanceOf(Repository::class, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canResolveMetaFromArray()
+    {
+        $meta = [
+            'foo' => 'bar'
+        ];
+
+        $resource = fopen('php://memory', 'rb');
+        $stream = Stream::make($resource, $meta);
+
+        $result = $stream->meta();
+
+        $this->assertInstanceOf(Repository::class, $result);
+        $this->assertTrue($result->has('foo'));
+        $this->assertsame($meta['foo'], $result->get('foo'));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canResolveMetaFromMetaRepository()
+    {
+        $meta = new DefaultMetaRepository([
+            'foo' => 'bar'
+        ]);
+
+        $resource = fopen('php://memory', 'rb');
+        $stream = Stream::make($resource, $meta);
+
+        $result = $stream->meta();
+
+        $this->assertInstanceOf(Repository::class, $result);
+        $this->assertSame($meta, $result);
+        $this->assertsame($meta->get('foo'), $result->get('foo'));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function mergesRawMetaIntoRepository()
+    {
+        $resource = fopen('php://memory', 'rb');
+        $stream = Stream::make($resource);
+
+        $raw = $stream->rawMeta();
+        $result = $stream->meta();
+
+        $keys = array_keys(Arr::dot($raw));
+        foreach ($keys as $key) {
+            ConsoleDebugger::output($key);
+
+            $this->assertTrue($result->has($key));
+        }
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws \Aedart\Contracts\Streams\Exceptions\StreamException
+     */
+    public function canObtainRawMeta()
+    {
+        $resource = fopen('php://memory', 'rb');
+        $stream = Stream::make($resource);
+
+        $raw = $stream->rawMeta();
+
+        ConsoleDebugger::output($raw);
+
+        $this->assertNotEmpty($raw);
+    }
 }
