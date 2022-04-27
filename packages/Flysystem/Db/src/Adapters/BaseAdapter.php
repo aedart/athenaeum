59139634da6e3108538a24615eff1ae87b4ca58b
alin <alin@rspsystems.com>
2022-04-27 13:23:49 +0200

    Change base adapter, add "resolve" connection method
    The general idea is that this method should be used to obtain the database connection.

--- packages/Flysystem/Db/src/Adapters/BaseAdapter.php
+++ packages/Flysystem/Db/src/Adapters/BaseAdapter.php
@@ -3,9 +3,11 @@
 namespace Aedart\Flysystem\Db\Adapters;
 
 use Aedart\Contracts\Support\Helpers\Database\DbAware;
+use Aedart\Flysystem\Db\Exceptions\ConnectionException;
 use Aedart\Support\Helpers\Database\DbTrait;
 use Illuminate\Database\ConnectionInterface;
 use League\Flysystem\FilesystemAdapter;
+use Packages\Filesystem\Flysystem\Adapters\Exceptions\UnableToResolveDatabaseConnection;
 
 /**
  * Base Adapter
@@ -31,5 +33,22 @@ abstract class BaseAdapter implements
     )
     {
         $this->setDb($connection);
+    }
+
+    /**
+     * Get the established database connection
+     *
+     * @return ConnectionInterface
+     *
+     * @throws ConnectionException
+     */
+    public function connection(): ConnectionInterface
+    {
+        $connection = $this->getDb();
+        if (!isset($connection)) {
+            throw new ConnectionException('Database connection could not be resolved. Please check your configuration.');
+        }
+
+        return $connection;
     }
 }
