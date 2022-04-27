a2fcc473f2e8696b7a72f812ad862cf046906d4a
alin <alin@rspsystems.com>
2022-04-27 12:49:20 +0200

    Add base adapter (incomplete)

---
+++ packages/Flysystem/Db/src/Adapters/BaseAdapter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Flysystem\Db\Adapters;
+
+use League\Flysystem\FilesystemAdapter;
+
+/**
+ * Base Adapter
+ *
+ * Abstraction for database adapters
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Flysystem\Db\Adapters
+ */
+abstract class BaseAdapter implements
+    FilesystemAdapter
+{
+}
