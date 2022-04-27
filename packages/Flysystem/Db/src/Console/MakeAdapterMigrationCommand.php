02a7231a4366a0bbea2f7e73bbca493bb380f0f6
alin <alin@rspsystems.com>
2022-04-27 11:18:11 +0200

    Add make adapter migration command (incomplete)
    This will create a migration file, for the selected database adapter type. It still needs a bit more customisation, but should be working good enough for now.

--- packages/Flysystem/Db/src/Console/MakeAdapterMigrationCommand.php
+++ packages/Flysystem/Db/src/Console/MakeAdapterMigrationCommand.php
@@ -3,7 +3,8 @@
 namespace Aedart\Flysystem\Db\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Composer;
+use InvalidArgumentException;
+use RuntimeException;
 
 /**
  * Make Adapter Migration Command
@@ -18,7 +19,11 @@ class MakeAdapterMigrationCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'flysystem:make-adapter-migration';
+    protected $signature = 'flysystem:make-adapter-migration
+                            {--t|type= : The type of database adapter to create migrations for. Accepts "default" or "deduplicate".}
+                            {--name= : Name of the table to hold files.}
+                            {--path= : Path to where migration file must be created (Relative to project root). Defaults to Laravel\'s default migrations directory}
+    ';
 
     /**
      * The console command description.
@@ -28,16 +33,14 @@ class MakeAdapterMigrationCommand extends Command
     protected $description = 'Creates a new migration file for a Flysystem database adapter';
 
     /**
-     * Creates new adapter migration console command instance
+     * Allowed adapter types
      *
-     * @param Composer $composer The composer instance
+     * @var string[]
      */
-    public function __construct(
-        protected Composer $composer
-    )
-    {
-        parent::__construct();
-    }
+    protected array $allowedAdapterTypes = [
+        'default',
+        'deduplicate'
+    ];
 
     /**
      * Executes console command
@@ -46,17 +49,196 @@ class MakeAdapterMigrationCommand extends Command
      */
     public function handle(): void
     {
-        // TODO: Obtain adapter type
+        $this->output->title('Create new database adapter migration file');
 
-        // TODO: Obtain table name
+        // Resolve type, name... etc
+        $type = $this->resolveAdapterType();
+        $table = $this->resolveTableName();
 
-        // TODO: Obtain stub and populate it
+        // Write migration file
+        $this->writeMigrationFile($type, $table, [
+            'table' => $table,
+        ]);
 
-        // TODO: Migrations path?
+        // Done...
+        $this->output->success([
+            'Migration file created',
 
-        // TODO: Write migration file
+            // TODO: Must be a bit more dynamic...
+            <<<EOF
+Please add appropriate storage disk profile, in your `config/filesystems.php` configuration:
 
-        // Dump Composer's autoload
-        $this->composer->dumpAutoloads();
+'disks' => [
+
+    'database' => [
+        'driver' => 'database',
+        'connection' => env('DB_CONNECTION', 'mysql'),
+        'files_table' => '{$table}',
+        'path_prefix' => '',
+        'throw_exceptions' => (bool) env('APP_DEBUG', false),
+    ]
+],
+EOF,
+            'Run `php artisan migrate` to install new migration',
+            'Run `composer dump-autoload` if the migration is not automatically detected by Laravel!'
+        ]);
+    }
+
+    /*****************************************************************
+     * Internals
+     ****************************************************************/
+
+    /**
+     * Resolves the database adapter type
+     *
+     * @return string
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function resolveAdapterType(): string
+    {
+        // Obtain type or ask for it
+        $type = $this->option('type');
+        if (!isset($type)) {
+            $type = $this->choice('Type of database adapter?', $this->allowedAdapterTypes, 0);
+        }
+
+        $type = strtolower(trim($type));
+
+        // Ensure type is supported
+        if (!in_array($type, $this->allowedAdapterTypes)) {
+            throw new InvalidArgumentException(sprintf('Invalid adapter type. Accepted types are: %s', implode(', ', $this->allowedAdapterTypes)));
+        }
+
+        return $type;
+    }
+
+    /**
+     * Resolve the table name
+     *
+     * @return string
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function resolveTableName(): string
+    {
+        $validation = function($answer) {
+            if (!preg_match('/^[a-zA-Z_][a-zA-Z\p{N}_]{0,127}$/', $answer)) {
+                throw new InvalidArgumentException('Invalid table name provided. Please try again...');
+            }
+
+            return $answer;
+        };
+
+        // Obtain table name or ask for it
+        $name = $this->option('name');
+        if (!isset($name)) {
+            $name = $this->output->ask('Table name?', null, $validation);
+        }
+
+        // (Re)validate table name, when provided as option.
+        $validation($name);
+
+        return strtolower(trim($name));
+    }
+
+    /**
+     * Resolves path where migration file must be created
+     *
+     * @return string
+     */
+    protected function resolveMigrationsPath(): string
+    {
+        $path = $this->option('path');
+
+        if (!isset($path)) {
+            return $this->getLaravel()->databasePath('migrations');
+        }
+
+        return $this->getLaravel()->basePath($path);
+    }
+
+    /**
+     * Creates the migration file
+     *
+     * @param string $type
+     * @param string $table
+     * @param array $data
+     *
+     * @return void
+     */
+    protected function writeMigrationFile(string $type, string $table, array $data): void
+    {
+        $populated = $this->populateStub(
+            $this->getMigrationStub($type),
+            $data
+        );
+
+        $filename = $this->makeMigrationFilename($table, $this->resolveMigrationsPath());
+
+        $result = file_put_contents($filename, $populated);
+        if ($result === false) {
+            throw new RuntimeException(sprintf('Unable to write migration file: %s. Please check your permissions!', $filename));
+        }
+    }
+
+    /**
+     * Populate the migration stub with data
+     *
+     * @param string $stub
+     * @param array $data
+     *
+     * @return string
+     */
+    protected function populateStub(string $stub, array $data): string
+    {
+        foreach ($data as $key => $value) {
+            $stub = str_replace([ "{{ {$key} }}", "{{{$key}}}" ], $value, $stub);
+        }
+
+        return $stub;
+    }
+
+    /**
+     * Returns the migration stub's content, for given adapter type
+     *
+     * @param string $type Adapter type
+     *
+     * @return string
+     *
+     * @throws RuntimeException
+     */
+    protected function getMigrationStub(string $type): string
+    {
+        $path = realpath($this->stubsPath() . "/{$type}.stub");
+
+        if (!file_exists($path)) {
+            throw new RuntimeException(sprintf('Unable to find migration stub: %s', $path));
+        }
+
+        return file_get_contents($path);
+    }
+
+    /**
+     * Returns path to stubs directory
+     *
+     * @return string
+     */
+    protected function stubsPath(): string
+    {
+        return __DIR__ . '/../../stubs';
+    }
+
+    /**
+     * Makes migration filename for given table
+     *
+     * @param string $table
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function makeMigrationFilename(string $table, string $path): string
+    {
+        return $path . DIRECTORY_SEPARATOR . date('Y_m_d_His') . '_create_' . $table . '_table.php';
     }
 }
