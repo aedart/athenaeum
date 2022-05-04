3210bcc98815b27b2fdedadc095a9f5734973d98
alin <alin@rspsystems.com>
2022-05-04 12:13:56 +0200

    Add create directory recursively logic
    Also, ported the fetch directory method from deprecated adapter.

--- packages/Flysystem/Db/src/Adapters/DatabaseAdapter.php
+++ packages/Flysystem/Db/src/Adapters/DatabaseAdapter.php
@@ -2,11 +2,13 @@
 
 namespace Aedart\Flysystem\Db\Adapters;
 
+use Aedart\Contracts\Flysystem\Db\RecordTypes;
 use Aedart\Contracts\Support\Helpers\Database\DbAware;
 use Aedart\Flysystem\Db\Adapters\Concerns;
 use Aedart\Flysystem\Db\Exceptions\ConnectionException;
 use Aedart\Flysystem\Db\Exceptions\DatabaseAdapterException;
 use Aedart\Support\Helpers\Database\DbTrait;
+use Aedart\Utils\Str;
 use Illuminate\Database\ConnectionInterface;
 use League\Flysystem\Config;
 use League\Flysystem\FileAttributes;
@@ -22,6 +24,8 @@ use League\Flysystem\UnableToMoveFile;
 use League\Flysystem\UnableToReadFile;
 use League\Flysystem\UnableToRetrieveMetadata;
 use League\Flysystem\UnableToWriteFile;
+use RuntimeException;
+use stdClass;
 use Throwable;
 
 /**
@@ -71,7 +75,9 @@ class DatabaseAdapter implements FilesystemAdapter,
      */
     public function directoryExists(string $path): bool
     {
-        // TODO: Implement directoryExists() method.
+        $dir = $this->fetchDirectory($path);
+
+        return isset($dir);
     }
 
     /**
@@ -127,7 +133,56 @@ class DatabaseAdapter implements FilesystemAdapter,
      */
     public function createDirectory(string $path, Config $config): void
     {
-        // TODO: Implement createDirectory() method.
+        try {
+            $connection = $this->resolveConnection($config);
+
+            $path = $this->applyPrefix($path);
+            $visibility = $this->resolveDirectoryVisibility($config);
+            $timestamp = $this->resolveLastModifiedTimestamp($config);
+            $extra = $this->resolveExtraMetaData($config);
+
+            // Although not very clearly documented, Flysystem expects directories to
+            // be created recursively. Therefore, based on the given path one or more
+            // directories must be created.
+
+            // Create tree structure based on given directory path.
+            $directories = Str::tree($path, '/');
+
+            // Prepare directory records (the tree structure)
+            $records = [];
+            foreach ($directories as $directory) {
+                $records[] = [
+                    'type' => RecordTypes::DIRECTORY,
+                    'path' => $directory,
+                    'level' => $this->directoryLevel($directory),
+                    'visibility' => $visibility,
+                    'last_modified' => $timestamp,
+                    'extra_metadata' => $extra
+                ];
+            }
+
+            // Upsert directories - create them if they do not exist,
+            // or update them, if already created.
+            $result = $connection
+                ->table($this->filesTable)
+                ->upsert(
+                    values: $records,
+                    uniqueBy: [ 'path' ],
+                    update: [
+                        'level',
+                        'visibility',
+                        'last_modified',
+                        'extra_metadata'
+                    ]
+                );
+
+            if (!$result) {
+                throw new RuntimeException(sprintf('Directory was not created in table: %s', $this->filesTable));
+            }
+
+        } catch (Throwable $e) {
+            throw UnableToCreateDirectory::dueToFailure($path, $e);
+        }
     }
 
     /**
@@ -231,5 +286,72 @@ class DatabaseAdapter implements FilesystemAdapter,
         } catch (Throwable $e) {
             throw new DatabaseAdapterException($e->getMessage(), $e->getCode(), $e);
         }
+    }
+
+    /**
+     * Resolve database connection
+     *
+     * If "connection" is set in given configuration, then it will be returned.
+     * Otherwise, the adapter's default connection is returned.
+     *
+     * @param Config|null $config [optional]
+     *
+     * @return ConnectionInterface
+     */
+    protected function resolveConnection(Config|null $config = null): ConnectionInterface
+    {
+        if (!isset($config)) {
+            return $this->connection();
+        }
+
+        return $config->get('connection', $this->connection());
+    }
+
+    /**
+     * Fetch a directory record from given table that matches given path
+     *
+     * @param string $path
+     * @param Config|null $config [optional]
+     *
+     * @return stdClass|null
+     *
+     * @throws UnableToCheckExistence
+     */
+    protected function fetchDirectory(string $path, Config|null $config = null): stdClass|null
+    {
+        try {
+            $connection = $this->resolveConnection($config);
+
+            $result = $connection
+                ->table($this->filesTable)
+                ->select()
+                ->where('path', $this->applyPrefix($path))
+                ->where('type', RecordTypes::DIRECTORY)
+                ->limit(1)
+                ->get();
+
+            if ($result->isEmpty()) {
+                return null;
+            }
+
+            return $result->first();
+        } catch (Throwable $e) {
+            throw UnableToCheckExistence::forLocation($path, $e);
+        }
+    }
+
+    /**
+     * Returns the directory level for given path
+     *
+     * Note: this method does NOT apply path-prefixing on
+     * given path!
+     *
+     * @param string $path
+     *
+     * @return int
+     */
+    protected function directoryLevel(string $path): int
+    {
+        return substr_count($path, '/');
     }
 }
