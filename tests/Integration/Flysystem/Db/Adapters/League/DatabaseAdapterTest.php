c69eac50f3912ab53f02f13b53ee378d763ae6de
alin <alin@rspsystems.com>
2022-05-09 15:36:08 +0200

    Add leagues flysystem test utility

---
+++ tests/Integration/Flysystem/Db/Adapters/League/DatabaseAdapterTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Aedart\Tests\Integration\Flysystem\Db\Adapters\League;
+
+use Aedart\Container\IoC;
+use Aedart\Flysystem\Db\Adapters\DatabaseAdapter;
+use Codeception\Configuration;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase as BaseTestCase;
+use League\Flysystem\FilesystemAdapter;
+
+/**
+ * DatabaseAdapterTest
+ *
+ * @group flysystem-db
+ * @group flysystem-db-league-tests
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\Flysystem\Db\Adapters\League
+ */
+class DatabaseAdapterTest extends BaseTestCase
+{
+
+    /**
+     * Service Container instance
+     *
+     * @var Container|null
+     */
+    protected static Container|null $container = null;
+
+    /*****************************************************************
+     * Setup
+     ****************************************************************/
+
+    /**
+     * @inheritdoc
+     */
+    public static function tearDownAfterClass(): void
+    {
+        IoC::getInstance()->destroy();
+        static::$container = null;
+
+        parent::tearDownAfterClass();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        static::$container = IoC::getInstance()
+            ->registerAsApplication();
+
+        parent::setUp();
+    }
+
+    /*****************************************************************
+     * Adapter
+     ****************************************************************/
+
+    /**
+     * @inheritdoc
+     */
+    protected static function createFilesystemAdapter(): FilesystemAdapter
+    {
+        // This test extends Leagues official "fly-system testing utils". This means
+        // that a full Laravel is NOT up and running. A connection is therefore configured
+        // manually here...
+
+        /** @var \Illuminate\Container\Container $container */
+        $container = static::$container;
+
+        // Setup connection manager
+        $capsule = new Manager($container);
+        $capsule->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'foreign_key_constraints' => true,
+        ]);
+        $capsule->setAsGlobal();
+
+        // Bind schema builder, so we can run migration manually!
+        $container->singleton('db.schema', function() use($capsule) {
+            return $capsule->getConnection()->getSchemaBuilder();
+        });
+
+        // Run migrations
+        $migrationClass = require_once Configuration::dataDir() . 'flysystem/db/migrations/2022_05_04_071658_create_files_table.php';
+        /** @var Migration $migration */
+        $migration = new $migrationClass();
+        $migration->up();
+
+        // Finally, return adapter
+        return new DatabaseAdapter('files', 'file_contents', $capsule->getConnection());
+    }
+}
