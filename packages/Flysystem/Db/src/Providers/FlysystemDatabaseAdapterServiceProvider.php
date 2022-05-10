4104a725210cc4b550cae8941c6b82564c4cdbe9
alin <alin@rspsystems.com>
2022-05-10 11:34:46 +0200

    Change provider, enable db adapter

--- packages/Flysystem/Db/src/Providers/FlysystemDatabaseAdapterServiceProvider.php
+++ packages/Flysystem/Db/src/Providers/FlysystemDatabaseAdapterServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace Aedart\Flysystem\Db\Providers;
 
+use Aedart\Flysystem\Db\Adapters\DatabaseAdapter;
 use Aedart\Flysystem\Db\Console\MakeAdapterMigrationCommand;
+use Aedart\Support\Helpers\Database\ConnectionResolverTrait;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
 
 /**
  * Flysystem Database Adapter Service Provider
@@ -13,6 +17,8 @@ use Illuminate\Support\ServiceProvider;
  */
 class FlysystemDatabaseAdapterServiceProvider extends ServiceProvider
 {
+    use ConnectionResolverTrait;
+
     /**
      * Boot this service
      *
@@ -25,5 +31,33 @@ class FlysystemDatabaseAdapterServiceProvider extends ServiceProvider
                 MakeAdapterMigrationCommand::class
             ]);
         }
+
+        $this->enableAdapterAsStorageDriver();
+    }
+
+    /**
+     * Enables Database Adapter as a Storage Driver
+     *
+     * @return void
+     */
+    protected function enableAdapterAsStorageDriver(): void
+    {
+        Storage::extend('database', function($app, array $settings) {
+            // Resolve database connection
+            $connection = $this
+                ->getConnectionResolver()
+                ->connection(data_get($settings, 'connection', 'mysql'));
+
+            // Create and configure adapter
+            $adapter = (new DatabaseAdapter(
+                data_get($settings, 'files_table', 'files'),
+                data_get($settings, 'contents_table', 'files_contents'),
+                $connection,
+            ))
+                ->setHashAlgorithm(data_get($settings, 'hash_algo', 'sha256'))
+                ->setPathPrefix(data_get($settings, 'path_prefix', ''));
+
+            return new Filesystem($adapter);
+        });
     }
 }
