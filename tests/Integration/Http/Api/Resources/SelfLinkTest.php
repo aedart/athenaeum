9321e306a96880b7c6ff63ba8be9657d08d4af76
Alin Eugen Deac <aedart@gmail.com>
2022-10-15 13:07:21 +0200

    Add tests for api resource's self-link

---
+++ tests/Integration/Http/Api/Resources/SelfLinkTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Api\Resources;
+
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Models\Owner;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Resources\OwnerResource;
+use Aedart\Tests\TestCases\Http\ApiResourcesTestCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * SelfLinkTest
+ *
+ * @group http-api
+ * @group api-resource
+ * @group api-resource-self-link
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Http\Api\Resources
+ */
+class SelfLinkTest extends ApiResourcesTestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canGenerateResourceRouteName(): void
+    {
+        $resource = new OwnerResource(null);
+
+        $result = $resource->resourceRouteName();
+
+        $this->assertSame('owners.show', $result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canMakeSelfLink(): void
+    {
+        // ------------------------------------------------------------------ //
+        // Prerequisites - we need a route to the resource, with appropriate
+        // name...
+
+        Route::get('/owners/{id}', function() {
+            return response()->json();
+        })->name('owners.show');
+
+        // Refresh name lookup or test could fail...
+        Route::getRoutes()->refreshNameLookups();
+
+        // ------------------------------------------------------------------ //
+
+        // Create dummy request
+        $request = Request::create('something');
+
+        // Create model with a name... Note: model is "sluggable"
+        $name = $this->getFaker()->name;
+        $model = new Owner([ 'name' => $name ]);
+
+        $resource = new OwnerResource($model);
+
+        // ------------------------------------------------------------------ //
+
+        $result = $resource->makeSelfLink($request);
+        $expected = '/owners/' . rawurlencode($name);
+
+        ConsoleDebugger::output($result);
+
+        $this->assertStringEndsWith($expected, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canMakeSelfLinkUsingCustomCallback(): void
+    {
+        $url = '/my-custom-resource-link';
+
+        $resource = new OwnerResource(null);
+        $resource->withSelfLink($url);
+
+        // ------------------------------------------------------------------ //
+
+        $result = $resource->makeSelfLink(
+            Request::create('something')
+        );
+
+        ConsoleDebugger::output($result);
+
+        $this->assertStringEndsWith($url, $result);
+    }
+}
