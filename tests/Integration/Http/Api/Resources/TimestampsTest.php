c4713a9d74b2c4c841d482b6a0777003ebc92862
Alin Eugen Deac <aedart@gmail.com>
2022-10-15 13:49:57 +0200

    Add tests for api resource's timestamps

---
+++ tests/Integration/Http/Api/Resources/TimestampsTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Api\Resources;
+
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Models\Game;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Resources\GameResource;
+use Aedart\Tests\TestCases\Http\ApiResourcesTestCase;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * TimestampsTest
+ *
+ * @group http-api
+ * @group api-resource
+ * @group api-resource-timestamps
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Integration\Http\Api\Resources
+ */
+class TimestampsTest extends ApiResourcesTestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws ValidationException
+     */
+    public function addsTimestampsToPayload(): void
+    {
+        $faker = $this->getFaker();
+
+        $model = new Game([
+            'slug' => $faker->slug(3),
+            'name' => $faker->text(25),
+            'description' => $faker->realText(100),
+            'created_at' => now()->subDays($faker->randomDigitNotNull),
+            'updated_at' => now()->subMinutes($faker->randomDigitNotNull),
+            'deleted_at' => now()
+        ]);
+
+        $resource = new GameResource($model);
+
+        // -------------------------------------------------------------------- //
+
+        $payload = $resource->toArray(Request::create('something'));
+
+        ConsoleDebugger::output($payload);
+
+        // -------------------------------------------------------------------- //
+
+        $this->assertArrayHasKey('created_at', $payload);
+        $this->assertNotEmpty($payload['created_at'], 'Empty created at property');
+
+        $this->assertArrayHasKey('updated_at', $payload);
+        $this->assertNotEmpty($payload['updated_at'], 'Empty updated at property');
+
+        $this->assertArrayHasKey('deleted_at', $payload);
+        $this->assertNotEmpty($payload['deleted_at'], 'Empty deleted at property');
+
+        $this->assertArrayHasKey('deleted', $payload);
+        $this->assertNotEmpty($payload['deleted'], 'Empty deleted property');
+        $this->assertTrue($payload['deleted'], 'Deleted state should be true');
+    }
+}
