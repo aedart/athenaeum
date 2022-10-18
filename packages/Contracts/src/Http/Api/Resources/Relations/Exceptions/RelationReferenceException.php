82b762e61a1cdf7126c2be0b24a89eca3debdb3c
Alin Eugen Deac <aedart@gmail.com>
2022-10-18 16:22:42 +0200

    Add relation reference interface
    This is intended to represent an eager-loaded relation in the Api Resource's model. It can be used to format how the relation must be displayed.

---
+++ packages/Contracts/src/Http/Api/Resources/Relations/Exceptions/RelationReferenceException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Contracts\Http\Api\Resources\Relations\Exceptions;
+
+use Throwable;
+
+/**
+ * Relation Reference Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Http\Api\Resources\Relations\Exceptions
+ */
+interface RelationReferenceException extends Throwable
+{
+}
