1d3cce402b1430de0df686c97916a68bd2770faf
Alin Eugen Deac <aedart@gmail.com>
2022-10-29 19:10:45 +0200

    Add list resources request abstraction

---
+++ packages/Http/Api/src/Requests/Resources/ListResourcesRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Aedart\Http\Api\Requests\Resources;
+
+use Aedart\Http\Api\Requests\Concerns;
+use Aedart\Http\Api\Requests\ValidatedApiRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+/**
+ * List Resources Request
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Http\Api\Requests\Resources
+ */
+abstract class ListResourcesRequest extends ValidatedApiRequest
+{
+    use Concerns\Pagination;
+
+    /**
+     * @inheritdoc
+     */
+    public function authorize(): bool
+    {
+        return $this->allows('index', $this->authorisationModel());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rules(): array
+    {
+        return $this->withPagination(
+            parent::rules()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function prepareForAfterValidation(Validator $validator): void
+    {
+        $this->preparePagination($validator);
+
+        parent::prepareForAfterValidation($validator);
+    }
+}
