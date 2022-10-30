058b54976d0179aa9f5eacfadf76fdcc5c5fcd57
Alin Eugen Deac <aedart@gmail.com>
2022-10-30 10:47:15 +0100

    Rename hook method
    New method name sound better...

--- packages/Http/Api/src/Requests/Concerns/SingleRecord.php
+++ packages/Http/Api/src/Requests/Concerns/SingleRecord.php
@@ -41,18 +41,21 @@ trait SingleRecord
     {
         $this->record = $this->findRecordOrFail($validator);
 
-        $this->onRecordFound($this->record, $validator);
+        $this->whenRecordIsFound($this->record, $validator);
     }
 
     /**
-     * Hook method for when requested record was found
+     * Hook method for when requested record is found
+     *
+     * This method is invoked immediately after {@see findRecordOrFail},
+     * if a record was found.
      *
      * @param \Illuminate\Database\Eloquent\Model $record
      * @param  Validator  $validator
      *
      * @return void
      */
-    public function onRecordFound($record, Validator $validator): void
+    public function whenRecordIsFound($record, Validator $validator): void
     {
         // N/A - Overwrite this method if you need additional prepare or
         // validation logic, immediately after requested record was found.
