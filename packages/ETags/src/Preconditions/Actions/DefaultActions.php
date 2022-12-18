a24dc93e3c12f09d420b79e252c42c49232945d8
Alin Eugen Deac <aedart@gmail.com>
2022-12-18 16:09:34 +0100

    Add default precondition actions

---
+++ packages/ETags/src/Preconditions/Actions/DefaultActions.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Aedart\ETags\Preconditions\Actions;
+
+use Aedart\Contracts\ETags\Preconditions\Actions;
+use Aedart\Contracts\ETags\Preconditions\ResourceContext;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
+
+/**
+ * Default Actions
+ *
+ * @see Actions
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\ETags\Preconditions\Actions
+ */
+class DefaultActions implements Actions
+{
+    /**
+     * @inheritDoc
+     */
+    public function abortStateChangeAlreadySucceeded(ResourceContext $resource)
+    {
+        throw new HttpException(200);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function abortPreconditionFailed(ResourceContext $resource)
+    {
+        throw new PreconditionFailedHttpException();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function abortNotModified(ResourceContext $resource)
+    {
+        throw new HttpException(304);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function processIfRangePartialContent(ResourceContext $resource): mixed
+    {
+        // Application SHOULD process the "Range" header field.
+        return $resource->set('process_range', true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function ignoreRangeHeader(ResourceContext $resource): mixed
+    {
+        // Application SHOULD ignore the "Range" header field
+        return $resource->set('process_range', false);
+    }
+}
