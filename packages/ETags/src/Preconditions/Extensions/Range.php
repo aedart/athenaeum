195de4a764a149376767f711e30aaf1ea311a9ce
Alin Eugen Deac <aedart@gmail.com>
2022-12-18 16:32:21 +0100

    Add range "extension"

---
+++ packages/ETags/src/Preconditions/Extensions/Range.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Aedart\ETags\Preconditions\Extensions;
+
+use Aedart\Contracts\ETags\Preconditions\ResourceContext;
+use Aedart\ETags\Preconditions\BasePrecondition;
+
+/**
+ * Range extension
+ *
+ * @see https://httpwg.org/specs/rfc9110.html#field.range
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\ETags\Preconditions\Extensions
+ */
+class Range extends BasePrecondition
+{
+    /**
+     * @inheritDoc
+     */
+    public function isApplicable(ResourceContext $resource): bool
+    {
+        // TODO: Implement isApplicable() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function passes(ResourceContext $resource): bool
+    {
+        // TODO: Implement passes() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whenPasses(ResourceContext $resource): ResourceContext|string
+    {
+        // TODO: Implement whenPasses() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whenFails(ResourceContext $resource): ResourceContext|string
+    {
+        // TODO: Implement whenFails() method.
+    }
+}
