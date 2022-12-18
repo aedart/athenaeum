dec7048e2cdcc652d1b47ccebaa7c6ab564ed1de
Alin Eugen Deac <aedart@gmail.com>
2022-12-18 16:47:00 +0100

    Change current request concern, add additional util methods
    Ported the request util methods from the deprecated preconditions evaluator.

--- packages/ETags/src/Preconditions/Concerns/CurrentRequest.php
+++ packages/ETags/src/Preconditions/Concerns/CurrentRequest.php
@@ -2,6 +2,9 @@
 
 namespace Aedart\ETags\Preconditions\Concerns;
 
+use Aedart\Contracts\ETags\Collection;
+use Aedart\Contracts\ETags\ETag;
+use DateTimeInterface;
 use Illuminate\Http\Request;
 
 /**
@@ -53,5 +56,93 @@ trait CurrentRequest
     public function request()
     {
         return $this->getRequest();
+    }
+
+    /**
+     * Get the request method
+     *
+     * @return string
+     */
+    protected function getMethod(): string
+    {
+        return $this->request()->getMethod();
+    }
+
+    /**
+     * Determine if request method is safe or not
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Glossary/Safe/HTTP
+     * @see https://developer.mozilla.org/en-US/docs/Glossary/Idempotent
+     *
+     * @return bool
+     */
+    protected function isSafeMethod(): bool
+    {
+        return $this->request()->isMethodSafe();
+    }
+
+    /**
+     * Get the Http Headers
+     *
+     * @return \Symfony\Component\HttpFoundation\HeaderBag
+     */
+    protected function getHeaders()
+    {
+        return $this->request()->headers;
+    }
+
+    /**
+     * Get collection of etags from the If-Match header
+     *
+     * @return Collection
+     */
+    protected function getIfMatchEtags(): Collection
+    {
+        // @see \Aedart\ETags\Mixins\RequestETagsMixin::ifMatchEtags
+        return $this->request()->ifMatchEtags();
+    }
+
+    /**
+     * Get collection of etags from the If-None-Match header
+     *
+     * @return Collection
+     */
+    protected function getIfNoneMatchEtags(): Collection
+    {
+        // @see \Aedart\ETags\Mixins\RequestETagsMixin::ifNoneMatchEtags
+        return $this->request()->ifNoneMatchEtags();
+    }
+
+    /**
+     * Get `If-Unmodified-Since` Http Date, when available
+     *
+     * @return DateTimeInterface|null
+     */
+    protected function getIfUnmodifiedSinceDate(): DateTimeInterface|null
+    {
+        // @see \Aedart\ETags\Mixins\RequestETagsMixin::ifUnmodifiedSinceDate
+        return $this->request()->ifUnmodifiedSinceDate();
+    }
+
+    /**
+     * Get `If-Modified-Since` Http Date, when available
+     *
+     * @return DateTimeInterface|null
+     */
+    protected function getIfModifiedSinceDate(): DateTimeInterface|null
+    {
+        // @see \Aedart\ETags\Mixins\RequestETagsMixin::ifModifiedSinceDate
+        return $this->request()->ifModifiedSinceDate();
+    }
+
+    /**
+     * Get Etag or Datetime from the `If-Range` header, if any is available
+     *
+     * @return ETag|DateTimeInterface|null
+     */
+    protected function getIfRangeEtagOrDate(): ETag|DateTimeInterface|null
+    {
+        // @see \Aedart\ETags\Mixins\RequestETagsMixin::ifRangeEtagOrDate
+        return $this->request()->ifRangeEtagOrDate();
     }
 }
