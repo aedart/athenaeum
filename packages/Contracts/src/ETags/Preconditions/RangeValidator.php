c0d6a29274fde9719c12b8a1dc8a92412533be7a
Alin Eugen Deac <aedart@gmail.com>
2022-12-25 15:53:03 +0100

    Add range validator
    This is a sub-component for preconditions that concern themselves with "If-Range" and "Range" requests.

---
+++ packages/Contracts/src/ETags/Preconditions/RangeValidator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Aedart\Contracts\ETags\Preconditions;
+
+use Ramsey\Collection\CollectionInterface;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+/**
+ * Http "Range" Validator
+ *
+ * Specialised validator that is intended to be a subcomponent of Http Request
+ * preconditions, when determining if requested "Range" is applicable or not.
+ *
+ * @see https://httpwg.org/specs/rfc9110.html#field.range
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\ETags\Preconditions
+ */
+interface RangeValidator extends
+    HasRequest,
+    HasActions
+{
+    /**
+     * Validates requested "Range" header field, for given resource
+     *
+     * Method is able to use abort {@see Actions} if requested range is invalid
+     * or not satisfiable.
+     *
+     * @param  ResourceContext  $resource
+     *
+     * @return CollectionInterface Validated unit ranges. Empty collection, if none was requested e.g. empty "Range" header.
+     *                             Or if a single range is requested that matches resource's total size.
+     *
+     *
+     * @throws HttpExceptionInterface
+     */
+    public function validate(ResourceContext $resource): CollectionInterface;
+}
