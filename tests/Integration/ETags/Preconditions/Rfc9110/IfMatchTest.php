f60e5b823109525298b640b4343255fcc7237b07
alin <alin@rspsystems.com>
2023-01-11 15:23:54 +0100

    Add If-Match tests (incomplete)

---
+++ tests/Integration/ETags/Preconditions/Rfc9110/IfMatchTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aedart\Tests\Integration\ETags\Preconditions\Rfc9110;
+
+use Aedart\Tests\TestCases\ETags\PreconditionsTestCase;
+
+/**
+ * IfMatchTest
+ *
+ * @group etags
+ * @group preconditions
+ * @group preconditions-if-match
+ * @group rfc9110
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\ETags\Preconditions\Rfc9110
+ */
+class IfMatchTest extends PreconditionsTestCase
+{
+    // TODO: Incomplete...
+}
