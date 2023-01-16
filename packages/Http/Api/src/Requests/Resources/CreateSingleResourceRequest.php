1669cb04eeb3509d2a4e2e01353db511c80b2315
alin <alin@rspsystems.com>
2023-01-16 14:05:22 +0100

    Change create single resource request, add precondition evaluation
    Not sure how many API "create new resource" requests will be able to make use of this, but it can now be enabled if developer wishes to support this.

--- packages/Http/Api/src/Requests/Resources/CreateSingleResourceRequest.php
+++ packages/Http/Api/src/Requests/Resources/CreateSingleResourceRequest.php
@@ -2,7 +2,13 @@
 
 namespace Aedart\Http\Api\Requests\Resources;
 
+use Aedart\Contracts\ETags\ETag;
 use Aedart\Http\Api\Requests\ValidatedApiRequest;
+use DateTimeInterface;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Throwable;
 
 /**
  * Create Single Resource Request
@@ -18,5 +24,94 @@ abstract class CreateSingleResourceRequest extends ValidatedApiRequest
     public function authorize(): bool
     {
         return $this->allows('store', $this->authorisationModel());
+    }
+
+    /**
+     * Determine if this request supports preconditions
+     *
+     * @return bool True if preconditions must be evaluated
+     */
+    abstract public function mustEvaluateRequestPreconditions(): bool;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws HttpExceptionInterface
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function after(Validator $validator): void
+    {
+        // After received data has been validated, we can use it to evaluate evt. request
+        // preconditions.
+        $this->performRequestPreconditionsEvaluation(
+            $validator->validated()
+        );
+
+        // Extend this to add additional business logic validation... etc
+    }
+
+    /**
+     * Performs request preconditions evaluation, if supported
+     *
+     * @param array $data Received input data
+     *
+     * @return void
+     *
+     * @throws HttpExceptionInterface
+     * @throws Throwable
+     */
+    public function performRequestPreconditionsEvaluation(array $data): void
+    {
+        if (!$this->mustEvaluateRequestPreconditions()) {
+            return;
+        }
+
+        // [...] a recipient cache or origin server MUST evaluate received request preconditions after
+        // it has successfully performed its normal request checks and JUST BEFORE it would process the
+        // request content (if any) or perform the action associated with the request method. [...]
+        // @see https://httpwg.org/specs/rfc9110.html#when.to.evaluate
+
+        $this->evaluateRequestPreconditions(
+            record: $data,
+            etag: $this->generateEtag($data),
+            lastModifiedDate: $this->generateLastModifiedDate($data)
+        );
+    }
+
+    /**
+     * Returns an {@see ETag} for given data, if able to
+     *
+     * Applicable for "If-Match" and "If-None-Match" preconditions.
+     *
+     * @param array $data
+     *
+     * @return ETag|null
+     */
+    protected function generateEtag(array $data): ETag|null
+    {
+        // If you are able to generate an etag for the resource that is about to be
+        // created, then overwrite this method...
+        // E.g. return \Aedart\ETags\Facades\Generator::makeStrong($data);
+
+        return null;
+    }
+
+    /**
+     * Returns "last modified date" for given data, if able to
+     *
+     * Applicable for "If-Modified-Since" and "If-Unmodified-Since" preconditions
+     *
+     * @param array $data
+     *
+     * @return DateTimeInterface|null
+     */
+    protected function generateLastModifiedDate(array $data): DateTimeInterface|null
+    {
+        // Although a resource is not yet created, we can relatively safely assume that the
+        // "last modified date" will be "now" (for a new Eloquent models).
+        // @see \Illuminate\Database\Eloquent\Concerns\HasTimestamps::updateTimestamps
+
+        return now();
     }
 }
