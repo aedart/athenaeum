6cfecaf92b2282e94df77b8243f871daec739952
alin <alin@rspsystems.com>
2023-01-18 15:55:54 +0100

    Add tests for list resources request

---
+++ tests/Helpers/Dummies/Http/Api/Requests/Users/ListUsersRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Aedart\Tests\Helpers\Dummies\Http\Api\Requests\Users;
+
+use Aedart\Http\Api\Requests\Resources\ListResourcesRequest;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Models\User;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Requests\Users\Filters\UsersFiltersBuilder;
+
+/**
+ * List Users Request
+ *
+ * FOR TESTING PURPOSES ONLY!
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Helpers\Dummies\Http\Api\Requests\Users
+ */
+class ListUsersRequest extends ListResourcesRequest
+{
+    /**
+     * @inheritDoc
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function filtersBuilder(): string|null
+    {
+        return UsersFiltersBuilder::class;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function authorisationModel(): string|null
+    {
+        return User::class;
+    }
+}
---
+++ tests/Integration/Http/Api/Requests/ListResourcesRequestTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Aedart\Tests\Integration\Http\Api\Requests;
+
+use Aedart\Testing\Helpers\Http\Response;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Models\User;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Requests\Users\ListUsersRequest;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Resources\UserResource;
+use Aedart\Tests\TestCases\Http\ApiResourceRequestsTestCase;
+use Illuminate\Support\Facades\Route;
+use JsonException;
+
+/**
+ * ListResourcesRequestTest
+ *
+ * @group http-api
+ * @group api-resource
+ * @group api-resource-requests
+ * @group list-resources-request
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\Http\Api\Requests
+ */
+class ListResourcesRequestTest extends ApiResourceRequestsTestCase
+{
+    /**
+     * @inheritDoc
+     */
+    protected function _before()
+    {
+        parent::_before();
+
+        // Debugging
+        // $this->withoutExceptionHandling();
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws JsonException
+     */
+    public function canListPaginatedResources(): void
+    {
+        Route::get('/users', function (ListUsersRequest $request) {
+            return UserResource::collection(
+                User::query()
+                    ->paginate($request->show)
+            );
+        })->name('users.index');
+
+        Route::getRoutes()->refreshNameLookups();
+
+        // ------------------------------------------------------------------ //
+
+        $limit = 5;
+
+        $url = route('users.index') . '?show=' . $limit . '&page=2';
+        $response = $this
+            ->getJson($url)
+            ->assertOk();
+
+        $content = Response::decode($response);
+
+        // ------------------------------------------------------------------ //
+
+        $this->assertArrayHasKey('data', $content);
+        $this->assertCount($limit, $content['data']);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws JsonException
+     */
+    public function canBuildAndApplyFilters(): void
+    {
+        /** @var User $expected */
+        $expected = User::factory()
+            ->create();
+
+        // ------------------------------------------------------------------ //
+
+        Route::get('/users', function (ListUsersRequest $request) {
+            return UserResource::collection(
+                User::applyFilters($request->filters->all())
+                    ->paginate($request->show)
+            );
+        })->name('users.index');
+
+        Route::getRoutes()->refreshNameLookups();
+
+        // ------------------------------------------------------------------ //
+
+        $url = route('users.index') . '?search=' . $expected->name . '&show=1';
+        $response = $this
+            ->getJson($url)
+            ->assertOk();
+
+        $content = Response::decode($response);
+
+        // ------------------------------------------------------------------ //
+
+        $this->assertArrayHasKey('data', $content);
+        $this->assertCount(1, $content['data']);
+
+        $found = $content['data'][0];
+        $this->assertSame($expected->name, $found['name']);
+    }
+}
