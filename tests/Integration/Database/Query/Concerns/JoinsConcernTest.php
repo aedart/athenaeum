29b1d3dbab3051b1b01ad68e7e4989c85cb5632b
alin <alin@rspsystems.com>
2023-01-20 16:11:44 +0100

    Add query joins concern

---
+++ tests/Integration/Database/Query/Concerns/JoinsConcernTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Aedart\Tests\Integration\Database\Query\Concerns;
+
+use Aedart\Database\Query\Concerns\Joins;
+use Aedart\Tests\Helpers\Dummies\Database\Models\Category;
+use Aedart\Tests\TestCases\Database\DatabaseTestCase;
+
+/**
+ * JoinsConcernTest
+ *
+ * @group database
+ * @group db
+ * @group db-query
+ * @group db-query-concerns
+ * @group query-joins-concern
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Tests\Integration\Database\Query\Concerns
+ */
+class JoinsConcernTest extends DatabaseTestCase
+{
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Returns a class what uses desired concern
+     *
+     * @return object
+     */
+    public function makeConcern()
+    {
+        return new class {
+            use Joins;
+        };
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function returnsFalseWhenQueryHasNoJoins(): void
+    {
+        $query = Category::query();
+
+        $result = $this->makeConcern()
+            ->hasJoinTo($query, 'some_table');
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function returnsTrueWhenQueryHasJoinExpressionToTable(): void
+    {
+        $table = 'owners';
+
+        $query = Category::query()
+            ->join($table, 'id', '=', 'owner_id');
+
+        // ------------------------------------------------------------ //
+
+        $result = $this->makeConcern()
+            ->hasJoinTo($query, $table);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function returnsFalseWhenNotJoinExpressionToDesiredTable(): void
+    {
+        $query = Category::query()
+            ->join('owners', 'id', '=', 'owner_id');
+
+        // ------------------------------------------------------------ //
+
+        $result = $this->makeConcern()
+            ->hasJoinTo($query, 'stores');
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function appliesJoinWhenNotAlreadyJoinedToTable(): void
+    {
+        $concern = $this->makeConcern();
+        $query = Category::query();
+
+        $query = $concern
+            ->safeJoin($query, 'owners', 'id', '=', 'owner_id');
+
+        $this->assertTrue($concern->hasJoinTo($query, 'owners'));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function doesNotApplyJoinIfAlreadyJoinedToTable(): void
+    {
+        $concern = $this->makeConcern();
+        $query = Category::query()
+            ->join('owners', 'id', '=', 'owner_id');
+
+        $query = $concern
+            ->safeJoin($query, 'owners', 'id', '=', 'owner_id');
+
+        $this->assertTrue($concern->hasJoinTo($query, 'owners'));
+
+        $this->assertCount(1, $query->getQuery()->joins, 'Too many join expressions added');
+    }
+}
