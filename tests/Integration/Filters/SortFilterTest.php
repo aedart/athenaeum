181d8b1d18b277afb8248a7432fb5755371f7070
alin <alin@rspsystems.com>
2023-01-23 09:23:08 +0100

    Add tests for sorting callbacks

--- tests/Integration/Filters/SortFilterTest.php
+++ tests/Integration/Filters/SortFilterTest.php
@@ -2,10 +2,13 @@
 
 namespace Aedart\Tests\Integration\Filters;
 
+use Aedart\Filters\Query\Filters\BaseSortingQuery;
 use Aedart\Filters\Query\Filters\SortFilter;
 use Aedart\Testing\Helpers\ConsoleDebugger;
 use Aedart\Tests\Helpers\Dummies\Database\Models\Category;
 use Aedart\Tests\TestCases\Filters\FiltersTestCase;
+use Illuminate\Contracts\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Contracts\Database\Query\Builder;
 
 /**
  * SortFilterTest
@@ -20,8 +23,10 @@ class SortFilterTest extends FiltersTestCase
 {
     /**
      * @test
+     *
+     * @return void
      */
-    public function canBeApplied()
+    public function canBeApplied(): void
     {
         $filter = new SortFilter([
             'slug' => 'desc',
@@ -37,5 +42,62 @@ class SortFilterTest extends FiltersTestCase
         $this->assertStringContainsString('name', $sql);
         $this->assertStringContainsString('desc', $sql);
         $this->assertStringContainsString('asc', $sql);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canApplySortingCallback(): void
+    {
+        $filter = (new SortFilter([
+            'slug' => 'desc',
+            'name' => 'asc'
+        ]))->withSortingCallback('name', function($query, $column, $direction) {
+            return $query->orderBy("users.{$column}", $direction);
+        });
+
+        $query = $filter->apply(Category::query());
+        $sql = $query->toSql();
+
+        ConsoleDebugger::output($sql);
+
+        $this->assertStringContainsString('order by', $sql);
+        $this->assertStringContainsString('`users`.`name` asc', $sql);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canApplyInvokableSortingQuery(): void
+    {
+        $sortingQuery = new class extends BaseSortingQuery
+        {
+            public function __invoke(
+                Builder|EloquentBuilder $query,
+                string $column,
+                string $direction = 'asc'
+            ): Builder|EloquentBuilder {
+                return $query->orderBy("users.{$column}", $direction);
+            }
+        };
+
+        $filter = (new SortFilter([
+            'slug' => 'desc',
+            'age' => 'desc'
+        ]))->withSortingCallbacks([
+            'age' => $sortingQuery
+        ]);
+
+        $query = $filter->apply(Category::query());
+        $sql = $query->toSql();
+
+        ConsoleDebugger::output($sql);
+
+        $this->assertStringContainsString('order by', $sql);
+        $this->assertStringContainsString('`users`.`age` desc', $sql);
     }
 }
