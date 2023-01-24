d910005d1e3c6f987d3fa84107b0f2db637dd134
alin <alin@rspsystems.com>
2023-01-24 13:06:05 +0100

    Deprecate trait in favour of "model audit trail" concern

--- packages/Audit/src/Traits/HasAuditTrail.php
+++ packages/Audit/src/Traits/HasAuditTrail.php
@@ -2,37 +2,17 @@
 
 namespace Aedart\Audit\Traits;
 
-use Aedart\Audit\Models\AuditTrail;
-use Aedart\Audit\Models\Concerns\AuditTrailConfiguration;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Aedart\Audit\Concerns\AuditTrail;
 
 /**
+ * @deprecated Since 7.0, replaced by {@see \Aedart\Audit\Concerns\AuditTrail}
+ *
  * Has Audit Trails
- *
- * Intended to be used by a "user" model, which is able to cause models
- * to store audit trail entries.
- *
- * @property-read AuditTrail[]|Collection $auditTrail Audit trail entries caused by this user
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Audit\Traits
  */
 trait HasAuditTrail
 {
-    use AuditTrailConfiguration;
-
-    /*****************************************************************
-     * Relations
-     ****************************************************************/
-
-    /**
-     * Returns the audit trail entries caused by this user
-     *
-     * @return HasMany
-     */
-    public function auditTrail(): HasMany
-    {
-        return $this->hasMany($this->auditTrailModel());
-    }
+    use AuditTrail;
 }
