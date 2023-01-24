38d4b02a6d3035f4908ad3a0d3f4d38da5de9804
alin <alin@rspsystems.com>
2023-01-24 16:45:22 +0100

    Add test that proves message restoring in audit callback

--- tests/Integration/Audit/D0_AuditCallbackTest.php
+++ tests/Integration/Audit/D0_AuditCallbackTest.php
@@ -6,6 +6,7 @@ use Aedart\Audit\Helpers\Callback;
 use Aedart\Audit\Models\AuditTrail;
 use Aedart\Testing\Helpers\ConsoleDebugger;
 use Aedart\Tests\TestCases\Audit\AuditTestCase;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * D0_AuditCallbackTest
@@ -53,5 +54,54 @@ class D0_AuditCallbackTest extends AuditTestCase
 
         $this->assertNotNull($history);
         $this->assertSame($reason, $history->message);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function canRestorePreviousReasons(): void
+    {
+        $reason = 'a';
+
+        $category = Callback::perform(function () {
+            $categoryA = $this->makeCategory();
+
+            $changed = Callback::perform(function () use ($categoryA) {
+                $categoryA
+                    ->forceFill([
+                        'name' => 'other name',
+                    ])
+                    ->save();
+
+                return $categoryA;
+            })->because('b')->execute();
+
+            $categoryA
+                ->forceFill([
+                    'name' => 'yet another name'
+                ])
+                ->save();
+
+            return $changed;
+        })
+            ->because($reason)
+            ->execute();
+
+        // -------------------------------------------------------- //
+
+        /** @var Collection<AuditTrail> $history */
+        $history = $category->recordedChanges()->get();
+
+        ConsoleDebugger::output($history->toArray());
+
+        // -------------------------------------------------------- //
+
+        $this->assertNotNull($history);
+        $this->assertCount(2, $history);
+
+        $this->assertSame('b', $history[0]->message);
+        $this->assertSame('a', $history[1]->message);
     }
 }
