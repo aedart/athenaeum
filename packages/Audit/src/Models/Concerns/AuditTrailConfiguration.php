63fc839f0fac3c92d039aba6d02dd307e76ba0d4
alin <alin@rspsystems.com>
2023-01-24 13:10:58 +0100

    Deprecate in favour of new audit trail configuration concern

--- packages/Audit/src/Models/Concerns/AuditTrailConfiguration.php
+++ packages/Audit/src/Models/Concerns/AuditTrailConfiguration.php
@@ -2,12 +2,11 @@
 
 namespace Aedart\Audit\Models\Concerns;
 
-use Aedart\Audit\Models\AuditTrail;
-use Aedart\Support\Helpers\Config\ConfigTrait;
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
+use Aedart\Audit\Concerns\AuditTrailConfig;
 
 /**
+ * @deprecated Since 7.0, replaced by {@see \Aedart\Audit\Concerns\AuditTrailConfig}
+ *
  * Concerns Audit Trail Configuration
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
@@ -15,72 +14,5 @@ use Illuminate\Database\Eloquent\Model;
  */
 trait AuditTrailConfiguration
 {
-    use ConfigTrait;
-
-    /**
-     * Returns Audit Trail model class path
-     *
-     * @return string
-     */
-    public function auditTrailModel(): string
-    {
-        return $this->getConfig()->get('audit-trail.models.audit_trail', AuditTrail::class);
-    }
-
-    /**
-     * Returns Audit Trail model instance
-     *
-     * @return AuditTrail|Model
-     */
-    public function auditTrailModelInstance(): Model|AuditTrail
-    {
-        return $this->auditTrailModel()::make();
-    }
-
-    /**
-     * Returns Application User Model class path
-     *
-     * @return string
-     */
-    public function auditTrailUserModel(): string
-    {
-        return $this->getConfig()->get('audit-trail.models.user', \App\Models\User::class);
-    }
-
-    /**
-     * Returns Application User Model instance
-     *
-     * Warning: Method does NOT return current authenticated user;
-     * it only returns an empty user model instance!
-     *
-     * @return Model|Authenticatable
-     */
-    public function auditTrailUserModelInstance(): Model|Authenticatable
-    {
-        // NOTE: We cannot rely on the "make()" method being available
-        // for the user model. So we create a new instance the old
-        // fashion way...
-        $class = $this->auditTrailUserModel();
-        return new $class();
-    }
-
-    /**
-     * Returns table name of the audit trail(s) table
-     *
-     * @return string
-     */
-    public function auditTrailTable(): string
-    {
-        return $this->getConfig()->get('audit-trail.table', 'audit_trails');
-    }
-
-    /**
-     * Returns table attributes column datatype
-     *
-     * @return string
-     */
-    public function auditTableAttributesColumnType(): string
-    {
-        return $this->getConfig()->get('audit-trail.attributes_column_type', 'json');
-    }
+    use AuditTrailConfig;
 }
