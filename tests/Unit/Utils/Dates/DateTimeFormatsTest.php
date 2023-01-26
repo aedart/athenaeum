68d332bc405e20b5efc106c9f242cb95853aeee1
alin <alin@rspsystems.com>
2023-01-26 13:49:26 +0100

    Add date and time formats
    PHP's predefined formats, along with a few additional formats.

---
+++ tests/Unit/Utils/Dates/DateTimeFormatsTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Aedart\Tests\Unit\Utils\Dates;
+
+use Aedart\Contracts\Utils\Dates\DateTimeFormats;
+use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Testing\TestCases\UnitTestCase;
+use Illuminate\Support\Carbon;
+
+/**
+ * DateTimeFormatsTest
+ *
+ * @group utils
+ * @group date
+ * @group datetime-formats
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Tests\Unit\Utils\Dates
+ */
+class DateTimeFormatsTest extends UnitTestCase
+{
+    public function formatsProvider(): array
+    {
+        return [
+            'RFC3339 ZULU, using "Z"' => [
+                DateTimeFormats::RFC3339_ZULU,
+                '2005-08-15T15:52:01Z'
+            ],
+            'RFC3339 ZULU, using offset' => [
+                DateTimeFormats::RFC3339_ZULU,
+                '2005-08-15T15:52:01+01:00'
+            ],
+            'RFC3339 ZULU, using UTC offset' => [
+                DateTimeFormats::RFC3339_ZULU,
+                '2005-08-15T15:52:01-00:00'
+            ],
+
+            // ------------------------------------------------------------- //
+
+            'RFC3339 EXTENDED ZULU, using "Z"' => [
+                DateTimeFormats::RFC3339_EXTENDED_ZULU,
+                '2005-08-15T15:52:01.123Z'
+            ],
+            'RFC3339 EXTENDED ZULU, using offset' => [
+                DateTimeFormats::RFC3339_EXTENDED_ZULU,
+                '2005-08-15T15:52:01.321+01:00'
+            ],
+            'RFC3339 EXTENDED ZULU, using UTC offset' => [
+                DateTimeFormats::RFC3339_EXTENDED_ZULU,
+                '2005-08-15T15:52:01.000-00:00'
+            ],
+
+            // ------------------------------------------------------------- //
+
+            // TODO: Enable this from PHP v8.2
+//            'ISO8601 EXPANDED' => [
+//                DateTimeFormats::ISO8601_EXPANDED,
+//                '+10191-07-26T08:59:52+01:00'
+//            ]
+
+            // ------------------------------------------------------------- //
+
+            'RFC9110 (Http Date)' => [
+                DateTimeFormats::RFC9110,
+                'Sun, 06 Nov 1994 08:49:37 GMT'
+            ],
+        ];
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    /**
+     * @test
+     * @dataProvider formatsProvider
+     *
+     * @param string $format
+     * @param string $dateStr
+     *
+     * @return void
+     */
+    public function canCreateFromFormat(string $format, string $dateStr): void
+    {
+        // a) Determine if date string has specified format
+        $this->assertTrue(Carbon::hasFormat($dateStr, $format), 'Invalid format of date string');
+
+        // b) Attempt to create date instance... If it does not fail, all is good
+        $date = Carbon::createFromFormat($format, $dateStr);
+        $this->assertNotFalse($date, 'Something went really wrong. Parsing failed, but no exception thrown!');
+
+        // Debug...
+        ConsoleDebugger::output([
+            'format' => $format,
+            'input' => $dateStr,
+            'date' => $date->toArray()
+        ]);
+    }
+}
