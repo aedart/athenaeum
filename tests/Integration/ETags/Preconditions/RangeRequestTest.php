12651a349a02ce193bb86a5b530360d7700210d8
alin <alin@rspsystems.com>
2023-02-02 09:02:22 +0100

    Add test for single part partial response

--- tests/Integration/ETags/Preconditions/RangeRequestTest.php
+++ tests/Integration/ETags/Preconditions/RangeRequestTest.php
@@ -2,11 +2,14 @@
 
 namespace Aedart\Tests\Integration\ETags\Preconditions;
 
+use Aedart\Contracts\Streams\Exceptions\StreamException;
 use Aedart\ETags\Preconditions\Responses\DownloadStream;
+use Aedart\Streams\FileStream;
 use Aedart\Testing\Helpers\Http\Response;
 use Aedart\Tests\Helpers\Dummies\ETags\Requests\DownloadFileRequest;
 use Aedart\Tests\TestCases\ETags\PreconditionsTestCase;
 use Illuminate\Support\Facades\Route;
+use Teapot\StatusCode\All as Status;
 
 /**
  * RangeRequestTest
@@ -47,7 +50,7 @@ class RangeRequestTest extends PreconditionsTestCase
      *
      * @return void
      */
-    public function respondsFullFileContentWhenNoRangeRequested(): void
+    public function respondsWithEntireAttachmentWhenNoRangeRequested(): void
     {
         Route::get('/files/{name}', function (DownloadFileRequest $request) {
             return DownloadStream::for($request->resource)
@@ -68,15 +71,68 @@ class RangeRequestTest extends PreconditionsTestCase
         $headers = $response->headers;
         $content = Response::streamResponse($response);
 
+        // ------------------------------------------------------------ //
+
         $this->assertTrue($headers->has('Accept-Ranges'), 'Accept Ranges not set');
         $this->assertTrue($headers->has('Last-Modified'), 'Last Modified not set');
         $this->assertTrue($headers->has('ETag'), 'ETag not set');
         $this->assertTrue($headers->has('Content-Length'), 'Content Length not set');
+        $this->assertTrue($headers->has('Content-Type'), 'Content Type not set');
         $this->assertTrue($headers->has('Content-Disposition'), 'Content Disposition not set');
 
         $this->assertNotEmpty($content, 'No content was streamed');
 
         $original = $this->getOriginalFileContent($file);
         $this->assertSame($original, $content, 'Stream content does not match original content!');
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws StreamException
+     */
+    public function respondsSinglePartWhenASingleRangeRequested(): void
+    {
+        Route::get('/files/{name}', function (DownloadFileRequest $request) {
+            return DownloadStream::for($request->resource)
+                ->setName($request->route('name'));
+        })->name('file.download');
+
+        Route::getRoutes()->refreshNameLookups();
+
+        // ------------------------------------------------------------ //
+
+        $file = 'my-document.txt';
+        $url = route('file.download', [ 'name' => $file ]);
+        $response = $this
+            ->get($url, [
+                'Range' => 'bytes=0-99'
+            ])
+            ->assertStatus(Status::PARTIAL_CONTENT)
+            ->assertDownload($file);
+
+        $headers = $response->headers;
+        $content = Response::streamResponse($response);
+
+        // ------------------------------------------------------------ //
+
+        $this->assertTrue($headers->has('Accept-Ranges'), 'Accept Ranges not set');
+        $this->assertTrue($headers->has('Last-Modified'), 'Last Modified not set');
+        $this->assertFalse($headers->has('ETag'), 'ETag should NOT be set (default behaviour)');
+        $this->assertTrue($headers->has('Content-Length'), 'Content Length not set');
+        $this->assertTrue($headers->has('Content-Type'), 'Content Type not set');
+        $this->assertTrue($headers->has('Content-Disposition'), 'Content Disposition not set');
+
+        $this->assertNotEmpty($content, 'No content was streamed');
+
+        $original = $this->getOriginalFileContent($file);
+        $part = FileStream::openTemporary()
+            ->put($original)
+            ->positionToStart()
+            ->read(100);
+
+        $this->assertSame($part, $content, 'Stream content does not match requested range');
     }
 }
