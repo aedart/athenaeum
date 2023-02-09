c553253a077ade1b2d15ec38501dd8a521df3307
alin <alin@rspsystems.com>
2023-02-09 13:27:23 +0100

    Change branch name to main

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -22,9 +22,9 @@ git add -A
 git commit -m 'deploy docs'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:aedart/athenaeum.git master:gh-pages
+git push -f git@github.com:aedart/athenaeum.git main:gh-pages
 
 cd -
