098292785987d7c4fcb32efdd60eb4b83716437d
alin <alin@rspsystems.com>
2023-02-20 15:31:00 +0100

    Add example and test configuration

---
+++ tests/_data/configs/translation/translations-loader.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | Paths
+     |--------------------------------------------------------------------------
+     |
+     | By default, the application's "lang" directory is searched, along with any
+     | namespace and json paths registered by service providers that are booted.
+     | Use this to state additional paths to search in.
+    */
+
+    'paths' => [
+
+        realpath(__DIR__ . '/../../../../vendor/laravel/framework/src/Illuminate/Translation/lang')
+
+    ],
+];
