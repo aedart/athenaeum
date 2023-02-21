3399cbd2d6cb6092a87b26ad632424ba2c58b2d7
alin <alin@rspsystems.com>
2023-02-21 10:26:46 +0100

    Add exporter exceptions

---
+++ packages/Translation/src/Exports/Exceptions/FailedToExportTranslations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aedart\Translation\Exports\Exceptions;
+
+/**
+ * Failed To Export Translations
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Translation\Exports\Exceptions
+ */
+class FailedToExportTranslations extends ExporterException
+{
+}
