29ebb92d31509437bbd328f2e24c8b71dd8ae484
alin <alin@rspsystems.com>
2023-02-21 11:07:08 +0100

    Add null exporter

---
+++ packages/Translation/src/Exports/Drivers/NullExporter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aedart\Translation\Exports\Drivers;
+
+/**
+ * Null Exporter
+ *
+ * Intended for testing or situation when an exporter is
+ * required, yet not intended to do anything.
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Translation\Exports\Drivers
+ */
+class NullExporter extends BaseExporter
+{
+    /**
+     * @inheritDoc
+     */
+    public function performExport(array $locales, array $paths): mixed
+    {
+        return null;
+    }
+}
