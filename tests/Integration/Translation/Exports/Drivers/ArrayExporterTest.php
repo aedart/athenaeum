aa54068d828ae7fb3f146af94a527f527690e290
alin <alin@rspsystems.com>
2023-02-22 13:22:20 +0100

    Add assertions of exported translations

--- tests/Integration/Translation/Exports/Drivers/ArrayExporterTest.php
+++ tests/Integration/Translation/Exports/Drivers/ArrayExporterTest.php
@@ -72,7 +72,79 @@ class ArrayExporterTest extends TranslationTestCase
 
         ConsoleDebugger::output($translations);
 
-        // TODO: Incomplete...
-        $this->markTestIncomplete('Implementation not ready');
+        // -----------------------------------------------------------------------------//
+
+        $this->assertNotEmpty($translations, 'No translations exported');
+
+        $this->assertArrayHasKey('en', $translations);
+        $this->assertArrayHasKey('__JSON__', $translations['en'], 'en has no json group');
+        $this->assertNotEmpty($translations['en']['__JSON__'], 'en __JSON__ is empty');
+
+        $this->assertArrayHasKey('auth', $translations['en'], 'en has no auth group');
+        $this->assertNotEmpty($translations['en']['auth'], 'en auth is empty');
+
+        $this->assertArrayHasKey('pagination', $translations['en'], 'en has no pagination group');
+        $this->assertNotEmpty($translations['en']['pagination'], 'en pagination is empty');
+
+        $this->assertArrayHasKey('passwords', $translations['en'], 'en has no passwords group');
+        $this->assertNotEmpty($translations['en']['passwords'], 'en passwords is empty');
+
+        $this->assertArrayHasKey('validation', $translations['en'], 'en has no validation group');
+        $this->assertNotEmpty($translations['en']['validation'], 'en validation is empty');
+
+        $this->assertArrayHasKey('athenaeum-http-api::api-resources', $translations['en'], 'en has no athenaeum-http-api::api-resources group');
+        $this->assertNotEmpty($translations['en']['athenaeum-http-api::api-resources'], 'en athenaeum-http-api::api-resources is empty');
+
+        $this->assertArrayHasKey('translation-test::users', $translations['en'], 'en has no translation-test::users group');
+        $this->assertNotEmpty($translations['en']['translation-test::users'], 'en translation-test::users is empty');
+
+        $this->assertArrayHasKey('da', $translations);
+        $this->assertArrayHasKey('__JSON__', $translations['da'], 'da has no json group');
+        $this->assertNotEmpty($translations['da']['__JSON__'], 'da __JSON__ is empty');
+
+        $this->assertArrayHasKey('translation-test::users', $translations['da'], 'da has no translation-test::users group');
+        $this->assertNotEmpty($translations['da']['translation-test::users'], 'da translation-test::users is empty');
+
+        $this->assertArrayHasKey('en-uk', $translations);
+        $this->assertArrayHasKey('__JSON__', $translations['en-uk'], 'en-uk has no json group');
+        $this->assertNotEmpty($translations['en-uk']['__JSON__'], 'en-uk __JSON__ is empty');
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws ExporterException
+     * @throws ProfileNotFoundException
+     */
+    public function canExportSpecificLocaleAndGroups(): void
+    {
+        $translations = $this->exporter()->export(
+            locales: 'en',
+            groups: [
+                'auth',
+                'translation-test::users'
+            ]
+        );
+
+        ConsoleDebugger::output($translations);
+
+        // -----------------------------------------------------------------------------//
+
+        $this->assertNotEmpty($translations, 'No translations exported');
+
+        $this->assertArrayHasKey('en', $translations);
+        $this->assertArrayHasKey('__JSON__', $translations['en'], 'en has no json group');
+        $this->assertNotEmpty($translations['en']['__JSON__'], 'en __JSON__ is empty');
+
+        $this->assertArrayHasKey('auth', $translations['en'], 'en has no auth group');
+        $this->assertNotEmpty($translations['en']['auth'], 'en auth is empty');
+
+        $this->assertArrayHasKey('translation-test::users', $translations['en'], 'en has no translation-test::users group');
+        $this->assertNotEmpty($translations['en']['translation-test::users'], 'en translation-test::users is empty');
+
+        $this->assertArrayNotHasKey('da', $translations);
+        $this->assertArrayNotHasKey('en-uk', $translations);
     }
 }
