668ecde82eebebe4b58c2037edc365210655705c
alin <alin@rspsystems.com>
2023-02-23 15:55:27 +0100

    Add scanner interface (incomplete)
    This is going to be the main "driver" component

---
+++ packages/Contracts/src/Antivirus/Scanner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aedart\Contracts\Antivirus;
+
+/**
+ * Antivirus Scanner
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Antivirus
+ */
+interface Scanner
+{
+}
