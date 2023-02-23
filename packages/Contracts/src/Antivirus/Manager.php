abb47638f7ec3605f3bbe3c7c747101e676069a8
alin <alin@rspsystems.com>
2023-02-23 16:08:34 +0100

    Add antivirus manager interface

---
+++ packages/Contracts/src/Antivirus/Manager.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Aedart\Contracts\Antivirus;
+
+use Aedart\Contracts\Antivirus\Exceptions\ProfileNotFoundException;
+
+/**
+ * Antivirus Scanner Manager
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Antivirus
+ */
+interface Manager
+{
+    /**
+     * Creates a new antivirus scanner instance or returns existing
+     *
+     * @param string|null $name [optional] Name of scanner profile to obtain or create
+     * @param array $options [optional] Scanner options
+     *
+     * @return Scanner
+     *
+     * @throws ProfileNotFoundException
+     */
+    public function make(string|null $name = null, array $options = []): Scanner;
+
+    /**
+     * Creates a new antivirus scanner instance
+     *
+     * @param string|null $driver [optional] Class path to {@see Scanner}
+     * @param array $options [optional] Scanner options
+     *
+     * @return Scanner
+     */
+    public function scanner(string|null $driver, array $options = []): Scanner;
+
+    /**
+     * Returns class path of a default scanner to use
+     *
+     * @return class-string<Scanner>
+     */
+    public function defaultScanner(): string;
+}
