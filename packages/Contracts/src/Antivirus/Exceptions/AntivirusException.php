82ea7b2a253cab518b59de61e39898e5ddcc0d16
alin <alin@rspsystems.com>
2023-02-23 15:53:51 +0100

    Add antivirus related exception interfaces

---
+++ packages/Contracts/src/Antivirus/Exceptions/AntivirusException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Contracts\Antivirus\Exceptions;
+
+use Throwable;
+
+/**
+ * Antivirus Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Antivirus\Exceptions
+ */
+interface AntivirusException extends Throwable
+{
+}
