66ef69dffcbe958debbbf6de8f14e3114b52a9a4
alin <alin@rspsystems.com>
2023-02-24 13:27:26 +0100

    Add user resolver interface

---
+++ packages/Contracts/src/Antivirus/UserResolver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aedart\Contracts\Antivirus;
+
+/**
+ * User Resolver
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Contracts\Antivirus\Results
+ */
+interface UserResolver
+{
+    /**
+     * Returns the user that caused a file scan, if possible
+     *
+     * @return string|null User identifier, e.g. username, email or database id,
+     *                     if a user was identified.
+     */
+    public function resolve(): string|null;
+}
