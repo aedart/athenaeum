e0848e7730fafcd49cf9883e871827c00b3126b6
alin <alin@rspsystems.com>
2023-02-24 17:08:06 +0100

    Add generic scan status

---
+++ packages/Antivirus/src/Scanners/Status/GenericStatus.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Aedart\Antivirus\Scanners\Status;
+
+use Aedart\Antivirus\Exceptions\UnsupportedStatusValue;
+use Aedart\Antivirus\Results\BaseStatus;
+
+/**
+ * Generic Status
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Antivirus\Scanners\Status
+ */
+class GenericStatus extends BaseStatus
+{
+    /**
+     * @inheritDoc
+     */
+    public function resolveValue(mixed $value): bool
+    {
+        if (!is_bool($value)) {
+            throw new UnsupportedStatusValue(sprintf('Value must be boolean, %s provided', gettype($value)));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isOk(): bool
+    {
+        return $this->value() === true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        $value = $this->value()
+            ? 'Passed'
+            : 'Failed';
+
+        $reason = $this->reason();
+        $message = isset($reason)
+            ? ': ' . $reason
+            : '';
+
+        return "{$value}{$message}";
+    }
+}
