d383df9c3e1cb0a67c093958469d8d9d89a03b55
alin <alin@rspsystems.com>
2023-02-27 17:42:00 +0100

    Add ClamAV scanner

--- packages/Antivirus/configs/antivirus.php
+++ packages/Antivirus/configs/antivirus.php
@@ -1,5 +1,7 @@
 <?php
 
+use Aedart\Contracts\Streams\BufferSizes;
+
 return [
 
     /*
@@ -23,7 +25,31 @@ return [
     'profiles' => [
 
         'default' => [
-            // TODO: Add a default scanner profile...
+            'driver' => \Aedart\Antivirus\Scanners\ClamAv::class,
+            'options' => [
+
+                // Socket address to the ClamAV client
+                // E.g.:
+                // - Unix socket: '/var/run/clamav/clamd.ctl'
+                // - TCP socket: 'tcp://127.0.0.1:3310'
+                'socket' => env('CLAMAV_SOCKET', '/var/run/clamav/clamd.ctl'),
+
+                // Socket connection timeout (in seconds). If null, then
+                // the timeout is disabled!
+                'socket_timeout' => env('CLAMAV_SOCKET_TIMEOUT', 2),
+
+                // Timeout (in seconds) timeout for obtaining scan results.
+                'timeout' => env('CLAMAV_SCAN_TIMEOUT', 30),
+
+                // Maximum amount of bytes to send to the ClamAV client, in a single chunk.
+                // This value SHOULD NOT exceed "StreamMaxLength", defined in your clamd.conf (default 25 Mb).
+                'chunk_size' => env('CLAMAV_STREAM_CHUNK_SIZE', BufferSizes::BUFFER_1MB * 10),
+
+                // When dealing with PSR-7 Uploaded Files / Streams
+                // See documentation for additional details.
+                // 'temporary_stream_max_memory' => BufferSizes::BUFFER_1MB * 2,
+                // 'stream_buffer_size' => BufferSizes::BUFFER_1MB * 2,
+            ],
         ],
 
         'null' => [
