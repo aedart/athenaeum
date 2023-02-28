ece618fcc149a13cbfcf14a93375736b62b741a8
alin <alin@rspsystems.com>
2023-02-28 11:26:09 +0100

    Add "live" testing option....
    Should had been part of prev. commit!

--- tests/TestCases/Antivirus/AntivirusTestCase.php
+++ tests/TestCases/Antivirus/AntivirusTestCase.php
@@ -10,8 +10,10 @@ use Aedart\Contracts\Antivirus\Exceptions\ProfileNotFoundException;
 use Aedart\Contracts\Antivirus\Scanner;
 use Aedart\Contracts\Config\Loaders\Exceptions\InvalidPathException;
 use Aedart\Contracts\Config\Parsers\Exceptions\FileParserException;
+use Aedart\Testing\Helpers\ConsoleDebugger;
 use Aedart\Testing\TestCases\LaravelTestCase;
 use Codeception\Configuration;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 
 /**
  * Antivirus Test Case
@@ -23,6 +25,16 @@ abstract class AntivirusTestCase extends LaravelTestCase
 {
     use ConfigLoaderTrait;
     use AntivirusManagerTrait;
+
+    /**
+     * WARNING: When true, then some tests will use real
+     * connections, clients, native drivers,...erc
+     *
+     * Ensure to set CLAMAV_LIVE_TEST
+     *
+     * @var bool
+     */
+    protected bool $live = false;
 
     /*****************************************************************
      * Setup
@@ -41,6 +53,31 @@ abstract class AntivirusTestCase extends LaravelTestCase
         $this->getConfigLoader()
             ->setDirectory($this->configDir())
             ->load();
+
+        // Read the "live" test state from the environment file
+        $this->live = env('ANTIVIRUS_LIVE_TEST', false);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Ensure .env is loaded
+        $app->useEnvironmentPath(__DIR__ . '/../../../');
+        $app->loadEnvironmentFrom('.testing');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+    }
+
+    /**
+     * Determine if "real" (live) API requests should
+     * be undertaken!
+     *
+     * @return bool
+     */
+    protected function isLive(): bool
+    {
+        return $this->live;
     }
 
     /**
@@ -83,14 +120,23 @@ abstract class AntivirusTestCase extends LaravelTestCase
      *
      * @param string|null $profile [optional]
      * @param array $options [optional]
+     * @param callable|null $mockSetup [optional] Callback to configure native driver mocking.
+     *                                  Callback is only invoked if {@see live} is true.
      *
      * @return Scanner
      *
      * @throws ProfileNotFoundException
      */
-    public function makeScanner(string|null $profile = null, array $options = []): Scanner
+    public function makeScanner(string|null $profile = null, array $options = [], callable|null $mockSetup = null): Scanner
     {
-        return $this->getAntivirusManager()->profile($profile, $options);
+        $scanner = $this->getAntivirusManager()->profile($profile, $options);
+
+        if (!$this->isLive() && isset($mockSetup)) {
+            ConsoleDebugger::output(sprintf('Mocking driver for %s scanner profile', $profile ?? 'default'));
+            $mockSetup($scanner);
+        }
+
+        return $scanner;
     }
 
     /**
