68b5e423053c502f798dc222445de8189ba40aa1
Alin Eugen Deac <aedart@gmail.com>
2023-03-26 17:38:04 +0200

    Enable version disclaimer

--- docsx/.vuepress/layouts/Layout.vue
+++ docsx/.vuepress/layouts/Layout.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue';
 import {usePageData} from "@vuepress/client";
-// import VersionDisclaimer from "@aedart/vuepress-utils/components/VersionDisclaimer.vue";
-// import {isViewingNextRef, isViewingOtherRef} from "@aedart/vuepress-utils";
-// import archive from "../archive";
+import VersionDisclaimer from "@aedart/vuepress-utils/components/VersionDisclaimer.vue";
+import {isViewingNextRef, isViewingOtherRef} from "@aedart/vuepress-utils";
+import archive from "../archive";
 
-// /**
-//  * Page data...
-//  *
-//  * @type {PageDataRef<Record<never, never>>}
-//  */
-// const page = usePageData();
-//
-// /**
-//  * Determine if warning must be shown for upcoming version docs...
-//  *
-//  * @type {ComputedRef<boolean>}
-//  */
-// const showWarningForNext = isViewingNextRef(page, archive);
-//
-// /**
-//  * Determine if warning must be shown for outdated version docs...
-//  *
-//  * @type {ComputedRef<boolean>}
-//  */
-// const showWarningForOutdated = isViewingOtherRef(page, archive);
+/**
+ * Page data...
+ *
+ * @type {PageDataRef<Record<never, never>>}
+ */
+const page = usePageData();
+
+/**
+ * Determine if warning must be shown for upcoming version docs...
+ *
+ * @type {ComputedRef<boolean>}
+ */
+const showWarningForNext = isViewingNextRef(page, archive);
+
+/**
+ * Determine if warning must be shown for outdated version docs...
+ *
+ * @type {ComputedRef<boolean>}
+ */
+const showWarningForOutdated = isViewingOtherRef(page, archive);
 
 </script>
 
@@ -32,15 +32,15 @@ import {usePageData} from "@vuepress/client";
     <ParentLayout>
         <template #page-top>
 
-<!--            &lt;!&ndash; When viewing docs. for next version &ndash;&gt;-->
-<!--            <VersionDisclaimer v-if="showWarningForNext">-->
-<!--                You are viewing documentation for an upcoming version. <strong>It has not yet been released!</strong>!-->
-<!--            </VersionDisclaimer>-->
+            <!-- When viewing docs. for next version -->
+            <VersionDisclaimer v-if="showWarningForNext">
+                You are viewing documentation for an upcoming version. <strong>It has not yet been released!</strong>!
+            </VersionDisclaimer>
 
-<!--            &lt;!&ndash; When viewing docs. for outdated version &ndash;&gt;-->
-<!--            <VersionDisclaimer v-if="showWarningForOutdated" type="danger" label="Warning">-->
-<!--                You are viewing documentation for an outdated version. <strong>It is no longer supported!</strong>-->
-<!--            </VersionDisclaimer>-->
+            <!-- When viewing docs. for outdated version -->
+            <VersionDisclaimer v-if="showWarningForOutdated" type="danger" label="Warning">
+                You are viewing documentation for an outdated version. <strong>It is no longer supported!</strong>
+            </VersionDisclaimer>
 
         </template>
     </ParentLayout>
