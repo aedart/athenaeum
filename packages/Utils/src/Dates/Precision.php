603c4f2460c05b4e8cd62388f91c999d7ffda758
alin <alin@rspsystems.com>
2023-06-16 14:31:38 +0200

    Add PHPDoc for each precision case

--- packages/Utils/src/Dates/Precision.php
+++ packages/Utils/src/Dates/Precision.php
@@ -10,11 +10,38 @@ namespace Aedart\Utils\Dates;
  */
 enum Precision
 {
+    /**
+     * Yearly time precision
+     */
     case Year;
+
+    /**
+     * Monthly time precision
+     */
     case Month;
+
+    /**
+     * Daily time precision
+     */
     case Day;
+
+    /**
+     * Hourly time precision
+     */
     case Hour;
+
+    /**
+     * Minutes time precision
+     */
     case Minute;
+
+    /**
+     * Seconds time precision
+     */
     case Second;
+
+    /**
+     * Milliseconds time precision
+     */
     case Millisecond;
 }
