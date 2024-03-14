82dc2f45779a489c8a64860a1be094abb52aa0c7
alin <alin@rspsystems.com>
2024-03-14 14:12:20 +0100

    Cleanup

--- tests/Unit/Utils/Memory/E1_UsageTest.php
+++ tests/Unit/Utils/Memory/E1_UsageTest.php
@@ -10,10 +10,10 @@ use Aedart\Utils\Memory;
  * E1_UsageTest
  *
  * @group utils
- * * @group utils-memory
- * * @group utils-memory-unit
- * * @group utils-memory-unit-e1
- * * @group utils-memory-usage
+ * @group utils-memory
+ * @group utils-memory-unit
+ * @group utils-memory-unit-e1
+ * @group utils-memory-usage
  *
  * @author Alin Eugen Deac <ade@rspsystems.com>
  * @package Aedart\Tests\Unit\Utils\Memory
