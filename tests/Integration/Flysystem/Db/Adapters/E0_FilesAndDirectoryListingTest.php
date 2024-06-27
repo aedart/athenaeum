f498acbe04bdfd445015bc18433d0908459efb97
alin <alin@rspsystems.com>
2024-06-27 14:02:44 +0200

    Add test for content listing of directories / files with similar path

--- tests/Integration/Flysystem/Db/Adapters/E0_FilesAndDirectoryListingTest.php
+++ tests/Integration/Flysystem/Db/Adapters/E0_FilesAndDirectoryListingTest.php
@@ -96,4 +96,87 @@ class E0_FilesAndDirectoryListingTest extends FlysystemDbTestCase
 
         $this->assertCount(15, $list);
     }
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws \League\Flysystem\FilesystemException
+     */
+    public function doesNotListContentsFromOtherDirectoriesWithSimilarPath(): void
+    {
+        // @see https://github.com/aedart/athenaeum/issues/193
+
+        $directories = [
+            'home/projects/2/src',
+            'home/projects/22/src', // Notice that this path is similar to previous (starts with "home/projects/2...")
+                                    // The adapter should NOT list files from this directory, unless requested!
+            'home/projects/3/src',
+            'home/utils',
+        ];
+
+        $this->createDirectories($directories);
+
+        // ---------------------------------------------------------------- //
+
+        $pathA = 'home/projects/2/README.txt';
+        $pathB = 'home/projects/2/src/book.txt';
+
+        $pathC = 'home/projects/22/people.txt';
+        $pathD = 'home/projects/22/src/cookbook.txt';
+
+        $pathE = 'home/projects/3/README.txt';
+        $pathF = 'home/projects/3/src/book.txt';
+
+        $fs = $this->filesystem();
+
+        $fs->write($pathA, $this->getFaker()->sentence());
+        $fs->write($pathB, $this->getFaker()->sentence());
+        $fs->write($pathC, $this->getFaker()->sentence());
+        $fs->write($pathD, $this->getFaker()->sentence());
+        $fs->write($pathE, $this->getFaker()->sentence());
+        $fs->write($pathF, $this->getFaker()->sentence());
+
+        // ---------------------------------------------------------------- //
+        // List directories inside "project a"
+
+        $list = $this
+            ->filesystem()
+            ->listContents('home/projects/2')
+            ->toArray();
+
+        ConsoleDebugger::output($list);
+
+        $this->assertCount(2, $list);
+        $this->assertSame('home/projects/2/README.txt', $list[0]->path());
+        $this->assertSame('home/projects/2/src', $list[1]->path());
+
+        // ---------------------------------------------------------------- //
+        // List directories inside "project a" deep
+
+        $list = $this
+            ->filesystem()
+            ->listContents('home/projects/2', true)
+            ->toArray();
+
+        ConsoleDebugger::output($list);
+
+        $this->assertCount(3, $list);
+        $this->assertSame('home/projects/2/README.txt', $list[0]->path());
+        $this->assertSame('home/projects/2/src', $list[1]->path());
+        $this->assertSame('home/projects/2/src/book.txt', $list[2]->path());
+
+        // ---------------------------------------------------------------- //
+        // List all directories and files
+
+        $list = $this
+            ->filesystem()
+            ->listContents('', true)
+            ->toArray();
+
+        ConsoleDebugger::output($list);
+
+        $this->assertCount(15, $list);
+    }
 }
