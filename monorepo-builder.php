eea451e294296698d350746e7c9b7711a3060432
alin <alin@rspsystems.com>
2024-07-22 09:49:14 +0200

    Update dependencies
    Service update

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -49,9 +49,9 @@ return static function (MBConfig $config): void {
             'roave/security-advisories' => 'dev-master',
             'codeception/codeception' => '^5.1.2',
             "codeception/module-asserts" => "^3.0.0",
-            'orchestra/testbench' => '^v9.1.2',
-            'orchestra/testbench-dusk' => '^v9.3.0',
-            'illuminate/testing' => '^v11.13.0'
+            'orchestra/testbench' => '^v9.2.0',
+            'orchestra/testbench-dusk' => '^v9.4.0',
+            'illuminate/testing' => '^v11.16.0'
         ],
 
         ComposerJsonSection::AUTOLOAD => [
