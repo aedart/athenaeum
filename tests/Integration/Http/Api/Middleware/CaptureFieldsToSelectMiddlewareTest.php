dcc2b2ef2db5c1ae014810cb415840f0f914cd0f
alin <alin@rspsystems.com>
2024-10-29 13:13:41 +0100

    Add test that proves array input not handled
    This shows that when the "select" query parameter is submitted as an array, the middleware fails.

--- tests/Integration/Http/Api/Middleware/CaptureFieldsToSelectMiddlewareTest.php
+++ tests/Integration/Http/Api/Middleware/CaptureFieldsToSelectMiddlewareTest.php
@@ -6,9 +6,13 @@ use Aedart\Contracts\Http\Api\SelectedFieldsCollection;
 use Aedart\Http\Api\Middleware\CaptureFieldsToSelect;
 use Aedart\Support\Facades\IoCFacade;
 use Aedart\Testing\Helpers\ConsoleDebugger;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Models\User;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Requests\Users\ListUsersRequest;
+use Aedart\Tests\Helpers\Dummies\Http\Api\Resources\UserResource;
 use Aedart\Tests\TestCases\Http\ApiResourcesTestCase;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 
 /**
@@ -158,9 +162,50 @@ class CaptureFieldsToSelectMiddlewareTest extends ApiResourcesTestCase
 
         $request = new Request([ 'select' => implode(',', $requestedFields) ]);
 
-        (new CaptureFieldsToSelect())->handle($request, function () use (&$result) {
+        (new CaptureFieldsToSelect())->handle($request, function () {
             $result = IoCFacade::tryMake(SelectedFieldsCollection::class);
             return new Response();
         });
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     *
+     * @throws ValidationException
+     */
+    public function doesNotFailWhenFieldsRequestedAsAnArray(): void
+    {
+        // @see https://github.com/aedart/athenaeum/issues/197
+
+        /** @var SelectedFieldsCollection|null $result */
+        $result = null;
+
+        Route::get('/users', function (Request $request) use(&$result) {
+            $result = IoCFacade::tryMake(SelectedFieldsCollection::class);
+
+            return new Response();
+        })
+            ->middleware(CaptureFieldsToSelect::class)
+            ->name('users.index');
+
+        Route::getRoutes()->refreshNameLookups();
+
+        // ------------------------------------------------------------------------------------ //
+
+        $url = route('users.index') . '?select[0]=id&select[1]=user&select[2]=details';
+        $this
+            ->getJson($url)
+            ->assertOk();
+
+        // ------------------------------------------------------------------------------------ //
+
+        ConsoleDebugger::output($result);
+
+        $this->assertNotNull($result, 'no fields collection registered');
+        $this->assertTrue($result->has('id'), '"id" field not available');
+        $this->assertTrue($result->has('user'), '"user" field not available');
+        $this->assertTrue($result->has('details'), '"details" field not available');
     }
 }
