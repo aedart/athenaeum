2fc2bc18b610c881af293ffa22989d2b2914f477
alin <alin@rspsystems.com>
2025-02-25 14:41:43 +0100

    Add Env File related exceptions

---
+++ packages/Support/src/Env/Exceptions/EnvironmentFileException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Support\Env\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Environment File Exception
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Support\Env\Exceptions
+ */
+class EnvironmentFileException extends RuntimeException
+{
+}
---
+++ packages/Support/src/Env/Exceptions/KeyNotFound.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aedart\Support\Env\Exceptions;
+
+/**
+ * Key Not Found Exception
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Support\Env\Exceptions
+ */
+class KeyNotFound extends EnvironmentFileException
+{
+}
