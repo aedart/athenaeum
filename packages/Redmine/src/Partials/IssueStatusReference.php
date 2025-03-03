42fd08ee718d5695975f2a795ee9fa3f0408c3e4
alin <alin@rspsystems.com>
2025-03-03 10:55:52 +0100

    Add Issue Status Reference

---
+++ packages/Redmine/src/Partials/IssueStatusReference.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Aedart\Redmine\Partials;
+
+use Aedart\Dto\ArrayDto;
+
+/**
+ * Issue Status Reference
+ *
+ * @property string|int|null $id
+ * @property string $name
+ * @property bool $is_closed
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Redmine\Partials
+ */
+class IssueStatusReference extends Reference
+{
+    protected array $allowed = [
+        'id' => 'string', // type ignore - see getter / setter
+        'name' => 'string',
+        'is_closed' => 'bool'
+    ];
+}
