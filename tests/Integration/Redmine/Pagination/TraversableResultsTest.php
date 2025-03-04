7a4c77439f397b4a0ce950371b8e5ba7a5b20cda
alin <alin@rspsystems.com>
2025-03-04 10:38:21 +0100

    Fix "SQLite3::BuzyException: database is locked" error
    Uh... this really ugly, but for now this is all that can be done.

--- tests/Integration/Redmine/Pagination/TraversableResultsTest.php
+++ tests/Integration/Redmine/Pagination/TraversableResultsTest.php
@@ -104,7 +104,18 @@ class TraversableResultsTest extends RedmineTestCase
             $issue->delete();
         }
 
+        // When testing locally, using a Sqlite database, the API request might be too soon after the first
+        // project was deleted, which causes a "Database locked" exception / 500 Internal Server Error from
+        // Redmine. To avoid this, we wait for ~250 ms.
+        if ($this->isLive()) {
+            usleep(250_000);
+        }
+
         $project->delete();
+
+        if ($this->isLive()) {
+            usleep(250_000);
+        }
 
         // Debug
         // RedmineApiResource::$debug = false;
