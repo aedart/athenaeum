c0712128eee412787eb1fd7b607e6a05680a9ee6
alin <alin@rspsystems.com>
2025-10-02 08:42:22 +0200

    Update dependencies
    Service update

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -51,7 +51,7 @@ return static function (MBConfig $config): void {
             "codeception/module-asserts" => "^3.2.0",
             'orchestra/testbench' => '^v10.6.0',
             'orchestra/testbench-dusk' => '^v10.6.0',
-            'illuminate/testing' => '^v12.29.1'
+            'illuminate/testing' => '^v12.32.5'
         ],
 
         ComposerJsonSection::AUTOLOAD => [
