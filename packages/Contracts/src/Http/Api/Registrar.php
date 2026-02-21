0100965b86f392494cd0eecac2f3160318d48c20
aedart <aedart@gmail.com>
2026-02-21 14:05:57 +0100

    Improve PHPDoc for API Resource Registrar

--- packages/Contracts/src/Http/Api/Registrar.php
+++ packages/Contracts/src/Http/Api/Registrar.php
@@ -5,6 +5,9 @@ namespace Aedart\Contracts\Http\Api;
 /**
  * Api Resource Registrar
  *
+ * @template M of \Illuminate\Database\Eloquent\Model Eloquent model instance or class path
+ * @template A of \Aedart\Http\Api\Resources\ApiResource API Resource instance or class path
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Contracts\Http\Api
  */
@@ -13,8 +16,8 @@ interface Registrar
     /**
      * Register multiple Api Resources
      *
-     * @param  array  $resources key = eloquent model instance or class path,
-     *                           value = api resource instance or class path
+     * @param  array<M|class-string<M>, A, class-string<A>>  $resources
+     *
      * @return self
      */
     public function register(array $resources): static;
@@ -22,8 +25,8 @@ interface Registrar
     /**
      * Set the Api Resource to be associated with given model
      *
-     * @param string|\Illuminate\Database\Eloquent\Model $model class path or eloquent model instance
-     * @param string|\Aedart\Http\Api\Resources\ApiResource $resource class path or api resource instance
+     * @param M|class-string<M> $model
+     * @param A|class-string<A> $resource
      *
      * @return self
      */
@@ -32,7 +35,7 @@ interface Registrar
     /**
      * Determine if an Api Resource is registered for the model
      *
-     * @param string|\Illuminate\Database\Eloquent\Model $model class path or eloquent model instance
+     * @param M|class-string<M> $model
      *
      * @return bool
      */
@@ -41,9 +44,9 @@ interface Registrar
     /**
      * Get Api Resource for given model
      *
-     * @param string|\Illuminate\Database\Eloquent\Model $model class path or eloquent model instance
+     * @param M|class-string<M> $model
      *
-     * @return string|null Class path or null if no resource was registered for the model
+     * @return class-string<A>|null Class path or null if no resource was registered for the model
      */
     public function get($model): string|null;
 
@@ -52,7 +55,7 @@ interface Registrar
      *
      * @param  string  $type Resource type (singular or plural form)
      *
-     * @return string|null Eloquent model class path or null when none found
+     * @return class-string<M>|null Eloquent model class path or null when none found
      */
     public function findModelByType(string $type): string|null;
 
@@ -61,14 +64,14 @@ interface Registrar
      *
      * @param  string  $type Resource type (singular or plural form)
      *
-     * @return string|null Api Resource class path or null when none found
+     * @return class-string<A>|null Api Resource class path or null when none found
      */
     public function findResourceByType(string $type): string|null;
 
     /**
      * Forget Api Resource for given model
      *
-     * @param string|\Illuminate\Database\Eloquent\Model $model class path or eloquent model instance
+     * @param M|class-string<M> $model class path or eloquent model instance
      *
      * @return bool
      */
