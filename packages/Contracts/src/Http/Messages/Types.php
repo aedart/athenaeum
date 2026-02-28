8a0f53628b5e76d2ee15bf09f0c930ad04d6cf4f
aedart <aedart@gmail.com>
2026-02-28 11:53:31 +0100

    Deprecate Http Message Types interface
    Replaced by new Type enum.

--- packages/Contracts/src/Http/Messages/Types.php
+++ packages/Contracts/src/Http/Messages/Types.php
@@ -6,6 +6,8 @@ namespace Aedart\Contracts\Http\Messages;
 /**
  * Http Message Types
  *
+ * @deprecated Replaced by {@see \Aedart\Contracts\Http\Messages\Type}, since v10.x
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Contracts\Http\Messages
  */
@@ -13,16 +15,25 @@ interface Types
 {
     /**
      * Request type
+     *
+     * @deprecated use {@see \Aedart\Contracts\Http\Messages\Type::REQUEST} instead, since v10.x
      */
+    #[\Deprecated(message: "use \Aedart\Contracts\Http\Messages\Type::REQUEST instead", since: "10.x")]
     public const string TYPE_REQUEST = 'request';
 
     /**
      * Server Request type
+     *
+     * @deprecated use {@see \Aedart\Contracts\Http\Messages\Type::SERVER_REQUEST} instead, since v10.x
      */
+    #[\Deprecated(message: "use \Aedart\Contracts\Http\Messages\Type::SERVER_REQUEST instead", since: "10.x")]
     public const string TYPE_SERVER_REQUEST = 'server-request';
 
     /**
      * Response type
+     *
+     * @deprecated use {@see \Aedart\Contracts\Http\Messages\Type::RESPONSE} instead, since v10.x
      */
+    #[\Deprecated(message: "use \Aedart\Contracts\Http\Messages\Type::RESPONSE instead", since: "10.x")]
     public const string TYPE_RESPONSE = 'response';
 }
