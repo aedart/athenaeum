73eb0ea60110349bb41049a862bbd151764ff9d7
alin <alin@rspsystems.com>
2026-03-04 11:44:34 +0100

    Add Undefined Property Exception interface

---
+++ packages/Contracts/src/Dto/Exceptions/UndefinedPropertyException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aedart\Contracts\Dto\Exceptions;
+
+use Throwable;
+
+/**
+ * Undefined Property Exception
+ *
+ * @author Alin Eugen Deac <ade@rspsystems.com>
+ * @package Aedart\Contracts\Dto\Exceptions
+ */
+interface UndefinedPropertyException extends Throwable
+{
+}
