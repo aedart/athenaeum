d189f7a6d386525efc76706245dfeff6ec1cb858
alin <alin@rspsystems.com>
2026-03-04 11:52:14 +0100

    Deprecate Undefined Property exception (properties package)
    Replaced by exception in Dto package of the same name. Also, ensured that this exception inherits from new "undefined property exception" interface, in hopes that it makes it a bit easier for consumers to adapt their code.

--- packages/Properties/src/Exceptions/UndefinedProperty.php
+++ packages/Properties/src/Exceptions/UndefinedProperty.php
@@ -2,6 +2,7 @@
 
 namespace Aedart\Properties\Exceptions;
 
+use Aedart\Contracts\Dto\Exceptions\UndefinedPropertyException;
 use LogicException;
 
 /**
@@ -11,9 +12,11 @@ use LogicException;
  *
  * Throw this exception when a non-existing property is attempted accessed
  *
+ * @deprecated use {@see \Aedart\Dto\Exceptions\UndefinedProperty} instead, since v10.x
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Properties\Exceptions
  */
-class UndefinedProperty extends LogicException
+class UndefinedProperty extends LogicException implements UndefinedPropertyException
 {
 }
