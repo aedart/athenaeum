3826e4eb0f03bd610c168663f3761db997797380
alin <alin@rspsystems.com>
2026-03-04 11:27:52 +0100

    Deprecate AccessibilityLevels
    Use ReflectionProperty instead.

--- packages/Contracts/src/Properties/AccessibilityLevels.php
+++ packages/Contracts/src/Properties/AccessibilityLevels.php
@@ -15,6 +15,8 @@ use ReflectionProperty;
  *
  * @see \ReflectionProperty
  *
+ * @deprecated use {@see \ReflectionProperty} instead, since v10.x
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Contracts\Properties
  */
@@ -23,24 +25,36 @@ interface AccessibilityLevels
     /**
      * Public level - no 'protected' nor 'private' properties
      * are going to be automatically get / set
+     *
+     * @deprecated use {@see \ReflectionProperty::IS_PUBLIC} instead, since v10.x
      */
+    #[\Deprecated(message: "use \ReflectionProperty::IS_PUBLIC instead", since: "10.x")]
     public const int PUBLIC_LEVEL = ReflectionProperty::IS_PUBLIC;
 
     /**
      * Protected level - properties that are declared
      * 'protected' can be set / get.
+     *
+     * @deprecated use {@see \ReflectionProperty::IS_PROTECTED} instead, since v10.x
      */
+    #[\Deprecated(message: "use \ReflectionProperty::IS_PROTECTED instead", since: "10.x")]
     public const int PROTECTED_LEVEL = ReflectionProperty::IS_PROTECTED;
 
     /**
      * Private level - properties that are declared
      * 'protected' or 'private' can be set / get.
+     *
+     * @deprecated use {@see \ReflectionProperty::IS_PRIVATE} instead, since v10.x
      */
+    #[\Deprecated(message: "use \ReflectionProperty::IS_PRIVATE instead", since: "10.x")]
     public const int PRIVATE_LEVEL = ReflectionProperty::IS_PRIVATE;
 
     /**
      * Levels map, key = level, value = name of level
+     *
+     * @deprecated will not be available in the next major version, since v10.x
      */
+    #[\Deprecated(message: "will not be available in the next major version", since: "10.x")]
     public const array LEVELS = [
         self::PUBLIC_LEVEL => 'public',
         self::PROTECTED_LEVEL => 'protected',
