af458322e874ae9227a676d27ed590643b2829cb
alin <alin@rspsystems.com>
2026-07-13 10:03:32 +0200

    Use new ECSConfig::configure() fluent api
    Previous The previous "callback" method has been deprecated.

--- ecs.php
+++ ecs.php
@@ -7,41 +7,44 @@ use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\LanguageConstruct\NullableTypeDeclarationFixer;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $config): void {
+return ECSConfig::configure()
     // A. standalone rules
-    $config->rulesWithConfiguration([
-        ArraySyntaxFixer::class => [
-            'syntax' => 'short',
-        ],
-        NullableTypeDeclarationFixer::class => [
+    ->withConfiguredRule(
+        ArraySyntaxFixer::class,
+        [
+            'syntax' => 'short'
+        ]
+    )
+    ->withConfiguredRule(
+        NullableTypeDeclarationFixer::class,
+        [
             'syntax' => 'union'
         ]
-    ]);
+    )
 
     // B. full sets
-    $config->sets([
+    ->withSets([
         SetList::CLEAN_CODE,
         SetList::PSR_12,
-    ]);
+    ])
 
     // Set paths
-    $config->paths([
+    ->withPaths([
         'packages',
         'tests/Helpers',
         'tests/TestCases',
         'tests/Integration',
         'tests/Unit',
         //'src',
-    ]);
+    ])
 
     // Skip
-    $config->skip([
-        SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class . '.UnusedProperty' => [
+    ->withSkip([
+        \SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class . '.UnusedProperty' => [
             # The "private int $height" is used by Dto, via magic methods...
             'tests/Helpers/Dummies/Properties/Accessibility/Person.php'
         ]
-    ]);
+    ])
 
     // Run all checks parallel (should be much faster)
-    $config->parallel();
-};
+    ->withParallel();
